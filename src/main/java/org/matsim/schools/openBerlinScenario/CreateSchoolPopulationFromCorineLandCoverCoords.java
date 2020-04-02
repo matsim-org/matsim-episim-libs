5c9479aa9a31ea9486fcbb2a4a42aa40bc1420f3
tschlenther <schlenther@vsp.tu-berlin.de>
2020-04-02 21:32:03 +0200

    set up input files

--- src/main/java/org/matsim/schools/openBerlinScenario/CreateSchoolPopulationFromCorineLandCoverCoords.java
+++ src/main/java/org/matsim/schools/openBerlinScenario/CreateSchoolPopulationFromCorineLandCoverCoords.java
@@ -29,27 +29,33 @@ import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.population.io.PopulationReader;
 import org.matsim.core.population.io.PopulationWriter;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.schools.CreateSchoolPopulation;
 import playground.vsp.corineLandcover.CORINELandCoverCoordsModifier;
 import playground.vsp.openberlinscenario.cemdap.output.CemdapOutput2MatsimPlansConverter;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
-public class RunCorineLandCoverCoordsModifier {
+public class CreateSchoolPopulationFromCorineLandCoverCoords {
 
 	private static final double SAMPLE_SIZE = 0.1;
 
 	public static void main(String[] args) {
 	    String inputPlansFile = "../../svn/shared-svn/studies/countries/de/open_berlin_scenario/be_5/cemdap_input/500/plans_children.xml.gz";
-		String inputPlansreadyForCorine = "../../svn/shared-svn/studies/countries/de/open_berlin_scenario/be_5/cemdap_input/500/plans_children_readyForCorine_10pct.xml.gz";
+		String inputPlansReadyForCorine = "../../svn/shared-svn/studies/countries/de/open_berlin_scenario/be_5/cemdap_input/500/plans_children_readyForCorine_10pct.xml.gz";
 		String corineLandCoverFile = "../../svn/shared-svn/studies/countries/de/open_berlin_scenario/input/shapefiles/corine_landcover/corine_lancover_berlin-brandenburg_GK4.shp";
+
+		String inputBerlinAdultPlans = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.4-10pct/input/berlin-v5.4-10pct.plans.xml.gz";
+		String inputSchoolFacilities = "../shared-svn/projects/episim/matsim-files/snz/Berlin/educFacilities_optimated.txt";
 
 		String zoneShapeFile = "../../svn/shared-svn/studies/countries/de/open_berlin_scenario/input/shapefiles/2016/gemeinden_Planungsraum_GK4.shp";
 		String zoneIdTag = "NR";
 
-		String outputPlansFileWOPlans = "../../svn/shared-svn/studies/countries/de/open_berlin_scenario/be_5/population/plans_children_corineCoords_WOPlans_10pct.xml.gz";
-		String outputPlansFile = "../../svn/shared-svn/studies/countries/de/open_berlin_scenario/be_5/population/plans_children_corineCoords_10pct.xml.gz";
+		String outputPlansEntireBerlin = "../../svn/shared-svn/studies/countries/de/open_berlin_scenario/be_5/population/plans_500_includingChildren_corineCoords_10pct.xml.gz";
+		String outputSchoolPlans = "../../svn/shared-svn/studies/countries/de/open_berlin_scenario/be_5/population/plans_500_onlyChildren_corineCoords_10pct.xml.gz";
 
-	    assignDummyHomeActsAndWritePlans(inputPlansFile, inputPlansreadyForCorine);
+	    assignDummyHomeActsAndWritePlans(inputPlansFile, inputPlansReadyForCorine);
 
 	    boolean simplifyGeom = false;
 	    boolean combiningGeoms = false;
@@ -59,16 +65,28 @@ public class RunCorineLandCoverCoordsModifier {
 	    Map<String, String> shapeFileToFeatureKey = new HashMap<>();
 	    shapeFileToFeatureKey.put(zoneShapeFile, zoneIdTag);
 
-		CORINELandCoverCoordsModifier plansFilterForCORINELandCover = new CORINELandCoverCoordsModifier(inputPlansreadyForCorine, shapeFileToFeatureKey,
+		CORINELandCoverCoordsModifier plansFilterForCORINELandCover = new CORINELandCoverCoordsModifier(inputPlansReadyForCorine, shapeFileToFeatureKey,
 	            corineLandCoverFile, simplifyGeom, combiningGeoms, sameHomeActivity, homeActivityPrefix);
 
 		plansFilterForCORINELandCover.process();
 		Population population = plansFilterForCORINELandCover.getPopulation();
 		PopulationWriter writer = new PopulationWriter(population);
-		writer.write(outputPlansFile);
 		preparePlansForSchooPopulationCreation(population);
-		writer.write(outputPlansFileWOPlans);
-    }
+		writer.write(outputSchoolPlans);
+
+		//now run CreateSchoolPopulation which will read facilities, assign schools and build plans and finally will merge the adult population with the school population
+		try {
+			//we already sampled so sample size is set to 1 in this step
+			CreateSchoolPopulation.run(population, 1, inputBerlinAdultPlans, inputSchoolFacilities, outputPlansEntireBerlin );
+
+			//delete the file created in middle of the process - it is not needed any more
+			new File(inputPlansReadyForCorine).delete();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+	}
 
 
     private static void assignDummyHomeActsAndWritePlans(String inputPlans, String outputPlans){
