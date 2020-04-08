c3810da23ed27b8fbabbf1d57d3f86ea4c525951
tschlenther <schlenther@vsp.tu-berlin.de>
2020-04-08 20:16:33 +0200

    prepare berlin

--- src/main/java/org/matsim/scenarioCreation/FilterEducFacilitiesForCertainArea.java
+++ src/main/java/org/matsim/scenarioCreation/FilterEducFacilitiesForCertainArea.java
@@ -52,22 +52,26 @@ public class FilterEducFacilitiesForCertainArea {
 
 	static final Logger log = Logger.getLogger(FilterEducFacilitiesForCertainArea.class);
 
-	private static final String workingDir = "../../svn/shared-svn/projects/episim/matsim-files/";
-	private static final String pathOfEduFacilitiesGER = workingDir + "snz/Deutschland/de_facilities.education.xy";
-	private static final String ShapeFile = "../../svn/shared-svn/studies/countries/de/open_berlin_scenario/input/shapefiles/2016/gemeinden_Planungsraum_GK4.shp";
-	private static final String outputEducFileDir = workingDir + "snz/Berlin/processed-data/be_snz_educationFacilities.txt";
+	private static final String workingDir = "../../svn/shared-svn/projects/episim/matsim-files/snz/";
+	private static final String pathOfEduFacilitiesGER = workingDir + "Deutschland/de_facilities.education.xy";
+	private static final String ShapeFile = workingDir + "Berlin/original-data/berlin_umland.shp";
+	private static final String outputEducFileDir = workingDir + "Berlin/processed-data/be_snz_educationFacilities.txt";
 	private static List<EducFacility> educList = new ArrayList<>();
 	private static List<EducFacility> educListNewArea = new ArrayList<>();
 	private static List<EducFacility> educListNewAreaForOutput = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
 
+		boolean aggregateFacilities = true;
+
+
 		//input shape is GK4, input facilities are in UTM32. This flag determines if the result will be in GK4 (true) or on in UTM32N
 		//TODO clarify
 		boolean coordToTransform = false;
-		boolean aggregateFacilities = true;
+//		CoordinateTransformation transformation = TransformationFactory.getCoordinateTransformation("EPSG:25832", TransformationFactory.DHDN_GK4);
 
-		CoordinateTransformation transformation = TransformationFactory.getCoordinateTransformation("EPSG:25832", TransformationFactory.DHDN_GK4);
+		//new shape input (original-data/berlin_umland.shp) is also in UTM32N
+		CoordinateTransformation transformation= null;
 
 		educList.addAll(EducFacilities.readEducFacilites(pathOfEduFacilitiesGER, transformation));
 
@@ -82,8 +86,8 @@ public class FilterEducFacilitiesForCertainArea {
 		}
 		if (!coordToTransform) {
 			//transform back into UTM32N
-			CoordinateTransformation transformationBack = TransformationFactory.getCoordinateTransformation(TransformationFactory.DHDN_GK4, "EPSG:25832");
-			educListNewAreaForOutput.forEach(educFacility -> educFacility.setCoord(transformationBack.transform(educFacility.getCoord())));
+//			CoordinateTransformation transformationBack = TransformationFactory.getCoordinateTransformation(TransformationFactory.DHDN_GK4, "EPSG:25832");
+//			educListNewAreaForOutput.forEach(educFacility -> educFacility.setCoord(transformationBack.transform(educFacility.getCoord())));
 		}
 
 		writeOutputFile();
