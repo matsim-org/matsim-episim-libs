f6ebe9c8fd644155758a4bb620ccfaa018f3fdd2
Christian Rakow <rakow@vsp.tu-berlin.de>
2020-04-21 16:58:09 +0200

    also report required mask for restrictions

--- src/main/java/org/matsim/episim/policy/FixedPolicy.java
+++ src/main/java/org/matsim/episim/policy/FixedPolicy.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -23,6 +23,7 @@ package org.matsim.episim.policy;
 import com.google.common.collect.ImmutableMap;
 import com.typesafe.config.Config;
 import org.matsim.episim.EpisimReporting;
+import org.matsim.episim.model.FaceMask;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -91,6 +92,13 @@ public class FixedPolicy extends ShutdownPolicy {
 		 */
 		public ConfigBuilder restrict(long day, double fraction, String... activities) {
 			return restrict(day, Restriction.of(fraction), activities);
+		}
+
+		/**
+		 * Same as {@link #restrict(long, Restriction, String...)}  with default values.
+		 */
+		public ConfigBuilder restrict(long day, double fraction, FaceMask mask, String... activities) {
+			return restrict(day, Restriction.of(fraction, mask), activities);
 		}
 
 		/**
