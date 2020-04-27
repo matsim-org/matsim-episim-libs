2c2c26760e5351baaff5d99a2ae9f9589c3f93ba
smuellervsp <mueller@vsp.tu-berlin.de>
2020-04-27 20:25:29 +0200

    battery berlin v8

--- src/main/java/org/matsim/run/batch/SchoolClosureAndMasks.java
+++ src/main/java/org/matsim/run/batch/SchoolClosureAndMasks.java
@@ -38,17 +38,18 @@ public final class SchoolClosureAndMasks implements BatchRun<SchoolClosureAndMas
 	public Config baseCase(int id) {
 
 		Config config = ConfigUtils.createConfig(new EpisimConfigGroup());
-		config.plans().setInputFile("../be_entirePopulation_noPlans_withDistrict.xml.gz");
+		config.plans().setInputFile("../be_v2_snz_entirePopulation_emptyPlans_withDistricts.xml.gz");
 
 
 		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
 
-		episimConfig.setInputEventsFile("../be_snz_episim_events.xml.gz");
+		episimConfig.setInputEventsFile("../be_v2_snz_episim_events.xml.gz");
 		episimConfig.setFacilitiesHandling(EpisimConfigGroup.FacilitiesHandling.snz);
 
 		episimConfig.setSampleSize(0.25);
-		episimConfig.setCalibrationParameter(0.000002);
-		episimConfig.setInitialInfections(5);
+		episimConfig.setCalibrationParameter(0.000_001_7);
+		episimConfig.setInitialInfections(50);
+		episimConfig.setInitialInfectionDistrict("Berlin");
 
 		SnzScenario.addParams(episimConfig);
 		SnzScenario.setContactIntensities(episimConfig);
@@ -61,28 +62,77 @@ public final class SchoolClosureAndMasks implements BatchRun<SchoolClosureAndMas
 
 		Config config = baseCase(id);
 		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
+		int offset = params.offset;
+		FaceMask wornMask = FaceMask.valueOf(params.mask);
+		episimConfig.setMaskCompliance(params.maskCompliance);
+
 		
 		com.typesafe.config.Config policyConf = FixedPolicy.config()
-				.restrict(23 + params.offset, 0.6, "leisure")
-				.restrict(23 + params.offset, 0.1, "educ_primary", "educ_kiga")
-				.restrict(23 + params.offset, 0., "educ_secondary", "educ_higher")
-				.restrict(32 + params.offset, 0., "leisure")
-				.restrict(32 + params.offset, 0.6, "work")
-				.restrict(32 + params.offset, 0.4, "shopping", "errands", "business")
-				.restrict(67 + params.offset, Restriction.of(1, FaceMask.CLOTH), params.maskHome)
-				.restrict(67 + params.offset, Restriction.of(params.remainingFractionLeisure, FaceMask.CLOTH), "leisure")
-				.restrict(67 + params.offset, Restriction.of(params.remainingFractionWork, FaceMask.CLOTH), "work")
-				.restrict(67 + params.offset, Restriction.of(params.remainingFractionShoppingBusinessErrands, FaceMask.CLOTH), "shopping", "errands", "business")
-				.restrict(67 + params.offset, Restriction.of(0.1, FaceMask.CLOTH), "educ_primary", "educ_kiga")
-				.restrict(67 + params.offset, Restriction.of(1, FaceMask.CLOTH), "pt", "tr")
-				.restrict(74 + params.offset, Restriction.of(params.remainingFractionKiga, FaceMask.CLOTH), "educ_kiga")
-				.restrict(74 + params.offset, Restriction.of(params.remainingFractionPrima, FaceMask.CLOTH), "educ_primary")
+				//taken from Google mobility report
+				.restrict(20 - offset, 0.95, "work")
+				.restrict(22 - offset, 0.9, "work")
+				.restrict(23 - offset, 0.85, "work")
+				.restrict(25 - offset, 0.8, "work")
+				.restrict(26 - offset, 0.65, "work")	
+				.restrict(27 - offset, 0.6, "work")
+				.restrict(28 - offset, 0.55, "work")
+				.restrict(31 - offset, 0.5, "work")
+				.restrict(33 - offset, 0.45, "work")
+				.restrict(15 - offset, 0.95, "shopping", "errands", "business")
+				.open(17 - offset, "shopping", "errands", "business")
+				.restrict(19 - offset, 0.95, "shopping", "errands", "business")
+				.open(20 - offset, "shopping", "errands", "business")
+				.restrict(26 - offset, 0.95, "shopping", "errands", "business")
+				.restrict(27 - offset, 0.85, "shopping", "errands", "business")
+				.restrict(28 - offset, 0.75, "shopping", "errands", "business")
+				.restrict(29 - offset, 0.7, "shopping", "errands", "business")
+				.restrict(31 - offset, 0.65, "shopping", "errands", "business")
+				.restrict(33 - offset, 0.6, "shopping", "errands", "business")
+				.restrict(35 - offset, 0.65, "shopping", "errands", "business")
+				.restrict(36 - offset, 0.6, "shopping", "errands", "business")
+				.restrict(40 - offset, 0.65, "shopping", "errands", "business")
+				.restrict(45 - offset, 0.7, "shopping", "errands", "business")
+				.restrict(46 - offset, 0.75, "shopping", "errands", "business")
+				.restrict(48 - offset, 0.8, "shopping", "errands", "business")
+				.restrict(49 - offset, 0.85, "shopping", "errands", "business")
+				.restrict(50 - offset, 0.75, "shopping", "errands", "business")
+				.restrict(55 - offset, 0.7, "shopping", "errands", "business")
+				.restrict(18 - offset, 0.94, "leisure")
+				.restrict(19 - offset, 0.87, "leisure")
+				.restrict(20 - offset, 0.81, "leisure")
+				.restrict(21 - offset, 0.74, "leisure")
+				.restrict(22 - offset, 0.68, "leisure")
+				.restrict(23 - offset, 0.61, "leisure")
+				.restrict(24 - offset, 0.55, "leisure")
+				.restrict(25 - offset, 0.49, "leisure")
+				.restrict(26 - offset, 0.42, "leisure")
+				.restrict(27 - offset, 0.36, "leisure")
+				.restrict(28 - offset, 0.29, "leisure")
+				.restrict(29 - offset, 0.23, "leisure")
+				.restrict(30 - offset, 0.16, "leisure")
+				.restrict(31 - offset, 0.1, "leisure")
+				.restrict(23 - offset, 0.1, "educ_primary", "educ_kiga")
+				.restrict(23 - offset, 0., "educ_secondary", "educ_higher")
+				// Google mobility data currently stops at day 58 (18.04.2020)
+				.restrict(58 - offset, params.remainingFractionWork, "work")
+				.restrict(58 - offset, params.remainingFractionShoppingBusinessErrands, "shopping", "errands", "business")
+				.restrict(58 - offset, params.remainingFractionLeisure, "leisure")
+				// masks are worn from day 67 onwards (27.04.2020); compliance is set via config
+				.restrict(67 - offset, Restriction.of(params.remainingFractionWork, wornMask), "work")
+				.restrict(67 - offset, Restriction.of(params.remainingFractionShoppingBusinessErrands, wornMask), "shopping", "errands", "business")
+				.restrict(67 - offset, Restriction.of(params.remainingFractionLeisure, wornMask), "leisure")
+				.restrict(67 - offset, Restriction.of(1, wornMask), "pt", "tr")
+				.restrict(67 - offset, Restriction.of(0.1, wornMask), "educ_primary", "educ_kiga")
+				// edu facilities can be reopend from day 74 (04.05.2020)
+				.restrict(74 - offset, Restriction.of(params.remainingFractionKiga, wornMask), "educ_kiga")
+				.restrict(74 - offset, Restriction.of(params.remainingFractionPrima, wornMask), "educ_primary")
+				.restrict(74 - offset, Restriction.of(params.remainingFractionSeconHigher, wornMask), "educ_secondary", "educ_higher")
 				.build();
 
 		String policyFileName = "input/policy" + id + ".conf";
 		episimConfig.setOverwritePolicyLocation(policyFileName);
 		episimConfig.setPolicy(FixedPolicy.class, policyConf);
-		episimConfig.setMaskCompliance(params.maskCompliance);
+		
 		return config;
 	}
 
@@ -97,29 +147,32 @@ public final class SchoolClosureAndMasks implements BatchRun<SchoolClosureAndMas
 
 	public static final class Params {
 
-		@IntParameter({3, 0, 6})
+		@IntParameter({-6})
 		int offset;
-		
-		@Parameter({0., 0.5, 0.9, 1.})
-		double maskCompliance;
-		
-		@StringParameter({"noHome", "home"})
-		String maskHome;
 
 		@Parameter({0.5, 0.1})
 		double remainingFractionKiga;
 
 		@Parameter({0.5, 0.1})
 		double remainingFractionPrima;
+		
+		@Parameter({0.5, 0.})
+		double remainingFractionSeconHigher;
 
 		@Parameter({0.2, 0.})
 		double remainingFractionLeisure;
 
-		@Parameter({0.8, 0.6})
+		@Parameter({0.45, 0.6})
 		double remainingFractionWork;
 
 		@Parameter({0.6, 0.4})
 		double remainingFractionShoppingBusinessErrands;
+		
+		@StringParameter({"NONE", "CLOTH", "SURGICAL"})
+		String mask;
+		
+		@Parameter({0., 0.5, 0.9, 1.})
+		double maskCompliance;
 
 	}
 
