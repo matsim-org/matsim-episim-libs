305417fc3146f1fa23a84dc9a4fe91b9cb0231a2
smuellervsp <mueller@vsp.tu-berlin.de>
2020-04-27 09:57:33 +0200

    switch to berlin v2, calibParam, udate google mobility data

--- src/main/java/org/matsim/run/modules/SnzScenario.java
+++ src/main/java/org/matsim/run/modules/SnzScenario.java
@@ -74,8 +74,9 @@ public class SnzScenario extends AbstractModule {
 	@Singleton
 	public Config config() {
 		Config config = ConfigUtils.createConfig(new EpisimConfigGroup());
-
-		config.plans().setInputFile("../shared-svn/projects/episim/matsim-files/snz/Berlin/episim-input/be_entirePopulation_noPlans_withDistricts.xml.gz");
+		
+		config.plans().setInputFile("../shared-svn/projects/episim/matsim-files/snz/BerlinV2/episim-input/be_v2_snz_entirePopulation_emptyPlans_withDistricts.xml.gz");
+//		config.plans().setInputFile("../shared-svn/projects/episim/matsim-files/snz/Berlin/episim-input/be_entirePopulation_noPlans_withDistricts.xml.gz");
 //		config.plans().setInputFile("../shared-svn/projects/episim/matsim-files/snz/Munich/episim-input/mu_entirePopulation_noPlans.xml.gz");
 //		config.plans().setInputFile("../shared-svn/projects/episim/matsim-files/snz/Heinsberg/Heinsberg/episim-input/he_entirePopulation_noPlans.xml.gz");
 //		config.plans().setInputFile("./output/filteredPopulation.xml.gz");
@@ -84,69 +85,73 @@ public class SnzScenario extends AbstractModule {
 
 //		episimConfig.setInputEventsFile("../shared-svn/projects/episim/matsim-files/snz/Heinsberg/Heinsberg/episim-input/he_events_total.xml.gz");
 //		episimConfig.setInputEventsFile("../shared-svn/projects/episim/matsim-files/snz/Munich/episim-input/mu_snz_episim_events.xml.gz");
-		episimConfig.setInputEventsFile("../shared-svn/projects/episim/matsim-files/snz/Berlin/episim-input/be_snz_episim_events.xml.gz");
+//		episimConfig.setInputEventsFile("../shared-svn/projects/episim/matsim-files/snz/Berlin/episim-input/be_snz_episim_events.xml.gz");
+		episimConfig.setInputEventsFile("../shared-svn/projects/episim/matsim-files/snz/BerlinV2/episim-input/be_v2_snz_episim_events.xml.gz");
+
 //		episimConfig.setInputEventsFile("/Users/sebastianmuller/Documents/episim-original-data/20200410_CH2019.25pct.run1.output_events.reducedForEpisim.xml.gz");
 
 
 		episimConfig.setFacilitiesHandling(EpisimConfigGroup.FacilitiesHandling.snz);
 
 		episimConfig.setSampleSize(0.25);
-		episimConfig.setCalibrationParameter(0.000002);
-		episimConfig.setInitialInfections(5);
+		episimConfig.setCalibrationParameter(0.000_001_7);
+		episimConfig.setInitialInfections(50);
+		episimConfig.setInitialInfectionDistrict("Berlin");
 		addParams(episimConfig);
 
 		setContactIntensities(episimConfig);
 		int offset = 0;
 
 		episimConfig.setPolicy(FixedPolicy.class, FixedPolicy.config()
-				.restrict(20 + offset, 0.95, "work")
-				.restrict(22 + offset, 0.9, "work")
-				.restrict(23 + offset, 0.85, "work")
-				.restrict(25 + offset, 0.8, "work")
-				.restrict(26 + offset, 0.65, "work")
-				.restrict(27 + offset, 0.6, "work")
-				.restrict(28 + offset, 0.55, "work")
-				.restrict(31 + offset, 0.5, "work")
-				.restrict(33 + offset, 0.45, "work")
-				.restrict(51 + offset, 0.4, "work")
-				.restrict(15 + offset, 0.95, "shopping", "errands", "business")
-				.open(17 + offset, "shopping", "errands", "business")
-				.restrict(19 + offset, 0.95, "shopping", "errands", "business")
-				.open(20 + offset, "shopping", "errands", "business")
-				.restrict(26 + offset, 0.95, "shopping", "errands", "business")
-				.restrict(27 + offset, 0.85, "shopping", "errands", "business")
-				.restrict(28 + offset, 0.75, "shopping", "errands", "business")
-				.restrict(29 + offset, 0.7, "shopping", "errands", "business")
-				.restrict(31 + offset, 0.65, "shopping", "errands", "business")
-				.restrict(33 + offset, 0.6, "shopping", "errands", "business")
-				.restrict(35 + offset, 0.65, "shopping", "errands", "business")
-				.restrict(36 + offset, 0.6, "shopping", "errands", "business")
-				.restrict(40 + offset, 0.65, "shopping", "errands", "business")
-				.restrict(45 + offset, 0.7, "shopping", "errands", "business")
-				.restrict(46 + offset, 0.75, "shopping", "errands", "business")
-				.restrict(48 + offset, 0.8, "shopping", "errands", "business")
-				.restrict(49 + offset, 0.85, "shopping", "errands", "business")
-				.restrict(51 + offset, 0.6, "shopping", "errands", "business")
-				.restrict(18 + offset, 0.94, "leisure")
-				.restrict(19 + offset, 0.87, "leisure")
-				.restrict(20 + offset, 0.81, "leisure")
-				.restrict(21 + offset, 0.74, "leisure")
-				.restrict(22 + offset, 0.68, "leisure")
-				.restrict(23 + offset, 0.61, "leisure")
-				.restrict(24 + offset, 0.55, "leisure")
-				.restrict(25 + offset, 0.49, "leisure")
-				.restrict(26 + offset, 0.42, "leisure")
-				.restrict(27 + offset, 0.36, "leisure")
-				.restrict(28 + offset, 0.29, "leisure")
-				.restrict(29 + offset, 0.23, "leisure")
-				.restrict(30 + offset, 0.16, "leisure")
-				.restrict(31 + offset, 0.1, "leisure")
-				.restrict(23 + offset, 0.1, "educ_primary", "educ_kiga")
-				.restrict(23 + offset, 0., "educ_secondary", "educ_higher")
+				.restrict(20 - offset, 0.95, "work")
+				.restrict(22 - offset, 0.9, "work")
+				.restrict(23 - offset, 0.85, "work")
+				.restrict(25 - offset, 0.8, "work")
+				.restrict(26 - offset, 0.65, "work")	
+				.restrict(27 - offset, 0.6, "work")
+				.restrict(28 - offset, 0.55, "work")
+				.restrict(31 - offset, 0.5, "work")
+				.restrict(33 - offset, 0.45, "work")
+				.restrict(15 - offset, 0.95, "shopping", "errands", "business")
+				.open(17 - offset, "shopping", "errands", "business")
+				.restrict(19 - offset, 0.95, "shopping", "errands", "business")
+				.open(20 - offset, "shopping", "errands", "business")
+				.restrict(26 - offset, 0.95, "shopping", "errands", "business")
+				.restrict(27 - offset, 0.85, "shopping", "errands", "business")
+				.restrict(28 - offset, 0.75, "shopping", "errands", "business")
+				.restrict(29 - offset, 0.7, "shopping", "errands", "business")
+				.restrict(31 - offset, 0.65, "shopping", "errands", "business")
+				.restrict(33 - offset, 0.6, "shopping", "errands", "business")
+				.restrict(35 - offset, 0.65, "shopping", "errands", "business")
+				.restrict(36 - offset, 0.6, "shopping", "errands", "business")
+				.restrict(40 - offset, 0.65, "shopping", "errands", "business")
+				.restrict(45 - offset, 0.7, "shopping", "errands", "business")
+				.restrict(46 - offset, 0.75, "shopping", "errands", "business")
+				.restrict(48 - offset, 0.8, "shopping", "errands", "business")
+				.restrict(49 - offset, 0.85, "shopping", "errands", "business")
+				.restrict(50 - offset, 0.75, "shopping", "errands", "business")
+				.restrict(55 - offset, 0.7, "shopping", "errands", "business")
+				.restrict(18 - offset, 0.94, "leisure")
+				.restrict(19 - offset, 0.87, "leisure")
+				.restrict(20 - offset, 0.81, "leisure")
+				.restrict(21 - offset, 0.74, "leisure")
+				.restrict(22 - offset, 0.68, "leisure")
+				.restrict(23 - offset, 0.61, "leisure")
+				.restrict(24 - offset, 0.55, "leisure")
+				.restrict(25 - offset, 0.49, "leisure")
+				.restrict(26 - offset, 0.42, "leisure")
+				.restrict(27 - offset, 0.36, "leisure")
+				.restrict(28 - offset, 0.29, "leisure")
+				.restrict(29 - offset, 0.23, "leisure")
+				.restrict(30 - offset, 0.16, "leisure")
+				.restrict(31 - offset, 0.1, "leisure")
+				.restrict(23 - offset, 0.1, "educ_primary", "educ_kiga")
+				.restrict(23 - offset, 0., "educ_secondary", "educ_higher")
+				.restrict(74 - offset, 0.5, "educ_primary", "educ_kiga")
 				.build()
 		);
 
-		config.controler().setOutputDirectory("./output-belin-" + offset);
+		config.controler().setOutputDirectory("./output-belinV2-base-google-" + offset);
 
 		return config;
 	}
