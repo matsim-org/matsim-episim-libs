fbd55e596bc9da555c867138104dce6919df8e3a
smuellervsp <mueller@vsp.tu-berlin.de>
2020-05-02 10:08:25 +0200

    shedding and intake were inverted

--- src/main/java/org/matsim/episim/model/FaceMask.java
+++ src/main/java/org/matsim/episim/model/FaceMask.java
@@ -7,9 +7,9 @@ public enum FaceMask {
 
 	// Mask types need to be order by effectiveness
 	NONE(1d, 1d),
-	CLOTH(0.5, 0.6),
-	SURGICAL(0.2, 0.3),
-	N95(0.025, 0.15);
+	CLOTH(0.6, 0.5),
+	SURGICAL(0.3, 0.2),
+	N95(0.15, 0.025);
 
 	public final double shedding;
 	public final double intake;
