255b3481dde237dfd3324f5add4a186da5c0a965
smuellervsp <mueller@vsp.tu-berlin.de>
2020-05-15 10:56:58 +0200

    switch on restrictions

--- src/main/java/org/matsim/run/modules/SnzBerlinScenario25pct2020.java
+++ src/main/java/org/matsim/run/modules/SnzBerlinScenario25pct2020.java
@@ -41,21 +41,21 @@ public class SnzBerlinScenario25pct2020 extends AbstractSnzScenario2020 {
 	 */
 	public static FixedPolicy.ConfigBuilder basePolicy() {
 
-		FixedPolicy.ConfigBuilder builder = FixedPolicy.config();
-//				.interpolate("2020-03-06", "2020-03-13", Restriction.of(1), 0.8, "work")
-//				.interpolate("2020-03-13", "2020-03-20", Restriction.of(0.8), 0.5, "work")
-//				.interpolate("2020-03-20", "2020-03-27", Restriction.of(0.5), 0.45, "work")
-//				.interpolate("2020-04-05", "2020-04-19", Restriction.of(0.45), 0.55, "work")
-//
-//				.interpolate("2020-03-13", "2020-03-27", Restriction.of(1), 0.1, "leisure", "visit")
-//
-//				.interpolate("2020-02-28", "2020-03-06", Restriction.of(1), 0.95, "shop_daily", "shop_other", "errands", "business")
-//				.interpolate("2020-03-06", "2020-03-13", Restriction.of(0.95), 0.85, "shop_daily", "shop_other", "errands", "business")
-//				.interpolate("2020-03-13", "2020-03-20", Restriction.of(0.85), 0.4, "shop_daily", "shop_other", "errands", "business")
-//
-//				//saturday 14th of march, so the weekend before schools got closed..
-//				.restrict("2020-03-14", 0.1, "educ_primary", "educ_kiga") 
-//				.restrict("2020-03-14", 0., "educ_secondary", "educ_higher", "educ_tertiary", "educ_other");
+		FixedPolicy.ConfigBuilder builder = FixedPolicy.config()
+				.interpolate("2020-03-06", "2020-03-13", Restriction.of(1), 0.8, "work")
+				.interpolate("2020-03-13", "2020-03-20", Restriction.of(0.8), 0.5, "work")
+				.interpolate("2020-03-20", "2020-03-27", Restriction.of(0.5), 0.45, "work")
+				.interpolate("2020-04-05", "2020-04-19", Restriction.of(0.45), 0.55, "work")
+
+				.interpolate("2020-03-13", "2020-03-27", Restriction.of(1), 0.1, "leisure", "visit")
+
+				.interpolate("2020-02-28", "2020-03-06", Restriction.of(1), 0.95, "shop_daily", "shop_other", "errands", "business")
+				.interpolate("2020-03-06", "2020-03-13", Restriction.of(0.95), 0.85, "shop_daily", "shop_other", "errands", "business")
+				.interpolate("2020-03-13", "2020-03-20", Restriction.of(0.85), 0.4, "shop_daily", "shop_other", "errands", "business")
+
+				//saturday 14th of march, so the weekend before schools got closed..
+				.restrict("2020-03-14", 0.1, "educ_primary", "educ_kiga") 
+				.restrict("2020-03-14", 0., "educ_secondary", "educ_higher", "educ_tertiary", "educ_other");
 
 		return builder;
 	}
@@ -80,7 +80,7 @@ public class SnzBerlinScenario25pct2020 extends AbstractSnzScenario2020 {
 		episimConfig.setStartDate("2020-02-18");
 		episimConfig.setPolicy(FixedPolicy.class, basePolicy().build());
 
-		config.controler().setOutputDirectory("./output-berlin-25pct-" + episimConfig.getCalibrationParameter());
+		config.controler().setOutputDirectory("./output-berlin-25pct-restricts-" + episimConfig.getStartDate());
 
 
 		return config;
