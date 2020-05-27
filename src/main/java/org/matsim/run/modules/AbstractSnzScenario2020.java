4ef795ae49ead7b41e875be2fd45a11717417c9e
Christian Rakow <rakow@vsp.tu-berlin.de>
2020-05-27 10:40:36 +0200

    throw exception if container params would be accidentally overwritten

--- src/main/java/org/matsim/run/modules/AbstractSnzScenario2020.java
+++ src/main/java/org/matsim/run/modules/AbstractSnzScenario2020.java
@@ -97,22 +97,22 @@ public abstract class AbstractSnzScenario2020 extends AbstractModule {
 
 		return config;
 	}
-	
+
 	@Provides
 	@Singleton
 	public ProgressionModel progressionModel(SplittableRandom rnd, EpisimConfigGroup episimConfig, TracingConfigGroup tracingConfig) {
 		// WARNING: This does not affect runs with --config file, especially batch runs !!
 
 		AgeDependentProgressionModel defaultProgressionModel = new AgeDependentProgressionModel(rnd, episimConfig, tracingConfig);
-		
+
 		defaultProgressionModel.setTransition( DiseaseStatus.infectedButNotContagious, logNormalWithMedianAndStd( 3., 3. ) );
 		defaultProgressionModel.setTransition( DiseaseStatus.contagious, logNormalWithMedianAndStd( 1.5, 1.5 ) );
 		defaultProgressionModel.setTransition( DiseaseStatus.showingSymptoms, logNormalWithMedianAndStd( 8, 0 ) );
 		defaultProgressionModel.setTransition( DiseaseStatus.seriouslySick, logNormalWithMedianAndStd( 2, 0 ) );
 		defaultProgressionModel.setTransition( DiseaseStatus.critical, logNormalWithMedianAndStd( 10, 0 ) );
-		
+
 		return defaultProgressionModel;
-		
+
 	}
 
 
