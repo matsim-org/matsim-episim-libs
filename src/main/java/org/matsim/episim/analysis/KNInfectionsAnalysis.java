9289031b556854060b88d00112aa3182521c194e
kainagel <nagel@vsp.tu-berlin.de>
2020-06-16 09:49:20 +0200

    changes in kn scripts

--- src/main/java/org/matsim/episim/analysis/KNInfectionsAnalysis.java
+++ src/main/java/org/matsim/episim/analysis/KNInfectionsAnalysis.java
@@ -3,14 +3,9 @@ package org.matsim.episim.analysis;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.log4j.Logger;
-import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.scenario.ScenarioUtils;
 import tech.tablesaw.api.*;
-import tech.tablesaw.columns.numbers.NumberRollingColumn;
 import tech.tablesaw.plotly.Plot;
 import tech.tablesaw.plotly.components.Axis;
 import tech.tablesaw.plotly.components.Figure;
@@ -23,13 +18,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.util.function.Function;
 
 class KNInfectionsAnalysis{
 	private static final Logger log = Logger.getLogger( KNInfectionsAnalysis.class );
 
-	private static final String base = "../shared-svn/projects/episim/matsim-files/bmbf6/20200615-runs//tracing-inf-noSchools/";
+	//	private static final String base = "../shared-svn/projects/episim/matsim-files/bmbf6/20200615-runs/tracing-30-SchoolsAfterSummer/";
+	//	private static final String policy = "../shared-svn/projects/episim/matsim-files/bmbf6/20200615-runs/tracing-30-noSchools/";
+
+	private static final String base = "../shared-svn/projects/episim/matsim-files/bmbf6/20200615-runs/tracing-30-noSchools/";
+	private static final String policy = "../shared-svn/projects/episim/matsim-files/bmbf6/20200615-runs/tracing-inf-noSchools/";
 
 	private static class PersonInfo{
 		private final LocalDate infectionDay;
@@ -58,34 +57,99 @@ class KNInfectionsAnalysis{
 //			}
 //		}
 
-		// ---
-		Reader in = new FileReader( base + "infectionEvents.txt" );
-		Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().withDelimiter( '\t' ).parse( in );
-		Map<String, PersonInfo> map = new LinkedHashMap<>();
-		for( CSVRecord record : records ){
-			LocalDate date = LocalDate.parse( record.get("date") );
-			map.put( record.get( "infected" ), new PersonInfo( date ) );
+		// ===
+		DateColumn xx;
+		DoubleColumn yy;
+		{
+			Map<String, PersonInfo> map = new LinkedHashMap<>();
+			{
+				Reader in = new FileReader( base + "infectionEvents.txt" );
+				Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().withDelimiter( '\t' ).parse( in );
+				for( CSVRecord record : records ){
+					LocalDate date = LocalDate.parse( record.get( "date" ) );
+					map.put( record.get( "infected" ), new PersonInfo( date ) );
 
-			PersonInfo info = map.get( record.get("infector") );
-			if ( info != null ){
-				info.addInfection( date );
+					PersonInfo info = map.get( record.get( "infector" ) );
+					if( info != null ){
+						info.addInfection( date );
+					}
+				}
 			}
+			Map<LocalDate, Long> sums = new TreeMap<>();
+			Map<LocalDate, Long> cnts = new TreeMap<>();
+			{
+				for( PersonInfo personInfo : map.values() ){
+					Long sum = sums.get( personInfo.getInfectionDay() );
+					Long cnt = cnts.get( personInfo.getInfectionDay() );
+					if( sum == null ){
+						sums.put( personInfo.getInfectionDay(), (long) personInfo.infections.size() );
+						cnts.put( personInfo.getInfectionDay(), 1L );
+					} else{
+						sums.put( personInfo.getInfectionDay(), sum + personInfo.infections.size() );
+						cnts.put( personInfo.getInfectionDay(), cnt + 1 );
+					}
+				}
+			}
+			for ( int ii=0 ; ii<=365*2; ii++ ) {
+				LocalDate date = LocalDate.of( 2020, 2, 15).plusDays( ii );
+				sums.putIfAbsent( date, 1L );
+				cnts.putIfAbsent( date, 1L );
+			}
+
+
+			xx = DateColumn.create( "day", sums.keySet() );
+			DoubleColumn sss = DoubleColumn.create( "sss", sums.values() );
+			DoubleColumn ccc = DoubleColumn.create( "ccc", cnts.values() );
+			yy = sss.divide( ccc ).rolling( 28 ).mean();
+//			trace = ScatterTrace.builder( xx2,yy2 ).name("rolling").build();
+		}
+		// ===
+		DoubleColumn yy2;
+		{
+			Map<String, PersonInfo> map2 = new LinkedHashMap<>();
+			{
+				Reader in = new FileReader( policy + "infectionEvents.txt" );
+				Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().withDelimiter( '\t' ).parse( in );
+				for( CSVRecord record : records ){
+					LocalDate date = LocalDate.parse( record.get( "date" ) );
+					map2.put( record.get( "infected" ), new PersonInfo( date ) );
+
+					PersonInfo info = map2.get( record.get( "infector" ) );
+					if( info != null ){
+						info.addInfection( date );
+					}
+				}
+			}
+			Map<LocalDate, Long> sums = new TreeMap<>();
+			Map<LocalDate, Long> cnts = new TreeMap<>();
+			{
+				for( PersonInfo personInfo : map2.values() ){
+					Long sum = sums.get( personInfo.getInfectionDay() );
+					Long cnt = cnts.get( personInfo.getInfectionDay() );
+					if( sum == null ){
+						sums.put( personInfo.getInfectionDay(), (long) personInfo.infections.size() );
+						cnts.put( personInfo.getInfectionDay(), 1L );
+					} else{
+						sums.put( personInfo.getInfectionDay(), sum + personInfo.infections.size() );
+						cnts.put( personInfo.getInfectionDay(), cnt + 1 );
+					}
+				}
+			}
+			for ( int ii=0 ; ii<=365*2; ii++ ) {
+				LocalDate date = LocalDate.of( 2020, 2, 15).plusDays( ii );
+				sums.putIfAbsent( date, 1L );
+				cnts.putIfAbsent( date, 1L );
+			}
+
+			DateColumn xx2 = DateColumn.create( "day", sums.keySet() );
+			DoubleColumn sss = DoubleColumn.create( "sss", sums.values() );
+			DoubleColumn ccc = DoubleColumn.create( "ccc", cnts.values() );
+			yy2 = sss.divide( ccc ).rolling( 28 ).mean();
+//			trace = ScatterTrace.builder( xx2,yy2 ).name("rolling").build();
 		}
 
-		// ---
-		Map<LocalDate,Long> sums = new TreeMap<>();
-		Map<LocalDate,Long> cnts = new TreeMap<>();
-		for( PersonInfo personInfo : map.values() ){
-			Long sum = sums.get( personInfo.getInfectionDay() );
-			Long cnt = cnts.get( personInfo.getInfectionDay() );
-			if  ( sum==null ) {
-				sums.put( personInfo.getInfectionDay(), (long) personInfo.infections.size() );
-				cnts.put( personInfo.getInfectionDay(), 1L );
-			} else{
-				sums.put( personInfo.getInfectionDay(), sum + personInfo.infections.size() );
-				cnts.put( personInfo.getInfectionDay(), cnt + 1 ) ;
-			}
-		}
+		Trace trace = ScatterTrace.builder( xx, yy2.divide(yy) ).build();
+
 
 //		Map<LocalDate,Double> average = new TreeMap<>();
 //		double ssum = 0; ;
@@ -109,19 +173,12 @@ class KNInfectionsAnalysis{
 //		DoubleColumn yy = DoubleColumn.create( "sums", average.values() );
 //		Trace trace = ScatterTrace.builder( xx, yy ).name( "av" ).build();
 
-		DateColumn xx2 = DateColumn.create( "day", sums.keySet() );
-		DoubleColumn sss = DoubleColumn.create( "sss", sums.values() );
-		DoubleColumn ccc = DoubleColumn.create( "ccc", cnts.values() );
-		DoubleColumn yy2 = sss.divide( ccc ).rolling( 28 ).mean();
-		Trace trace2 = ScatterTrace.builder( xx2,yy2 ).name("rolling").build();
 
-		Figure figure = Figure.builder().addTraces( trace2 ).build();
+		Figure figure = Figure.builder().addTraces( trace ).build();
 
-		figure.setLayout( Layout.builder().xAxis(
-				Axis.builder().type( Axis.Type.DATE ).range( LocalDate.of(2020,2,15),LocalDate.of( 2021, 2, 15) ).build()
-							).yAxis(
-				Axis.builder().type( Axis.Type.LINEAR ).range( 0.5, 1.6 ).build()
-							       ).width( 1000 ).build() );
+		figure.setLayout( Layout.builder()
+					.xAxis( Axis.builder().type( Axis.Type.DATE ).range( LocalDate.of(2020,2,15),LocalDate.of( 2021, 2, 15) ).build() )
+					.yAxis( Axis.builder().type( Axis.Type.LINEAR ).range( 0.5, 1.2 ).build() ).width( 1000 ).build() );
 
 		Plot.show(figure, "dada", new File("output3.html") );
 
