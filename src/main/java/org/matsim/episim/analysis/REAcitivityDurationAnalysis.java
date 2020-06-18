900d65eb99dfff80f7a0bf342f6eaf3c403658dd
rewert <ewert@vsp.tu-berlin.de>
2020-06-18 16:45:37 +0200

    add district lookup

--- src/main/java/org/matsim/episim/analysis/REAcitivityDurationAnalysis.java
+++ src/main/java/org/matsim/episim/analysis/REAcitivityDurationAnalysis.java
@@ -23,9 +23,12 @@ package org.matsim.episim.analysis;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.population.PopulationUtils;
 
 /**
  * Class counts the persons in the events starting and ending an activity type
@@ -38,12 +41,21 @@ public class REAcitivityDurationAnalysis {
 
 	private final static Map<String, HashMap<String, Double>> activityStartMap = new HashMap<>();
 	private final static Map<String, HashMap<String, Double>> activityEndMap = new HashMap<>();
+	private static Population population;
 
 	public static void main(String[] args) {
 
-		String inputFile = "../shared-svn/projects/episim/matsim-files/snz/BerlinV2/episim-input/be_2020_snz_episim_events_25pt.xml.gz";
+		String inputFileEvents = "../shared-svn/projects/episim/matsim-files/snz/BerlinV2/episim-input/be_2020_snz_episim_events_25pt.xml.gz";
+		String inputFilePop = "../shared-svn/projects/episim/matsim-files/snz/BerlinV2/episim-input/be_2020_snz_entirePopulation_emptyPlans_withDistricts_25pt.xml.gz";
 //		String inputFile = "../shared-svn/projects/episim/matsim-files/snz/BerlinV2/episim-input/be_2020_snz_episim_events_100pt.xml.gz";
 
+		population = PopulationUtils.readPopulation(inputFilePop);
+		int countPersons = 0;
+		for (Person person : population.getPersons().values()) {
+			if (person.getAttributes().getAttribute("district")!= null && person.getAttributes().getAttribute("district").toString().equals("Berlin"))
+				countPersons++;
+		}
+		
 		EventsManager events = EventsUtils.createEventsManager();
 
 		REActivityDurationEventHandler reActivityDurationEventHandler = new REActivityDurationEventHandler();
@@ -52,28 +64,33 @@ public class REAcitivityDurationAnalysis {
 
 		MatsimEventsReader reader = new MatsimEventsReader(events);
 
-		reader.readFile(inputFile);
+		reader.readFile(inputFileEvents);
 
-		for (String a : activityStartMap.keySet()) {
-			System.out.println(a + ";" + activityStartMap.get(a).size() * 4);
-		}
-		System.out.println("");
-		for (String a : activityEndMap.keySet()) {
+		for (String activityType : activityEndMap.keySet()) {
 			double sumDurations = 0;
-			for (String personId : activityEndMap.get(a).keySet()) {
-				//if activities has started but not finished, the end of the day will be used as the endTime
-				if (activityStartMap.get(a).containsKey(personId)) {
-					sumDurations = sumDurations + activityEndMap.get(a).get(personId) + (86400.
-							- activityStartMap.get(a).get(personId));
+			for (String personId : activityEndMap.get(activityType).keySet()) {
+				/*
+				 * if activities has started but not finished, the end of the day will be used
+				 * as the endTime
+				 */
+				if (activityStartMap.get(activityType).containsKey(personId)) {
+					sumDurations = sumDurations + activityEndMap.get(activityType).get(personId)
+							+ (86400. - activityStartMap.get(activityType).get(personId));
 				} else
-					sumDurations = sumDurations + activityEndMap.get(a).get(personId);
+					sumDurations = sumDurations + activityEndMap.get(activityType).get(personId);
 			}
-			System.out.println(a + ";" + activityEndMap.get(a).size() * 4 + ";"
-					+ (sumDurations / activityEndMap.get(a).size() / 3600));
+			System.out.println(activityType + ";" + activityEndMap.get(activityType).size() * 4 + ";"
+					+ (sumDurations / activityEndMap.get(activityType).size() / 3600));
 		}
+
+		
+		System.out.println("Number of persons in population in Berlin: " + countPersons * 4);
 
 	}
 
+	static Population getPopulation() {
+		return population;
+	}
 	static Map<String, HashMap<String, Double>> getStartMap() {
 		return activityStartMap;
 	}
