e01feb832f26ac9938f7ccb7e07de826be49ced7
Christian Rakow <rakow@vsp.tu-berlin.de>
2020-06-30 17:02:13 +0200

    flush events before putting them into snapshot

--- src/main/java/org/matsim/episim/EpisimReporting.java
+++ src/main/java/org/matsim/episim/EpisimReporting.java
@@ -523,20 +523,26 @@ public final class EpisimReporting implements BasicEventHandler, Closeable, Exte
 
 	@Override
 	public void reset(int iteration) {
-
 		this.iteration = iteration;
 
 		if (iteration == 0 || writeEvents == EpisimConfigGroup.WriteEvents.none)
 			return;
 
+		events = IOUtils.getBufferedWriter(eventPath.resolve(String.format("day_%03d.xml.gz", iteration)).toString());
+		writer.append(events, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<events version=\"1.0\">\n");
+	}
+
+
+	/**
+	 * Flush written events.
+	 */
+	void flushEvents() {
 		if (events != null) {
 			writer.append(events, "</events>");
 			writer.close(events);
 		}
-
-		events = IOUtils.getBufferedWriter(eventPath.resolve(String.format("day_%03d.xml.gz", iteration)).toString());
-		writer.append(events, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<events version=\"1.0\">\n");
 	}
+
 
 	@Override
 	public void writeExternal(ObjectOutput out) throws IOException {
--- src/test/java/org/matsim/episim/BenchmarkIteration.java
+++ src/test/java/org/matsim/episim/BenchmarkIteration.java
@@ -22,6 +22,7 @@ public class BenchmarkIteration {
 	private EpisimRunner runner;
 	private InfectionEventHandler handler;
 	private ReplayHandler replay;
+	private EpisimReporting reporting;
 	private int iteration = 1;
 
 	public static void main(String[] args) throws RunnerException {
@@ -47,11 +48,12 @@ public class BenchmarkIteration {
 		runner = injector.getInstance(EpisimRunner.class);
 		replay = injector.getInstance(ReplayHandler.class);
 		handler = injector.getInstance(InfectionEventHandler.class);
+		reporting = injector.getInstance(EpisimReporting.class);
 
 		injector.getInstance(EventsManager.class).addHandler(handler);
 
 		// benchmark with event writing
-		// injector.getInstance(EventsManager.class).addHandler(injector.getInstance(EpisimReporting.class));
+		// injector.getInstance(EventsManager.class).addHandler(reporting);
 
 		handler.init(replay.getEvents());
 	}
@@ -59,7 +61,7 @@ public class BenchmarkIteration {
 	@Benchmark
 	public void iteration() {
 
-		runner.doStep(replay, handler, iteration);
+		runner.doStep(replay, handler, reporting, iteration);
 		iteration++;
 
 	}
