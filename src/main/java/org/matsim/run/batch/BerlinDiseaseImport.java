fe4a4bb48fdfbb0aa67190e561e10477259ca036
smuellervsp <mueller@vsp.tu-berlin.de>
2020-09-28 14:27:16 +0200

    remove code that has moved to SnzBerlinWeekScenario2020Symmetric

--- src/main/java/org/matsim/run/batch/BerlinDiseaseImport.java
+++ src/main/java/org/matsim/run/batch/BerlinDiseaseImport.java
@@ -4,28 +4,21 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import com.google.inject.util.Modules;
 
-import javafx.util.Pair;
 
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.episim.BatchRun;
 import org.matsim.episim.EpisimConfigGroup;
 import org.matsim.episim.TracingConfigGroup;
-import org.matsim.episim.BatchRun.StringParameter;
 import org.matsim.episim.model.AgeDependentInfectionModelWithSeasonality;
 import org.matsim.episim.model.AgeDependentProgressionModel;
 import org.matsim.episim.model.ContactModel;
 import org.matsim.episim.model.InfectionModel;
 import org.matsim.episim.model.OldSymmetricContactModel;
 import org.matsim.episim.model.ProgressionModel;
-import org.matsim.episim.model.SymmetricContactModel;
-import org.matsim.episim.policy.FixedPolicy;
-import org.matsim.episim.policy.Restriction;
-import org.matsim.run.modules.AbstractSnzScenario2020;
 import org.matsim.run.modules.SnzBerlinWeekScenario2020Symmetric;
 
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Nullable;
@@ -123,36 +116,6 @@ public class BerlinDiseaseImport implements BatchRun<BerlinDiseaseImport.Params>
 		episimConfig.setCalibrationParameter(params.calibrationParam);
 //		episimConfig.setCalibrationParameter(6.e-6);
 
-		Map<LocalDate, Integer> importMap = new HashMap<>();
-		double importFactor = 1.;
-		
-		importMap.put(episimConfig.getStartDate(), (int) Math.round(0.9 * importFactor));
-		
-		int importOffset = 0;
-		importMap = interpolateImport(importMap, importFactor, LocalDate.parse("2020-02-24").plusDays(importOffset), 
-				LocalDate.parse("2020-03-09").plusDays(importOffset), 0.9, 23.1);
-		importMap = interpolateImport(importMap, importFactor, LocalDate.parse("2020-03-09").plusDays(importOffset), 
-				LocalDate.parse("2020-03-23").plusDays(importOffset), 23.1, 3.9);
-		importMap = interpolateImport(importMap, importFactor, LocalDate.parse("2020-03-23").plusDays(importOffset), 
-				LocalDate.parse("2020-04-13").plusDays(importOffset), 3.9, 0.1);
-		importMap = interpolateImport(importMap, importFactor, LocalDate.parse("2020-06-08").plusDays(importOffset), 
-				LocalDate.parse("2020-07-13").plusDays(importOffset), 0.1, 2.7);
-		importMap = interpolateImport(importMap, importFactor, LocalDate.parse("2020-07-13").plusDays(importOffset), 
-				LocalDate.parse("2020-08-10").plusDays(importOffset), 2.7, 17.9);
-		importMap = interpolateImport(importMap, importFactor, LocalDate.parse("2020-08-10").plusDays(importOffset), 
-				LocalDate.parse("2020-08-24").plusDays(importOffset), 17.9, 8.6);
-		
-		episimConfig.setInfections_pers_per_day(importMap);
-		
-		FixedPolicy.ConfigBuilder builder = FixedPolicy.parse(episimConfig.getPolicy());
-		double ciCorrection = 1.;
-		builder.restrict("2020-03-07", Restriction.ofCiCorrection(ciCorrection), AbstractSnzScenario2020.DEFAULT_ACTIVITIES);
-		builder.restrict("2020-03-07", Restriction.ofCiCorrection(ciCorrection), "quarantine_home");
-		builder.restrict("2020-03-07", Restriction.ofCiCorrection(ciCorrection), "pt");
-		builder.restrict("2020-08-08", Restriction.ofCiCorrection(ciCorrection * 0.5), "educ_primary", "educ_kiga", "educ_secondary", "educ_higher", "educ_tertiary", "educ_other");
-		
-		episimConfig.setPolicy(FixedPolicy.class, builder.build());
-		
 		TracingConfigGroup tracingConfig = ConfigUtils.addOrGetModule(config, TracingConfigGroup.class);
 		tracingConfig.setTracingCapacity_pers_per_day(Map.of(
 				LocalDate.of(2020, 4, 1), 30,
@@ -167,9 +130,6 @@ public class BerlinDiseaseImport implements BatchRun<BerlinDiseaseImport.Params>
 		@GenerateSeeds(10)
 		long seed;
 		
-//		@Parameter({1.})
-//		private double importFactor;
-		
 		@Parameter({6.e-6, 4.e-6, 2.e-6})
 		private double calibrationParam;
 		
@@ -178,28 +138,7 @@ public class BerlinDiseaseImport implements BatchRun<BerlinDiseaseImport.Params>
 		
 		@IntParameter({100, Integer.MAX_VALUE})
 		private int tracingCapacity;
-		
-//		@Parameter({1.0, 0.9, 0.8, 0.7})
-//		private double ciCorrection;
-
-//		@IntParameter({0})
-//		private int importOffset;
-		
 
 	}
-	
-	private static Map<LocalDate, Integer> interpolateImport(Map<LocalDate, Integer> importMap, double importFactor, LocalDate start, LocalDate end, double a, double b) {
-		
-		int days = end.getDayOfYear() - start.getDayOfYear();
-		
-		for (int i = 1; i<=days; i++) {
-			double fraction = (double) i / days;
-			importMap.put(start.plusDays(i), (int) Math.round(importFactor * (a + fraction * (b-a))));
-		}
-		
-		return importMap;
-		
-	}
-	
 
 }
