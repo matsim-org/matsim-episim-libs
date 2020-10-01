96507d4a1567d7b0605afc575beb83abe2406719
smuellervsp <mueller@vsp.tu-berlin.de>
2020-10-01 14:55:12 +0200

    runs for hanna

--- src/main/java/org/matsim/run/batch/InterventionMix.java
+++ src/main/java/org/matsim/run/batch/InterventionMix.java
@@ -1,23 +1,30 @@
 package org.matsim.run.batch;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import com.google.inject.util.Modules;
+
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.episim.BatchRun;
 import org.matsim.episim.EpisimConfigGroup;
 import org.matsim.episim.TracingConfigGroup;
+import org.matsim.episim.model.AgeDependentInfectionModelWithSeasonality;
+import org.matsim.episim.model.AgeDependentProgressionModel;
+import org.matsim.episim.model.ContactModel;
 import org.matsim.episim.model.FaceMask;
+import org.matsim.episim.model.InfectionModel;
+import org.matsim.episim.model.OldSymmetricContactModel;
+import org.matsim.episim.model.ProgressionModel;
 import org.matsim.episim.policy.FixedPolicy;
 import org.matsim.episim.policy.Restriction;
-import org.matsim.run.modules.SnzBerlinScenario25pct2020;
+
+import org.matsim.run.modules.SnzBerlinWeekScenario2020Symmetric;
 
 import javax.annotation.Nullable;
+
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.Map;
-
-import static org.matsim.run.modules.AbstractSnzScenario2020.DEFAULT_ACTIVITIES;
-
 
 /**
  * This batch is for mixing different intervention strategies.
@@ -27,7 +34,15 @@ public class InterventionMix implements BatchRun<InterventionMix.Params> {
 
 	@Override
 	public AbstractModule getBindings(int id, @Nullable Params params) {
-		return new SnzBerlinScenario25pct2020();
+		return (AbstractModule) Modules.override(new SnzBerlinWeekScenario2020Symmetric(25))
+				.with(new AbstractModule() {
+					@Override
+					protected void configure() {
+						bind(ContactModel.class).to(OldSymmetricContactModel.class).in(Singleton.class);
+						bind(ProgressionModel.class).to(AgeDependentProgressionModel.class).in(Singleton.class);
+						bind(InfectionModel.class).to(AgeDependentInfectionModelWithSeasonality.class).in(Singleton.class);
+					}
+				});
 	}
 
 	@Override
@@ -38,46 +53,29 @@ public class InterventionMix implements BatchRun<InterventionMix.Params> {
 	@Override
 	public Config prepareConfig(int id, Params params) {
 
-		SnzBerlinScenario25pct2020 module = new SnzBerlinScenario25pct2020();
+		SnzBerlinWeekScenario2020Symmetric module = new SnzBerlinWeekScenario2020Symmetric();
 		Config config = module.config();
 
 		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
 		TracingConfigGroup tracingConfig = ConfigUtils.addOrGetModule(config, TracingConfigGroup.class);
 
-		FixedPolicy.ConfigBuilder builder = FixedPolicy.parse(episimConfig.getPolicy());
-
-		// by default no tracing
-		tracingConfig.setPutTraceablePersonsInQuarantineAfterDay(Integer.MAX_VALUE);
-
-		// reset present restrictions
-		builder.clearAfter(params.referenceDate);
-		openRestrictions(builder, params.referenceDate);
+		FixedPolicy.ConfigBuilder builder = FixedPolicy.config();
 
 		config.global().setRandomSeed(params.seed);
+		
+		int restrictionDay = 20;
 
-		LocalDate referenceDate = LocalDate.parse(params.referenceDate);
-
-		builder.restrict(referenceDate, params.edu, "educ_primary", "educ_kiga", "educ_secondary",
+		builder.restrict(restrictionDay, params.edu, "educ_primary", "educ_kiga", "educ_secondary",
 				"educ_higher", "educ_tertiary", "educ_other");
 
-		if (params.mask > 0)
-			builder.restrict(referenceDate, Restriction.ofMask(Map.of(FaceMask.SURGICAL, params.mask)), "pt", "shop_daily", "shop_other");
+		if (params.mask > 0) builder.restrict(restrictionDay, Restriction.ofMask(Map.of(FaceMask.SURGICAL, params.mask)), "pt", "shop_daily", "shop_other");
 
-		if (params.ct > 0) {
-
-			LocalDate warmUp = referenceDate.minusDays(14);
-			long offset = ChronoUnit.DAYS.between(episimConfig.getStartDate(), warmUp) + 1;
-
-			tracingConfig.setPutTraceablePersonsInQuarantineAfterDay((int) Math.max(1, offset));
-			tracingConfig.setTracingProbability(params.ct);
-
-			tracingConfig.setTracingCapacity_pers_per_day(Map.of(
-					warmUp, 0,
-					referenceDate, Integer.MAX_VALUE
-			));
-
-		}
-
+		tracingConfig.setTracingProbability(params.ct);
+		tracingConfig.setPutTraceablePersonsInQuarantineAfterDay(1);
+		tracingConfig.setTracingCapacity_pers_per_day(Map.of(
+				episimConfig.getStartDate(), 0,
+				episimConfig.getStartDate().plusDays(restrictionDay), Integer.MAX_VALUE)
+		);
 		episimConfig.setPolicy(FixedPolicy.class, builder.build());
 
 		return config;
@@ -86,29 +84,29 @@ public class InterventionMix implements BatchRun<InterventionMix.Params> {
 	/**
 	 * Opens all the restrictions
 	 */
-	private FixedPolicy.ConfigBuilder openRestrictions(FixedPolicy.ConfigBuilder builder, String date) {
-		return builder.restrict(date, Restriction.none(), DEFAULT_ACTIVITIES)
-				.restrict(date, Restriction.none(), "pt")
-				.restrict(date, Restriction.none(), "quarantine_home")
-				.restrict(date, Restriction.ofMask(Map.of(FaceMask.CLOTH, 0.,
-						FaceMask.SURGICAL, 0.)), "pt", "shop_daily", "shop_other");
-	}
+//	private FixedPolicy.ConfigBuilder openRestrictions(FixedPolicy.ConfigBuilder builder, String date) {
+//		return builder.restrict(date, Restriction.none(), DEFAULT_ACTIVITIES)
+//				.restrict(date, Restriction.none(), "pt")
+//				.restrict(date, Restriction.none(), "quarantine_home")
+//				.restrict(date, Restriction.ofMask(Map.of(FaceMask.CLOTH, 0.,
+//						FaceMask.SURGICAL, 0.)), "pt", "shop_daily", "shop_other");
+//	}
 
 	public static final class Params {
 
 		@GenerateSeeds(50)
 		long seed;
 
-		@StringParameter({"2020-03-07"})
-		String referenceDate;
-
-		@Parameter({0, 0.5, 1})
+		@Parameter({0.5})
+//		@Parameter({0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.})
 		double edu;
 
-		@Parameter({0, 0.5, 1})
+		@Parameter({0.5})
+//		@Parameter({0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.})
 		double mask;
 
-		@Parameter({0, 0.5, 1})
+//		@Parameter({0.5})
+		@Parameter({0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.})
 		double ct;
 	}
 
