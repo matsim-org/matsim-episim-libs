dfd823c72fbd76c6ff9592461fe632de3b984ece
smuellervsp <mueller@vsp.tu-berlin.de>
2020-10-01 13:31:02 +0200

    reduce output

--- src/main/java/org/matsim/episim/analysis/RValuesFromEvents.java
+++ src/main/java/org/matsim/episim/analysis/RValuesFromEvents.java
@@ -133,16 +133,20 @@ public class RValuesFromEvents implements Callable<Integer> {
 
 
 		BufferedWriter bw = Files.newBufferedWriter(scenario.resolve(id + "infectionsPerActivity.txt"));
-		bw.write("day\tdate\tactivity\tinfections\tscenario");
+		bw.write("day\tdate\tactivity\tinfections\tinfectionsShare\tscenario");
 
 		for (int i = 0; i <= eventFiles.size(); i++) {
 			for (Entry<String, HashMap<Integer, Integer>> e : infHandler.infectionsPerActivity.entrySet()) {
-				//if (e.getKey().equals("pt") || e.getKey().equals("total")|| e.getKey().equals("edu_kiga") || e.getKey().equals("edu_school") || e.getKey().equals("leisure") || e.getKey().equals("work&business") || e.getKey().equals("home") ) {
+				if (!e.getKey().equals("total")) {
 				int infections = 0;
+				double infectionsShare = 0.;
 				if (e.getValue().get(i) != null) infections = e.getValue().get(i);
-				bw.write("\n" + i + "\t" + startDate.plusDays(i).toString() + "\t" + e.getKey() + "\t" + infections + "\t" + scenario.getFileName());
-				infectionsPerActivity.write("\n" + i + "\t" + startDate.plusDays(i).toString() + "\t" + e.getKey() + "\t" + infections + "\t" + scenario.getFileName());
-				//}
+				if (infections != 0) {
+					infectionsShare = (double) infections / infHandler.infectionsPerActivity.get("total").get(i);
+					bw.write("\n" + i + "\t" + startDate.plusDays(i).toString() + "\t" + e.getKey() + "\t" + infections + "\t" + infectionsShare);
+					infectionsPerActivity.write("\n" + i + "\t" + startDate.plusDays(i).toString() + "\t" + e.getKey() + "\t" + infections + "\t" + infectionsShare + "\t" + scenario.getFileName());
+					}
+				}
 			}
 		}
 
@@ -243,13 +247,16 @@ public class RValuesFromEvents implements Callable<Integer> {
 		@Override
 		public void handleEvent(EpisimInfectionEvent event) {
 			String infectionType = event.getInfectionType();
-//			if (infectionType.startsWith("educ_higher")) infectionType = "edu_higher";
-//			if (infectionType.startsWith("educ_other")) infectionType = "edu_other";
-//			if (infectionType.startsWith("educ_kiga")) infectionType = "edu_kiga";
-//			if (infectionType.startsWith("educ_primary") || infectionType.startsWith("educ_secondary") || infectionType.startsWith("educ_tertiary")) infectionType = "edu_school";
-//			if (infectionType.startsWith("leisure")) infectionType = "leisure";
-//			if (infectionType.startsWith("work") || infectionType.startsWith("business")) infectionType = "work&business";
-//			if (infectionType.startsWith("home")) infectionType = "home";
+//			if (infectionType.endsWith("educ_higher")) infectionType = "edu_higher";
+//			else if (infectionType.endsWith("educ_other")) infectionType = "edu_other";
+//			else if (infectionType.endsWith("educ_kiga")) infectionType = "edu_kiga";
+//			else if (infectionType.endsWith("educ_primary") || infectionType.endsWith("educ_secondary") || infectionType.endsWith("educ_tertiary")) infectionType = "edu_school";
+			if (infectionType.endsWith("educ_primary") || infectionType.endsWith("educ_secondary") || infectionType.endsWith("educ_tertiary") || infectionType.endsWith("educ_higher") || infectionType.endsWith("educ_other") || infectionType.endsWith("educ_kiga")) infectionType = "edu";
+			else if (infectionType.endsWith("leisure")) infectionType = "leisure";
+			else if (infectionType.endsWith("work") || infectionType.endsWith("business")) infectionType = "work&business";
+			else if (infectionType.endsWith("home")) infectionType = "home";
+			else if (infectionType.startsWith("pt")) infectionType = "pt";
+			else infectionType = "other";
 
 
 			if (!infectionsPerActivity.containsKey("total")) infectionsPerActivity.put("total", new HashMap<>());
