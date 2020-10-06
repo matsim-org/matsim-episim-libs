63a71b1bff7ff3f565419145787aee0380299dfd
smuellervsp <mueller@vsp.tu-berlin.de>
2020-10-06 14:20:00 +0200

    update restrictions, clean up

--- src/main/java/org/matsim/run/batch/BerlinContactModelsAndInterventions.java
+++ src/main/java/org/matsim/run/batch/BerlinContactModelsAndInterventions.java
@@ -44,42 +44,10 @@ public class BerlinContactModelsAndInterventions implements BatchRun<BerlinConta
 	@Override
 	public Config prepareConfig(int id, Params params) {
 
-		String contactModel = params.contactModel;
-
-		double calibrationParam = 0.;
-
-		// not needed for unrestricted runs
-		String startDate = "2020-02-18";
-		double ciCorrection = 0.;
-		String dateOfCiCorrection = "";
-
 		int nSpaces = 1;
 		boolean withModifiedCi = true;
-
-		if (contactModel.equals(OLD)) {
-			calibrationParam = 0.;
-			startDate = "";
-			startDate = "";
-			dateOfCiCorrection = "";
-			withModifiedCi = false;
-		} else if (contactModel.equals(SYMMETRIC_OLD)) {
-			calibrationParam = 0.;
-			startDate = "";
-			startDate = "";
-			dateOfCiCorrection = "";
-		} else if (contactModel.equals(SYMMETRIC_NEW_NSPACES_1)) {
-			calibrationParam = 0.;
-			startDate = "";
-			startDate = "";
-			dateOfCiCorrection = "";
-		} else if (contactModel.equals(SYMMETRIC_NEW_NSPACES_20)) {
-			calibrationParam = 0.;
-			startDate = "";
-			startDate = "";
-			dateOfCiCorrection = "";
-			nSpaces = 20;
-		} else throw new RuntimeException("contact model not implemented");
-
+		
+		if (params.contactModel.equals(SYMMETRIC_NEW_NSPACES_20)) nSpaces = 20; 
 
 		SnzBerlinWeekScenario2020 module = new SnzBerlinWeekScenario2020(25, false, withModifiedCi, MODELS.get(params.contactModel));
 		Config config = module.config();
@@ -91,8 +59,7 @@ public class BerlinContactModelsAndInterventions implements BatchRun<BerlinConta
 			if (!infParams.includesActivity("home")) infParams.setSpacesPerFacility(nSpaces);
 		}
 
-		episimConfig.setCalibrationParameter(calibrationParam);
-		episimConfig.setStartDate(startDate);
+		episimConfig.setStartDate("2020-02-18");
 
 		TracingConfigGroup tracingConfig = ConfigUtils.addOrGetModule(config, TracingConfigGroup.class);
 
@@ -103,55 +70,45 @@ public class BerlinContactModelsAndInterventions implements BatchRun<BerlinConta
 		{
 			if (params.restriction.startsWith("none") || params.restriction.startsWith("calibr")) ;
 
-			else if (params.restriction.equals("0.9FFP@PT&SHOP"))
-				builder.restrict(20, Restriction.ofMask(FaceMask.N95, 0.9), "pt", "shop_daily", "shop_other");
-
 			else if (params.restriction.equals("0.9CLOTH@PT&SHOP"))
 				builder.restrict(20, Restriction.ofMask(FaceMask.CLOTH, 0.9), "pt", "shop_daily", "shop_other");
-
-			else if (params.restriction.equals("workBusiness50")) builder.restrict(20, 0.5, "work", "business");
-
-			else if (params.restriction.equals("leisure50")) builder.restrict(20, 0.5, "leisure");
-
-			else if (params.restriction.equals("shop50")) builder.restrict(20, 0.5, "shop_daily", "shop_other");
-
-			else if (params.restriction.equals("educ50"))
-				builder.restrict(20, 0.5, "educ_primary", "educ_kiga", "educ_secondary", "educ_higher", "educ_tertiary", "educ_other");
-			else if (params.restriction.equals("educ_kiga50")) builder.restrict(20, 0.5, "educ_kiga");
-			else if (params.restriction.equals("educ_primary50")) builder.restrict(20, 0.5, "educ_primary");
-			else if (params.restriction.equals("educ_secondary50")) builder.restrict(20, 0.5, "educ_secondary");
-			else if (params.restriction.equals("educ_tertiary50")) builder.restrict(20, 0.5, "educ_tertiary");
-			else if (params.restriction.equals("educ_higher50")) builder.restrict(20, 0.5, "educ_higher");
-			else if (params.restriction.equals("educ_other50")) builder.restrict(20, 0.5, "educ_other");
-
-			else if (params.restriction.equals("educ0"))
-				builder.restrict(20, 0., "educ_primary", "educ_kiga", "educ_secondary", "educ_higher", "educ_tertiary", "educ_other");
-			else if (params.restriction.equals("educ_kiga0")) builder.restrict(20, 0., "educ_kiga");
-			else if (params.restriction.equals("educ_primary0")) builder.restrict(20, 0., "educ_primary");
-			else if (params.restriction.equals("educ_secondary0")) builder.restrict(20, 0., "educ_secondary");
-			else if (params.restriction.equals("educ_tertiary0")) builder.restrict(20, 0., "educ_tertiary");
-			else if (params.restriction.equals("educ_higher0")) builder.restrict(20, 0., "educ_higher");
-			else if (params.restriction.equals("educ_other0")) builder.restrict(20, 0., "educ_other");
-
-			else if (params.restriction.equals("0.9FFP@EDU"))
-				builder.restrict(20, Restriction.ofMask(FaceMask.N95, 0.9), "educ_primary", "educ_kiga", "educ_secondary", "educ_higher", "educ_tertiary", "educ_other");
-
+			
 			else if (params.restriction.equals("0.9CLOTH@EDU"))
 				builder.restrict(20, Restriction.ofMask(FaceMask.CLOTH, 0.9), "educ_primary", "educ_kiga", "educ_secondary", "educ_higher", "educ_tertiary", "educ_other");
 
+			else if (params.restriction.equals("workBusiness50")) builder.restrict(20, 0.5, "work", "business");
+			else if (params.restriction.equals("workBusiness0")) builder.restrict(20, 0., "work", "business");
+
+			else if (params.restriction.equals("leisure50")) builder.restrict(20, 0.5, "leisure");
+			else if (params.restriction.equals("leisure0")) builder.restrict(20, 0., "leisure");
+
+			else if (params.restriction.equals("shop50")) builder.restrict(20, 0.5, "shop_daily", "shop_other");
+			else if (params.restriction.equals("shop0")) builder.restrict(20, 0., "shop_daily", "shop_other");
+			
+			else if (params.restriction.equals("educ_kiga50")) builder.restrict(20, 0.5, "educ_kiga");
+			else if (params.restriction.equals("educ_kiga0")) builder.restrict(20, 0., "educ_kiga");
+
+			else if (params.restriction.equals("educ_school50"))
+				builder.restrict(20, 0.5, "educ_primary", "educ_secondary", "educ_tertiary");
+			
+			else if (params.restriction.equals("educ_school0"))
+				builder.restrict(20, 0., "educ_primary", "educ_secondary", "educ_tertiary");
+			
+			else if (params.restriction.equals("educ_higher50")) builder.restrict(20, 0.5, "educ_higher");
+			else if (params.restriction.equals("educ_higher0")) builder.restrict(20, 0., "educ_higher");
+
 			else if (params.restriction.equals("outOfHome50")) builder.restrict(20, 0.5, AbstractSnzScenario2020.DEFAULT_ACTIVITIES);
+			else if (params.restriction.equals("outOfHome0")) builder.restrict(20, 0., AbstractSnzScenario2020.DEFAULT_ACTIVITIES);
 
 			else if (params.restriction.contains("tracing")) {
-				tracingConfig.setPutTraceablePersonsInQuarantineAfterDay(0);
+				tracingConfig.setPutTraceablePersonsInQuarantineAfterDay(1);
 				tracingConfig.setTracingCapacity_pers_per_day(Map.of(
 						episimConfig.getStartDate(), 0,
 						episimConfig.getStartDate().plusDays(20), Integer.MAX_VALUE)
 				);
-
-				if (params.restriction.equals("tracing50")) tracingConfig.setTracingProbability(0.5);
-				else if (params.restriction.equals("tracing75")) tracingConfig.setTracingProbability(0.75);
-				else throw new RuntimeException();
-			} else throw new RuntimeException("Measure not implemented: " + params.restriction);
+				tracingConfig.setTracingProbability(0.75);
+			} 
+			else throw new RuntimeException("Measure not implemented: " + params.restriction);
 		}
 
 		episimConfig.setPolicy(FixedPolicy.class, builder.build());
@@ -164,9 +121,9 @@ public class BerlinContactModelsAndInterventions implements BatchRun<BerlinConta
 		@GenerateSeeds(20)
 		public long seed;
 
-		@StringParameter({"none", "0.9FFP@PT&SHOP", "0.9CLOTH@PT&SHOP", "workBusiness50", "leisure50", "shop50", "educ50", "educ_kiga50",
-				"educ_primary50", "educ_secondary50", "educ_tertiary50", "educ_higher50", "educ_other50", "educ0", "educ_kiga0",
-				"educ_primary0", "educ_secondary0", "educ_tertiary0", "educ_higher0", "educ_other0", "outOfHome50", "0.9FFP@EDU", "0.9CLOTH@EDU", "tracing50", "tracing75"})
+		@StringParameter({"none", "tracing75", "0.9CLOTH@PT&SHOP", "0.9CLOTH@EDU", "workBusiness50", "workBusiness0", "leisure50", "leisure0", "shop50", "shop0", "educ_kiga50", "educ_kiga0",
+			"educ_school50", "educ_school0", "educ_higher50", "educ_higher0", "outOfHome50", "outOfHome0"})
+//		@StringParameter({"calibr"})
 		public String restriction;
 
 		@StringParameter({OLD, SYMMETRIC_OLD, SYMMETRIC_NEW_NSPACES_1, SYMMETRIC_NEW_NSPACES_20, PAIRWISE})
