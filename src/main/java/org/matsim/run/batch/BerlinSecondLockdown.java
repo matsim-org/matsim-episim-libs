56d41f7f0a472e6371a12ea41dc0f25605650513
smuellervsp <mueller@vsp.tu-berlin.de>
2020-11-10 13:47:06 +0100

    runs for bmbf

--- src/main/java/org/matsim/run/batch/BerlinSecondLockdown.java
+++ src/main/java/org/matsim/run/batch/BerlinSecondLockdown.java
@@ -64,16 +64,13 @@ public class BerlinSecondLockdown implements BatchRun<BerlinSecondLockdown.Param
 		
 		ConfigBuilder builder = FixedPolicy.parse(episimConfig.getPolicy());
 		
-		if (params.lockdown.equals("leisure50")) {
-			builder.restrict("2020-11-02", 0.5, "leisure");
-			builder.restrict("2020-11-30", params.fraction, "leisure");
-		}
+		if (params.lockdown.equals("leisure63")) builder.restrict("2020-11-02", 0.63, "leisure");
 		
-		if (params.lockdown.equals("allOutOfHome50")) {
+		
+		if (params.lockdown.equals("outOfHomeExceptEdu59")) {
 			for (String act : AbstractSnzScenario2020.DEFAULT_ACTIVITIES) {
 				if (act.contains("educ")) continue;
-				builder.restrict("2020-11-02", 0.5, act);
-				builder.restrict("2020-11-30", params.fraction, act);
+				builder.restrict("2020-11-02", 0.59, act);
 			}
 		}
 
@@ -81,21 +78,69 @@ public class BerlinSecondLockdown implements BatchRun<BerlinSecondLockdown.Param
 				LocalDate.of(2020, 4, 1), 30,
 				LocalDate.of(2020, 6, 15), 200,
 				LocalDate.parse("2020-11-02"), 400,
-				LocalDate.parse("2020-11-30"), params.tracingCapacity
+				LocalDate.parse("2020-11-23"), params.tracingCapacity
 		));
 		
 		tracingConfig.setTracingDelay_days(Map.of(
 				LocalDate.of(2020, 1, 1), 4,
-				LocalDate.parse("2020-11-30"), params.tracingDelay
+				LocalDate.parse("2020-11-23"), params.tracingDelay
 		));
 		
 		tracingConfig.setTracingProbability(Map.of(
 				LocalDate.of(2020, 1, 1), 0.6,
-				LocalDate.parse("2020-11-30"), params.tracingProbability
+				LocalDate.parse("2020-11-23"), params.tracingProbability
 		));
 
 		
+		if (params.schools.contains("school50")) {
+			builder
+			.restrict("2020-11-23", 0.5, "educ_kiga", "educ_primary", "educ_secondary", "educ_tertiary", "educ_other")
+			//Weihnachtsferien
+			.restrict("2020-12-21", 0.2, "educ_kiga", "educ_primary", "educ_secondary", "educ_tertiary", "educ_other")
+			.restrict("2021-01-03", 0.5, "educ_kiga", "educ_primary", "educ_secondary", "educ_tertiary", "educ_other")
+			//Winterferien
+			.restrict("2021-02-01", 0.2, "educ_primary", "educ_secondary", "educ_tertiary", "educ_other")
+			.restrict("2021-02-07", 0.5, "educ_primary", "educ_secondary", "educ_tertiary", "educ_other")
+			//Osterferien
+			.restrict("2021-03-29", 0.2, "educ_primary", "educ_secondary", "educ_tertiary", "educ_other")
+			.restrict("2021-04-11", 0.5, "educ_primary", "educ_secondary", "educ_tertiary", "educ_other")
+			;
+		}
+		if (params.schools.contains("N95masks")) {
+			builder.restrict("2020-11-23", Restriction.ofMask(FaceMask.N95, 0.9), "educ_kiga", "educ_primary", "educ_secondary", "educ_tertiary", "educ_other", "educ_higher");
+		}
+		
+		if (params.schools.contains("earlyHolidays")) {
+			builder.restrict("2020-11-23", 0., "educ_higher");
+			builder.restrict("2020-11-23", 0.2, "educ_kiga", "educ_primary", "educ_secondary", "educ_tertiary", "educ_other");
+			builder.restrict("2021-01-03", 0.2, "educ_higher");
+		}
+		
+		if (params.furhterMeasures.equals("outOfHomeExceptEdu59")) {
+			for (String act : AbstractSnzScenario2020.DEFAULT_ACTIVITIES) {
+				if (act.contains("educ")) continue;
+				builder.restrict("2020-11-23", 0.59, act);
+			}
+		}
+		
+		if (params.furhterMeasures.equals("leisure10")) {
+			builder.restrict("2020-11-23", 0.1, "leisure");
+		}
+		
+		if (params.furhterMeasures.equals("outOfHomeExceptEdu59&leisure10")) {
+			for (String act : AbstractSnzScenario2020.DEFAULT_ACTIVITIES) {
+				if (act.contains("educ")) continue;
+				if (act.contains("leisure")) continue;
+				builder.restrict("2020-11-23", 0.59, act);
+			}
+			builder.restrict("2020-11-23", 0.1, "leisure");
+		}
+		
 		episimConfig.setPolicy(FixedPolicy.class, builder.build());
+		
+		if (params.lockdown.equals("outOfHomeExceptEdu84") && params.tracingCapacity == 400 && params.tracingProbability == 0.6 && params.tracingDelay == 4 && params.schools.equals("open") && params.furhterMeasures.equals("no")) {
+			episimConfig.setSnapshotInterval(30);
+		}
 
 		return config;
 	}
@@ -105,20 +150,25 @@ public class BerlinSecondLockdown implements BatchRun<BerlinSecondLockdown.Param
 		@GenerateSeeds(1)
 		public long seed;
 		
-		@StringParameter({"leisure50", "allOutOfHome50"})
+		//2.11.
+		@StringParameter({"outOfHomeExceptEdu59", "outOfHomeExceptEdu84", "leisure63"})
 		public String lockdown;
 		
-		@IntParameter({200, 300, 400, Integer.MAX_VALUE})
+		//23.11.
+		@IntParameter({200, 400, Integer.MAX_VALUE})
 		int tracingCapacity;
 		
-		@Parameter({0.6, 0.8, 1.0})
+		@Parameter({0.6, 0.9})
 		double tracingProbability;
 		
-		@IntParameter({2, 3, 4})
+		@IntParameter({2, 4})
 		int tracingDelay;
 		
-		@Parameter({0.5, 0.75, 0.9, 1.0})
-		double fraction;
+		@StringParameter({"open", "school50", "N95masks", "school50&N95masks", "earlyHolidays"})
+		public String schools;
+		
+		@StringParameter({"no", "outOfHomeExceptEdu59", "leisure10", "outOfHomeExceptEdu59&leisure10"})
+		public String furhterMeasures;
 
 	}
 
