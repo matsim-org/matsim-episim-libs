3695ac9a7ef5bc22e73cdda868e3d9ee42630421
smuellervsp <mueller@vsp.tu-berlin.de>
2020-11-16 09:49:13 +0100

    many changes, especially tracing

--- src/main/java/org/matsim/run/batch/BerlinInterventionsOct.java
+++ src/main/java/org/matsim/run/batch/BerlinInterventionsOct.java
@@ -5,22 +5,22 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.episim.BatchRun;
 import org.matsim.episim.EpisimConfigGroup;
-import org.matsim.episim.EpisimConfigGroup.InfectionParams;
 import org.matsim.episim.TracingConfigGroup;
-import org.matsim.episim.BatchRun.Parameter;
 import org.matsim.episim.model.*;
 import org.matsim.episim.policy.FixedPolicy;
-import org.matsim.episim.policy.FixedPolicy.ConfigBuilder;
 import org.matsim.episim.policy.Restriction;
-import org.matsim.run.modules.SnzBerlinWeekScenario2020;
+import org.matsim.run.modules.AbstractSnzScenario2020;
+import org.matsim.run.modules.SnzBerlinProductionScenario;
+import org.matsim.run.modules.SnzBerlinProductionScenario.DiseaseImport;
+import org.matsim.run.modules.SnzBerlinProductionScenario.Masks;
+import org.matsim.run.modules.SnzBerlinProductionScenario.Restrictions;
+import org.matsim.run.modules.SnzBerlinProductionScenario.Snapshot;
+import org.matsim.run.modules.SnzBerlinProductionScenario.Tracing;
 
 import javax.annotation.Nullable;
 
 import java.time.LocalDate;
 import java.util.Map;
-import java.util.TreeMap;
-
-import static org.matsim.run.batch.ContactModels.*;
 
 
 /**
@@ -30,10 +30,8 @@ public class BerlinInterventionsOct implements BatchRun<BerlinInterventionsOct.P
 
 	@Override
 	public AbstractModule getBindings(int id, @Nullable Params params) {
-		if (params == null)
-			return new SnzBerlinWeekScenario2020();
 
-		return new SnzBerlinWeekScenario2020(25, true, true, ContactModels.MODELS.get(params.contactModel));
+		return new SnzBerlinProductionScenario(25, DiseaseImport.yes, Restrictions.yes, Masks.yes, Tracing.yes, Snapshot.episim_snapshot_240_2020_10_12, AgeDependentInfectionModelWithSeasonality.class);
 	}
 
 	@Override
@@ -44,93 +42,97 @@ public class BerlinInterventionsOct implements BatchRun<BerlinInterventionsOct.P
 	@Override
 	public Config prepareConfig(int id, Params params) {
 
-		SnzBerlinWeekScenario2020 module = new SnzBerlinWeekScenario2020(25, true, true, MODELS.get(params.contactModel));
+		SnzBerlinProductionScenario module = new SnzBerlinProductionScenario(25, DiseaseImport.yes, Restrictions.yes, Masks.yes, Tracing.yes, Snapshot.episim_snapshot_240_2020_10_12, AgeDependentInfectionModelWithSeasonality.class);
 		Config config = module.config();
 		config.global().setRandomSeed(params.seed);
 
 		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
+		
+		episimConfig.setSnapshotSeed(EpisimConfigGroup.SnapshotSeed.reseed);
 
 		TracingConfigGroup tracingConfig = ConfigUtils.addOrGetModule(config, TracingConfigGroup.class);
 		
-		tracingConfig.setTracingCapacity_pers_per_day(Map.of(
-				LocalDate.of(2020, 4, 1), 30,
-				LocalDate.of(2020, 6, 15), params.tracingCapacity
-		));
-		
 		FixedPolicy.ConfigBuilder builder = FixedPolicy.parse(episimConfig.getPolicy());
 		
-		//switch off holidays
-		builder.restrict("2020-10-12", 1., "educ_primary", "educ_secondary", "educ_tertiary", "educ_other");
-		LocalDate interventionDate = LocalDate.parse("2020-10-26");
+		if (params.lockdown.equals("outOfHomeExceptEdu59")) {
+			for (String act : AbstractSnzScenario2020.DEFAULT_ACTIVITIES) {
+				if (act.contains("educ")) continue;
+				builder.restrict("2020-11-02", 0.59, act);
+			}
+		}
+
+		LocalDate interventionDate = LocalDate.parse("2020-11-23");
 		builder.clearAfter(interventionDate.toString());
 
 		{
 			if (params.restriction.equals("current"));
-			else if (params.restriction.equals("tracing-inf-75-1d")) {
+			else if (params.restriction.equals("tracing-inf-90-2d")) {
 				tracingConfig.setTracingCapacity_pers_per_day(Map.of(
-						LocalDate.of(2020, 4, 1), 30,
-						LocalDate.of(2020, 6, 15), params.tracingCapacity,
+						LocalDate.of(2020, 4, 1), 300,
+						LocalDate.of(2020, 6, 15), 2000,
 						interventionDate, Integer.MAX_VALUE
 				));
 				tracingConfig.setTracingProbability(Map.of(
-						LocalDate.of(1970, 1, 1), 0.5,
-						interventionDate, 0.75
+						LocalDate.of(1970, 1, 1), 0.6,
+						interventionDate, 0.9
 				));
 				tracingConfig.setTracingDelay_days(Map.of(
-						LocalDate.of(1970, 1, 1), 2,
-						interventionDate, 1
+						LocalDate.of(1970, 1, 1), 4,
+						interventionDate, 2
 				));
 			}
-			else if (params.restriction.equals("tracing-cap-75-1d")) {
+			else if (params.restriction.equals("tracing-cap-90-2d")) {
 				tracingConfig.setTracingProbability(Map.of(
-						LocalDate.of(1970, 1, 1), 0.5,
-						interventionDate, 0.75
+						LocalDate.of(1970, 1, 1), 0.6,
+						interventionDate, 0.9
 				));
 				tracingConfig.setTracingDelay_days(Map.of(
-						LocalDate.of(1970, 1, 1), 2,
-						interventionDate, 1
+						LocalDate.of(1970, 1, 1), 4,
+						interventionDate, 2
 				));
 			}
-			else if (params.restriction.equals("tracing-inf-50-5d")) {
+			else if (params.restriction.equals("tracing-inf-90-4d")) {
 				tracingConfig.setTracingCapacity_pers_per_day(Map.of(
-						LocalDate.of(2020, 4, 1), 30,
-						LocalDate.of(2020, 6, 15), params.tracingCapacity,
+						LocalDate.of(2020, 4, 1), 300,
+						LocalDate.of(2020, 6, 15), 2000,
 						interventionDate, Integer.MAX_VALUE
 				));
-				tracingConfig.setTracingDelay_days(Map.of(
-						LocalDate.of(1970, 1, 1), 2,
-						interventionDate, 5
+				tracingConfig.setTracingProbability(Map.of(
+						LocalDate.of(1970, 1, 1), 0.6,
+						interventionDate, 0.9
+				));
+
+
+			}
+			else if (params.restriction.equals("tracing-cap-90-4d")) {
+				tracingConfig.setTracingProbability(Map.of(
+						LocalDate.of(1970, 1, 1), 0.6,
+						interventionDate, 0.9
 				));
 
 			}
-			else if (params.restriction.equals("tracing-cap-50-5d")) {
+			else if (params.restriction.equals("tracing-inf-60-2d")) {
+				tracingConfig.setTracingCapacity_pers_per_day(Map.of(
+						LocalDate.of(2020, 4, 1), 300,
+						LocalDate.of(2020, 6, 15), 2000,
+						interventionDate, Integer.MAX_VALUE
+				));
 				tracingConfig.setTracingDelay_days(Map.of(
-						LocalDate.of(1970, 1, 1), 2,
-						interventionDate, 5
+						LocalDate.of(1970, 1, 1), 4,
+						interventionDate, 2
 				));
 
 			}
-			else if (params.restriction.equals("tracing-inf-0-2d")) {
-				tracingConfig.setTracingCapacity_pers_per_day(Map.of(
-						LocalDate.of(2020, 4, 1), 30,
-						LocalDate.of(2020, 6, 15), params.tracingCapacity,
-						interventionDate, Integer.MAX_VALUE
-				));
-				tracingConfig.setTracingProbability(Map.of(
-						LocalDate.of(1970, 1, 1), 0.5,
-						interventionDate, 0.
+			else if (params.restriction.equals("tracing-cap-60-2d")) {
+				tracingConfig.setTracingDelay_days(Map.of(
+						LocalDate.of(1970, 1, 1), 4,
+						interventionDate, 2
 				));
 			}
-			else if (params.restriction.equals("tracing-cap-0-2d")) {
-				tracingConfig.setTracingProbability(Map.of(
-						LocalDate.of(1970, 1, 1), 0.5,
-						interventionDate, 0.
-				));
-			}
-			else if (params.restriction.equals("tracing-inf-50-2d")) {
+			else if (params.restriction.equals("tracing-inf-60-4d")) {
 				tracingConfig.setTracingCapacity_pers_per_day(Map.of(
-						LocalDate.of(2020, 4, 1), 30,
-						LocalDate.of(2020, 6, 15), params.tracingCapacity,
+						LocalDate.of(2020, 4, 1), 300,
+						LocalDate.of(2020, 6, 15), 2000,
 						interventionDate, Integer.MAX_VALUE
 				));
 			}
@@ -198,7 +200,7 @@ public class BerlinInterventionsOct implements BatchRun<BerlinInterventionsOct.P
 		public long seed;
 
 		@StringParameter({"current", 
-			"tracing-inf-75-1d", "tracing-cap-75-1d", "tracing-inf-50-5d", "tracing-cap-50-5d", "tracing-inf-0-2d", "tracing-cap-0-2d", "tracing-inf-50-2d",
+			"tracing-inf-60-4d", "tracing-cap-60-2d", "tracing-inf-60-2d", "tracing-cap-90-4d", "tracing-inf-90-4d", "tracing-cap-90-2d", "tracing-inf-90-2d",
 			"schoolClothMasks", "school50", "school0", "school50&ClothMasks", 
 			"universities0",
 			"kiga50", "kiga0",
@@ -210,11 +212,8 @@ public class BerlinInterventionsOct implements BatchRun<BerlinInterventionsOct.P
 			"work75", "work50", "work25", "work0", "workN95Masks"})
 		public String restriction;
 		
-		@StringParameter({SYMMETRIC_OLD})
-		public String contactModel;
-		
-		@IntParameter({100})
-		private int tracingCapacity;
+		@StringParameter({"outOfHomeExceptEdu59", "outOfHomeExceptEdu84"})
+		public String lockdown;
 
 	}
 
