20cc852774a63082e064a26883ef7872262d93be
smuellervsp <mueller@vsp.tu-berlin.de>
2020-11-18 09:29:14 +0100

    update runs for hanna

--- src/main/java/org/matsim/run/batch/InterventionMix.java
+++ src/main/java/org/matsim/run/batch/InterventionMix.java
@@ -1,8 +1,6 @@
 package org.matsim.run.batch;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Singleton;
-import com.google.inject.util.Modules;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.episim.BatchRun;
@@ -11,9 +9,16 @@ import org.matsim.episim.TracingConfigGroup;
 import org.matsim.episim.model.*;
 import org.matsim.episim.policy.FixedPolicy;
 import org.matsim.episim.policy.Restriction;
-import org.matsim.run.modules.SnzBerlinWeekScenario2020;
+import org.matsim.run.modules.SnzBerlinProductionScenario;
+import org.matsim.run.modules.SnzBerlinProductionScenario.DiseaseImport;
+import org.matsim.run.modules.SnzBerlinProductionScenario.Masks;
+import org.matsim.run.modules.SnzBerlinProductionScenario.Restrictions;
+import org.matsim.run.modules.SnzBerlinProductionScenario.Snapshot;
+import org.matsim.run.modules.SnzBerlinProductionScenario.Tracing;
 
 import javax.annotation.Nullable;
+
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -26,7 +31,7 @@ public class InterventionMix implements BatchRun<InterventionMix.Params> {
 
 	@Override
 	public AbstractModule getBindings(int id, @Nullable Params params) {
-		return new SnzBerlinWeekScenario2020(25, false, true, OldSymmetricContactModel.class);
+		return new SnzBerlinProductionScenario(25, DiseaseImport.yes, Restrictions.yes, Masks.yes, Tracing.yes, Snapshot.episim_snapshot_240_2020_10_12, AgeDependentInfectionModelWithSeasonality.class);
 	}
 
 	@Override
@@ -46,35 +51,35 @@ public class InterventionMix implements BatchRun<InterventionMix.Params> {
 		}
 
 
-		SnzBerlinWeekScenario2020 module = new SnzBerlinWeekScenario2020(25, false, true, OldSymmetricContactModel.class);
+		SnzBerlinProductionScenario module = new SnzBerlinProductionScenario(25, DiseaseImport.yes, Restrictions.yes, Masks.yes, Tracing.yes, Snapshot.episim_snapshot_240_2020_10_12, AgeDependentInfectionModelWithSeasonality.class);
 		Config config = module.config();
 
 		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
+		
+		episimConfig.setSnapshotSeed(EpisimConfigGroup.SnapshotSeed.reseed);
+		
 		TracingConfigGroup tracingConfig = ConfigUtils.addOrGetModule(config, TracingConfigGroup.class);
 
 		FixedPolicy.ConfigBuilder builder = FixedPolicy.config();
 
 		config.global().setRandomSeed(params.seed);
 
-		// by default no tracing
-		tracingConfig.setPutTraceablePersonsInQuarantineAfterDay(Integer.MAX_VALUE);
+		String restrictionDay = "2020-10-27";
 
-		int restrictionDay = 20;
+		builder.restrict(restrictionDay, params.edu, "educ_primary", "educ_secondary", "educ_tertiary", "educ_other", "educ_kiga");
 
-		builder.restrict(restrictionDay, params.edu, "educ_primary", "educ_kiga", "educ_secondary",
-				"educ_higher", "educ_tertiary", "educ_other");
-
-		if (params.mask > 0)
-			builder.restrict(restrictionDay, Restriction.ofMask(Map.of(FaceMask.SURGICAL, params.mask)), "pt", "shop_daily", "shop_other");
-
+		if (params.mask > 0) builder.restrict(restrictionDay, Restriction.ofMask(Map.of(FaceMask.SURGICAL, params.mask)), "pt", "shop_daily", "shop_other", "work", "business");
 
 		if (params.ct > 0) {
-			tracingConfig.setTracingProbability(params.ct);
-			tracingConfig.setPutTraceablePersonsInQuarantineAfterDay(1);
+			tracingConfig.setTracingProbability(Map.of(
+					LocalDate.of(1970, 1, 1), 0.6,
+					LocalDate.parse(restrictionDay), params.ct * 0.6
+			));
 			tracingConfig.setTracingCapacity_pers_per_day(Map.of(
-					episimConfig.getStartDate(), 0,
-					episimConfig.getStartDate().plusDays(restrictionDay), Integer.MAX_VALUE)
-			);
+					LocalDate.of(2020, 4, 1), 300,
+					LocalDate.of(2020, 6, 15), 2000,
+					LocalDate.parse(restrictionDay), Integer.MAX_VALUE
+			));
 		}
 
 		episimConfig.setPolicy(FixedPolicy.class, builder.build());
@@ -87,7 +92,7 @@ public class InterventionMix implements BatchRun<InterventionMix.Params> {
 
 	public static final class Params {
 
-		@GenerateSeeds(30)
+		@GenerateSeeds(24)
 		long seed;
 
 		@Parameter({0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.})
