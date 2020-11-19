7d45801cc233bc795dfe4d7972afa0329a0da89c
smuellervsp <mueller@vsp.tu-berlin.de>
2020-11-19 14:20:23 +0100

    add getOutdoorFractionsFromWeatherData method, calibration runs

--- src/main/java/org/matsim/run/batch/SMBatch.java
+++ src/main/java/org/matsim/run/batch/SMBatch.java
@@ -6,6 +6,7 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.episim.BatchRun;
 import org.matsim.episim.EpisimConfigGroup;
+import org.matsim.episim.EpisimUtils;
 import org.matsim.episim.TracingConfigGroup;
 import org.matsim.episim.TracingConfigGroup.CapacityType;
 import org.matsim.run.modules.SnzBerlinWeekScenario2020;
@@ -13,6 +14,7 @@ import org.matsim.run.modules.SnzBerlinProductionScenario;
 
 import javax.annotation.Nullable;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Map;
 
@@ -40,10 +42,18 @@ public class SMBatch implements BatchRun<SMBatch.Params> {
 		Config config = module.config();
 		config.global().setRandomSeed(params.seed);
 
-//		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
+		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
+		episimConfig.setCalibrationParameter(episimConfig.getCalibrationParameter() * params.theta);
+		
+		
+		try {
+			Map<LocalDate, Double> outdoorFractions = EpisimUtils.getOutdoorFractionsFromWeatherData("berlinWeather.csv", params.rainThreshold);
+			episimConfig.setLeisureOutdoorFraction(outdoorFractions);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		TracingConfigGroup tracingConfig = ConfigUtils.addOrGetModule(config, TracingConfigGroup.class);
-		tracingConfig.setCapacityType(CapacityType.PER_CONTACT_PERSON);
 
 		tracingConfig.setTracingCapacity_pers_per_day(Map.of(
 				LocalDate.of(2020, 4, 1), params.tracingCapacityApril,
@@ -55,14 +65,20 @@ public class SMBatch implements BatchRun<SMBatch.Params> {
 
 	public static final class Params {
 
-		@GenerateSeeds(3)
+		@GenerateSeeds(4)
 		public long seed;
 
-		@IntParameter({100, 200, 300, 400})
+		@IntParameter({200, 300, 400})
 		int tracingCapacityApril;
 
-		@IntParameter({500, 1000, 1500, 2000, Integer.MAX_VALUE})
+		@IntParameter({1000, 2000, 3000})
 		int tracingCapacityJune;
+		
+		@Parameter({0.85, 0.9, 0.95, 1.})
+		double theta;
+		
+		@Parameter({1., 2., 3., 4.})
+		double rainThreshold;
 
 	}
 
