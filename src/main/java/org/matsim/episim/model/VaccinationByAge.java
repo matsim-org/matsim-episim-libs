8e72a2566f37ab8e76b8379070f551f2561fe441
smuellervsp <mueller@vsp.tu-berlin.de>
2021-01-11 20:43:06 +0100

    shuffle candidates so they are not sorted by district

--- src/main/java/org/matsim/episim/model/VaccinationByAge.java
+++ src/main/java/org/matsim/episim/model/VaccinationByAge.java
@@ -4,8 +4,8 @@ import com.google.inject.Inject;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.episim.EpisimPerson;
-import org.matsim.episim.EpisimUtils;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -25,9 +25,13 @@ public class VaccinationByAge implements VaccinationModel {
 		List<EpisimPerson> candidates = persons.values().stream()
 				.filter(p -> p.getDiseaseStatus() == EpisimPerson.DiseaseStatus.susceptible)
 				.filter(p -> p.getVaccinationStatus() == EpisimPerson.VaccinationStatus.no)
+				.collect(Collectors.toList());
+		
+		Collections.shuffle(candidates);
+		candidates = candidates.stream()
 				.sorted(Comparator.comparingInt(EpisimPerson::getAge).reversed())
 				.collect(Collectors.toList());
-
+		
 		int vaccinationsLeft = availableVaccinations;
 		int vaccinated = 0;
 		for (int i = 0; i < candidates.size() && vaccinationsLeft > 0; i++) {
