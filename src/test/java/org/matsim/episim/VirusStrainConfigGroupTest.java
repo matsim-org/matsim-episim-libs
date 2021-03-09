4286480ccb7f37ca897e5d4e7cb37a4ed318ebfb
Christian Rakow <rakow@vsp.tu-berlin.de>
2021-03-09 15:25:17 +0100

    added virustrain config group, updated testing config

---
+++ src/test/java/org/matsim/episim/VirusStrainConfigGroupTest.java
@@ -0,0 +1,38 @@
+package org.matsim.episim;
+
+import org.junit.Test;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.episim.model.VirusStrain;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VirusStrainConfigGroupTest {
+
+	@Test
+	public void config() throws IOException {
+
+		VirusStrainConfigGroup group = new VirusStrainConfigGroup();
+
+		Config config = ConfigUtils.createConfig(group);
+
+		File tmp = File.createTempFile("matsim", "config");
+		tmp.deleteOnExit();
+
+
+		group.getOrAddParams(VirusStrain.B117)
+				.setInfectiousness(0.5);
+
+		ConfigUtils.writeConfig(config, tmp.toString());
+
+		VirusStrainConfigGroup copyGroup = new VirusStrainConfigGroup();
+		Config copyConfig = ConfigUtils.loadConfig(tmp.toString(), copyGroup);
+
+		assertThat(group.getParams(VirusStrain.B117).getInfectiousness())
+				.isEqualTo(0.5);
+
+	}
+}
