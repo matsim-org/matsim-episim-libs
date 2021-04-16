078eba0c0d386b03490d789949138797aab13056
smuellervsp <mueller@vsp.tu-berlin.de>
2021-04-16 16:25:53 +0200

    many changes

--- src/main/R/plotBatchRuns.R
+++ src/main/R/plotBatchRuns.R
@@ -2,12 +2,14 @@ library(tidyverse)
 library(lubridate)
 library(gridExtra)
 
+#plots will be saved here
+output <- "~/Desktop/strain_paper_plots/"
 
-batch <- "https://covid-sim.info/2021-03-26/strains/"
+#figure 1 "production model ..."
+batch <- "https://covid-sim.info/2021-04-09/bmbf-2.0"
+rundId <- "testing100001"
 
-rundId <- "20"
-
-path2Zip <- paste0("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/episim/battery/", substr(batch, 24, 1000), "summaries/", rundId, ".zip")
+path2Zip <- paste0("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/episim/battery/", substr(batch, 24, 1000), "/summaries/", rundId, ".zip")
 
 
 temp <- tempfile()
@@ -61,41 +63,62 @@ dateMax = "2021-07-31"
 
 #infections
 ggplot() +
-  geom_point(data=infections, mapping=aes(x = date, y = newShowingSymptoms), color="blue",show.legend = TRUE) +
-  geom_point(data=positiveTests, mapping=aes(x = date, y = mean), color="red",show.legend = TRUE) +
+  labs(
+    caption=paste0("Taken from: ", batch, ". Run Id: ", rundId),
+    x="date", y="cases") +
+  geom_point(data=infections, mapping=aes(x = date, y = newShowingSymptoms), color="blue", show.legend = TRUE) +
+  geom_point(data=positiveTests, mapping=aes(x = date, y = mean), color="red", show.legend = TRUE) +
   xlim(c(as.Date(dateMin), as.Date(dateMax))) +
   ylim(1, 3000) +
   theme(legend.position = "bottom") +
-  scale_y_log10()
+  scale_y_log10() +
+  # No idea how this works, I googled it so we have a legend. - SM
+  scale_color_manual(limits=c("model", "positive tests"), values = c("blue","red")) +
+  guides(colour = guide_legend(override.aes = list(color = c("blue", "red"))))
+
+ggsave(paste0(output, "infections_", rundId, ".png"), width = 10, height = 4, dpi = 300, units = "in")
+
 
 #strains
 strainPlot <- strains %>%
   filter(strain == "SARS_CoV_2" | strain == "B117" | strain == "B1351") %>%
   ggplot(mapping=aes(x = date)) +
+  labs(
+    caption=paste0("Taken from: ", batch, ". Run Id: ", rundId),
+    x="date", y="infections") +
   geom_point(mapping=aes(y = value, colour = strain)) +
   xlim(c(as.Date(dateMin), as.Date(dateMax))) +
   ylim(1, 3000) +
   theme(legend.position = "bottom") +
   scale_y_log10()
 
+ggsave(paste0(output, "strain_", rundId, ".png"), width = 10, height = 4, dpi = 300, units = "in")
 
 strainPlotShare <- strains %>%
   filter(strain == "SARS_CoV_2share" | strain == "B117share" | strain == "B1351share") %>%
   ggplot(mapping=aes(x = date)) +
+  labs(
+    caption=paste0("Taken from: ", batch, ". Run Id: ", rundId),
+    x="date", y="infections share") +
   geom_point(mapping=aes(y = value, colour = strain)) +
   xlim(c(as.Date(dateMin), as.Date(dateMax))) +
   ylim(0, 1) +
   theme(legend.position = "bottom") +
-  scale_y_log10()
+  scale_y_continuous(labels = scales::percent_format())
 
-grid.arrange(strainPlot, strainPlotShare, nrow = 2)
+ggsave(paste0(output, "strainShare_", rundId, ".png"), width = 10, height = 4, dpi = 300, units = "in")
+
+strainPlots <- grid.arrange(strainPlot, strainPlotShare, nrow = 2)
 
 #rValues
 ggplot(data = rValues, mapping=aes(x = date,y = rValue, fill = factor(type, levels=c("pt", "other", "leisure", "day care", "schools", "university", "work&business", "home")))) + 
+  labs(
+    caption=paste0("Taken from: ", batch, ". Run Id: ", rundId),
+    x="date", y="r value") +
   geom_line(mapping=aes(colour=type)) +
-  scale_y_log10() +
   xlim(c(as.Date(dateMin), as.Date(dateMax))) +
-  ylim(0, 1.2) +
+  ylim(0, 1) +
   theme(legend.position = "bottom")
 
-  
+# ggsave(paste0(output, "rValues_", rundId, ".png"), width = 10, height = 4, dpi = 300, units = "in")
+
