178bb24c362796d12fa453cc0c3276b4d4da525a
Jakob Rehmann <jakob.rehmann@gmail.com>
2021-04-20 18:18:50 -0400

    cleaned up zipcode/subdistrict lookup a bit

--- src/main/java/org/matsim/scenarioCreation/ZipcodeAndSubdistrictLookup.java
+++ src/main/java/org/matsim/scenarioCreation/ZipcodeAndSubdistrictLookup.java
@@ -69,15 +69,15 @@ import java.util.concurrent.Callable;
 		description = "Calculate and attach zipcode information to a population.",
 		mixinStandardHelpOptions = true
 )
-public class ZipcodeLookup implements Callable<Integer> {
+public class ZipcodeAndSubdistrictLookup implements Callable<Integer> {
 
-	private static final Logger log = LogManager.getLogger(ZipcodeLookup.class);
+	private static final Logger log = LogManager.getLogger(ZipcodeAndSubdistrictLookup.class);
 
 	@CommandLine.Parameters(paramLabel = "file", arity = "1", description = "Population file", defaultValue = "../shared-svn/projects/episim/matsim-files/snz/BerlinV2/episim-input/be_2020-week_snz_entirePopulation_emptyPlans_withDistricts_25pt_split.xml.gz")
 	//"../shared-svn/projects/episim/matsim-files/snz/Heinsberg/Heinsberg_smallerArea/episim-input/he_small_2020_snz_entirePopulation_noPlans.xml.gz")
 	private Path input;
 
-	@CommandLine.Option(names = "--shp", description = "Shapefile containing district information", defaultValue = "D:/Dropbox/Documents/VSP/plz/plz-gebiete.shp")
+	@CommandLine.Option(names = "--shp", description = "Shapefile containing plz information", defaultValue = "") // D:/Dropbox/Documents/VSP/plz/plz-gebiete.shp
 	private Path shapeFile;
 
 	@CommandLine.Option(names = "--output", description = "Output population file", defaultValue = "../shared-svn/projects/episim/matsim-files/snz/BerlinV2/episim-input/be_2020-week_snz_entirePopulation_emptyPlans_withDistricts_andNeighborhood_25pt_split.xml.gz")
@@ -95,28 +95,26 @@ public class ZipcodeLookup implements Callable<Integer> {
 	private String shapeCRS;
 
 	public static void main(String[] args) {
-		System.exit(new CommandLine(new ZipcodeLookup()).execute(args));
+		System.exit(new CommandLine(new ZipcodeAndSubdistrictLookup()).execute(args));
 	}
 
 	@Override
 	public Integer call() throws Exception {
 
 		//Berlin Neighborhood Zipcodes
-		HashMap<String, IntSet> berlinDistricts = new HashMap<String, IntSet>();
-		berlinDistricts.put("Mitte", new IntOpenHashSet(List.of(10115, 10559, 13355, 10117, 10623, 13357, 10119, 10785, 13359, 10787, 10557, 13353, 10555, 13351, 13349, 10551, 13347)));
-		berlinDistricts.put("Friedrichshain_Kreuzberg", new IntOpenHashSet(List.of(10179, 10967, 10243, 10969, 10245, 10997, 10247, 10999, 12045, 10961, 10963, 10965, 10178)));
-		berlinDistricts.put("Pankow", new IntOpenHashSet(List.of(10249, 10405, 10407, 10409, 10435, 10437, 10439, 13051, 13053, 13086, 13088, 13089, 13125, 13127, 13129, 13156, 13158, 13159, 13187, 13189)));
-		berlinDistricts.put("Charlottenburg_Wilmersdorf", new IntOpenHashSet(List.of(10553, 10585, 10587, 10589, 10625, 10627, 10629, 10707, 10709, 10711, 10713, 10715, 10717, 10719, 10789, 13597, 13627, 14050, 14053, 14055, 14057, 14059)));
-		berlinDistricts.put("Spandau", new IntOpenHashSet(List.of(13581, 13583, 13585, 13587, 13589, 13591, 13593, 13595, 13599, 14052, 14089)));
-		berlinDistricts.put("Steglitz_Zehlendorf", new IntOpenHashSet(List.of(12163, 12165, 12167, 12169, 12203, 12205, 12207, 12209, 12247, 12279, 14109, 14129, 14163, 14165, 14167, 14169, 14193, 14195, 14199)));
-		berlinDistricts.put("Tempelhof_Schoeneberg", new IntOpenHashSet(List.of(10777, 10779, 10781, 10783, 10823, 10825, 10827, 14197, 10829, 12101, 12103, 12105, 12109, 12157, 12159, 12161, 12249, 12277, 12307, 12309)));
-		berlinDistricts.put("Neukoelln", new IntOpenHashSet(List.of(12043, 12047, 12049, 12051, 12053, 12055, 12057, 12059, 12099, 12107, 12305, 12347, 12349, 12351, 12353, 12355, 12357, 12359)));
-		berlinDistricts.put("Treptow_Koepenick", new IntOpenHashSet(List.of(12435, 12437, 12439, 12459, 12487, 12489, 12524, 12526, 12527, 12555, 12557, 12559, 12587, 12589, 12623)));
-		berlinDistricts.put("Marzahn_Hellersdorf", new IntOpenHashSet(List.of(12619, 12621, 12627, 12629, 12679, 12681, 12683, 12685, 12687, 12689)));
-		berlinDistricts.put("Lichtenberg", new IntOpenHashSet(List.of(10315, 13057, 10317, 10318, 10319, 10365, 10367, 10369, 13055, 13059)));
-		berlinDistricts.put("Reinickendorf", new IntOpenHashSet(List.of(13403, 13405, 13407, 13409, 13435, 13437, 13439, 13465, 13467, 13469, 13503, 13505, 13507, 13509, 13629)));
-
-
+		HashMap<String, IntSet> subdistricts = new HashMap<String, IntSet>();
+		subdistricts.put("Mitte", new IntOpenHashSet(List.of(10115, 10559, 13355, 10117, 10623, 13357, 10119, 10785, 13359, 10787, 10557, 13353, 10555, 13351, 13349, 10551, 13347)));
+		subdistricts.put("Friedrichshain_Kreuzberg", new IntOpenHashSet(List.of(10179, 10967, 10243, 10969, 10245, 10997, 10247, 10999, 12045, 10961, 10963, 10965, 10178)));
+		subdistricts.put("Pankow", new IntOpenHashSet(List.of(10249, 10405, 10407, 10409, 10435, 10437, 10439, 13051, 13053, 13086, 13088, 13089, 13125, 13127, 13129, 13156, 13158, 13159, 13187, 13189)));
+		subdistricts.put("Charlottenburg_Wilmersdorf", new IntOpenHashSet(List.of(10553, 10585, 10587, 10589, 10625, 10627, 10629, 10707, 10709, 10711, 10713, 10715, 10717, 10719, 10789, 13597, 13627, 14050, 14053, 14055, 14057, 14059)));
+		subdistricts.put("Spandau", new IntOpenHashSet(List.of(13581, 13583, 13585, 13587, 13589, 13591, 13593, 13595, 13599, 14052, 14089)));
+		subdistricts.put("Steglitz_Zehlendorf", new IntOpenHashSet(List.of(12163, 12165, 12167, 12169, 12203, 12205, 12207, 12209, 12247, 12279, 14109, 14129, 14163, 14165, 14167, 14169, 14193, 14195, 14199)));
+		subdistricts.put("Tempelhof_Schoeneberg", new IntOpenHashSet(List.of(10777, 10779, 10781, 10783, 10823, 10825, 10827, 14197, 10829, 12101, 12103, 12105, 12109, 12157, 12159, 12161, 12249, 12277, 12307, 12309)));
+		subdistricts.put("Neukoelln", new IntOpenHashSet(List.of(12043, 12047, 12049, 12051, 12053, 12055, 12057, 12059, 12099, 12107, 12305, 12347, 12349, 12351, 12353, 12355, 12357, 12359)));
+		subdistricts.put("Treptow_Koepenick", new IntOpenHashSet(List.of(12435, 12437, 12439, 12459, 12487, 12489, 12524, 12526, 12527, 12555, 12557, 12559, 12587, 12589, 12623)));
+		subdistricts.put("Marzahn_Hellersdorf", new IntOpenHashSet(List.of(12619, 12621, 12627, 12629, 12679, 12681, 12683, 12685, 12687, 12689)));
+		subdistricts.put("Lichtenberg", new IntOpenHashSet(List.of(10315, 13057, 10317, 10318, 10319, 10365, 10367, 10369, 13055, 13059)));
+		subdistricts.put("Reinickendorf", new IntOpenHashSet(List.of(13403, 13405, 13407, 13409, 13435, 13437, 13439, 13465, 13467, 13469, 13503, 13505, 13507, 13509, 13629)));
 
 
 		Population population = PopulationUtils.readPopulation(input.toString());
@@ -127,6 +125,8 @@ public class ZipcodeLookup implements Callable<Integer> {
 
 		Index index = new Index(shapeFile.toFile(), ct, attr);
 
+
+		// Adds zipcode and subdistrict attributes to population (based on home location)
 		Map<String, String> personIdToNeighborhoodMap = new HashMap<>();
 		// Count errors
 		int unknown = 0;
@@ -139,9 +139,9 @@ public class ZipcodeLookup implements Callable<Integer> {
 				String plz = index.query(x, y);
 				p.getAttributes().putAttribute("zipcode", plz);
 
-				for (String neighborhoodName : berlinDistricts.keySet()) {
-					if (berlinDistricts.get(neighborhoodName).contains(Integer.parseInt(plz))) {
-						p.getAttributes().putAttribute("berlin-neighborhood", neighborhoodName);
+				for (String neighborhoodName : subdistricts.keySet()) {
+					if (subdistricts.get(neighborhoodName).contains(Integer.parseInt(plz))) {
+						p.getAttributes().putAttribute("subdistrict", neighborhoodName);
 						personIdToNeighborhoodMap.put(p.getId().toString(), neighborhoodName);
 
 
@@ -162,23 +162,23 @@ public class ZipcodeLookup implements Callable<Integer> {
 
 		log.info("Finished with failed lookup for {} out of {} persons.", unknown, population.getPersons().size());
 
-
-		FileWriter writer = new FileWriter("D:/Dropbox/Documents/VSP/episim/PersonToDistrictMap.txt");
-		BufferedWriter writer1 = new BufferedWriter(writer);
-		for (Map.Entry<String, String> xxx : personIdToNeighborhoodMap.entrySet()) {
-			writer1.write(xxx.getKey() + ";" + xxx.getValue());
-			writer1.newLine();
-		}
-		writer1.close();
-		writer.close();
-
 		PopulationUtils.writePopulation(population, output.toString());
 
+		//		FileWriter writer = new FileWriter("PersonToDistrictMap.txt");
+		//		BufferedWriter writer1 = new BufferedWriter(writer);
+		//		for (Map.Entry<String, String> personIdToNeighbhorhood : personIdToNeighborhoodMap.entrySet()) {
+		//			writer1.write(personIdToNeighbhorhood.getKey() + ";" + personIdToNeighbhorhood.getValue());
+		//			writer1.newLine();
+		//		}
+		//		writer1.close();
+		//		writer.close();
 
-		//Facilities
+
+
+		// Adds zipcode and subdistrict attributes to facilities (based on facility location)
 
 		Config config = ConfigUtils.createConfig();
-		config.facilities().setInputFile("D:/Dropbox/Documents/VSP/episim/be_2020-facilities_assigned_simplified_grid.xml.gz");
+		config.facilities().setInputFile(""); //"D:/Dropbox/Documents/VSP/episim/be_2020-facilities_assigned_simplified_grid.xml.gz"
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 
 		// Count errors
@@ -194,9 +194,9 @@ public class ZipcodeLookup implements Callable<Integer> {
 				((Attributable) f).getAttributes().putAttribute("zipcode", plz);
 
 
-				for (String neighborhoodName : berlinDistricts.keySet()) {
-					if (berlinDistricts.get(neighborhoodName).contains(Integer.parseInt(plz))) {
-						((Attributable) f).getAttributes().putAttribute("berlin-neighborhood", neighborhoodName);
+				for (String neighborhoodName : subdistricts.keySet()) {
+					if (subdistricts.get(neighborhoodName).contains(Integer.parseInt(plz))) {
+						((Attributable) f).getAttributes().putAttribute("subdistrict", neighborhoodName);
 					}
 				}
 			} catch (RuntimeException e) {
@@ -204,7 +204,7 @@ public class ZipcodeLookup implements Callable<Integer> {
 			}
 		}
 
-		new FacilitiesWriter(scenario.getActivityFacilities()).write("D:/Dropbox/Documents/VSP/episim/be_2020-facilities_assigned_simplified_grid_WithNeighborhoodAndPLZ.xml.gz");
+		new FacilitiesWriter(scenario.getActivityFacilities()).write("../shared-svn/projects/episim/matsim-files/snz/BerlinV2/episim-input/be_2020-facilities_assigned_simplified_grid_WithNeighborhoodAndPLZ.xml.gz");
 
 
 
