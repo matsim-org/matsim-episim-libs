02b564fd4786d463058b42e096f6377322d434db
Jakob Rehmann <jakob.rehmann@gmail.com>
2021-04-21 17:09:55 -0400

    cleaned up JRBatch

--- src/main/java/org/matsim/run/batch/JRBatch.java
+++ src/main/java/org/matsim/run/batch/JRBatch.java
@@ -5,10 +5,8 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.episim.BatchRun;
 import org.matsim.episim.EpisimConfigGroup;
-import org.matsim.episim.VaccinationConfigGroup;
 import org.matsim.run.RunParallel;
 import org.matsim.run.modules.SnzBerlinProductionScenario;
-import org.matsim.run.modules.SnzBerlinProductionScenarioJR;
 
 import javax.annotation.Nullable;
 
@@ -18,7 +16,7 @@ public class JRBatch implements BatchRun<JRBatch.Params> {
 
 	@Override
 	public AbstractModule getBindings(int id, @Nullable Params params) {
-		return new SnzBerlinProductionScenarioJR.Builder().setSnapshot(SnzBerlinProductionScenarioJR.Snapshot.no).createSnzBerlinProductionScenarioJR();
+		return new SnzBerlinProductionScenario.Builder().setSnapshot(SnzBerlinProductionScenario.Snapshot.no).createSnzBerlinProductionScenario();
 	}
 
 	@Override
@@ -29,56 +27,19 @@ public class JRBatch implements BatchRun<JRBatch.Params> {
 	@Override
 	public Config prepareConfig(int id, Params params) {
 
-		SnzBerlinProductionScenarioJR module = new SnzBerlinProductionScenarioJR.Builder().setSnapshot(
-				SnzBerlinProductionScenarioJR.Snapshot.no).setSample(25).createSnzBerlinProductionScenarioJR();
+		SnzBerlinProductionScenario module = new SnzBerlinProductionScenario.Builder().setSnapshot(
+				SnzBerlinProductionScenario.Snapshot.no).setSample(25).createSnzBerlinProductionScenario();
 		Config config = module.config();
-		//		config.global().setRandomSeed(params.seed);
 
 		config.global().setRandomSeed(7564655870752979346L);
 
 		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
-//		episimConfig.setStartFromSnapshot("");
 		episimConfig.setDistrictLevelRestrictions(params.districtLevelRestrictions);
-//		episimConfig.setSampleSize(0.01);
-//		double sampleSize = episimConfig.getSampleSize();
-
-		//		episimConfig.setSnapshotInterval();
-
-//		VaccinationConfigGroup vaccinationConfigGroup = ConfigUtils.addOrGetModule(config, VaccinationConfigGroup.class);
-//		vaccinationConfigGroup.setEffectiveness(params.dailyInitialVaccinations);
-
 
 		return config;
 	}
 
 	public static final class Params {
-
-		//		@GenerateSeeds(2)
-		//		public long seed;
-
-//		@IntParameter({3000, 10000})
-//		int dailyInitialVaccinations;
-//
-//		@StringParameter({"restrictive"})
-//		public String christmasModel;
-//
-//		@StringParameter({"closed", "open", "open&masks", "50%&masks", "50%open"})
-//		public String schools;
-//
-//		@StringParameter({"no", "ffp"})
-//		public String work;
-//
-//		@StringParameter({"no", "20-5", "22-5"})
-//		public String curfew;
-//
-//		@StringParameter({"2020-12-15", "2020-11-15", "2020-10-15"})
-//		String newVariantDate;
-//
-//		@StringParameter({"no", "yes", "yesUntil80", "no100%"})
-//		String extrapolateRestrictions;
-//
-//		@Parameter({1.35})
-//		double newVariantInfectiousness;
 
 		@EnumParameter(DistrictLevelRestrictions.class)
 		DistrictLevelRestrictions districtLevelRestrictions;
