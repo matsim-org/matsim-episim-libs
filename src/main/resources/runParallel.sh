a2b71e587ccb9f9defcc19dc6c80b118573edd3f
Christian Rakow <rakow@vsp.tu-berlin.de>
2021-04-27 17:35:54 +0200

    update run paralell script

--- src/main/resources/runParallel.sh
+++ src/main/resources/runParallel.sh
@@ -45,7 +45,7 @@ for sId in $(seq 0 $numaIndex); do
 
   # Needs to be unique among all processes
   let workerId=offset+sId
-  arguments="--threads $SLURM_NTASKS_PER_SOCKET --total-worker $totalWorker --worker-index $workerId"
+  arguments="--tasks $SLURM_NTASKS_PER_SOCKET --task-threads 8 --total-worker $totalWorker --worker-index $workerId"
   command="java -cp $classpath $JAVA_OPTS @jvm.options -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector $main $arguments"
   echo ""
   echo "command on socket $sId is $command"
