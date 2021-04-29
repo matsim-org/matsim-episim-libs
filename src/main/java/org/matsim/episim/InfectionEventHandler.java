8b8ae408386a585069d7e5e88e1f46893ee17bd3
Christian Rakow <rakow@vsp.tu-berlin.de>
2021-04-29 10:08:35 +0200

    fix typo

--- src/main/java/org/matsim/episim/InfectionEventHandler.java
+++ src/main/java/org/matsim/episim/InfectionEventHandler.java
@@ -560,9 +560,9 @@ public final class InfectionEventHandler implements Externalizable {
 		Double compliance = EpisimUtils.findValidEntry(vaccinationConfig.getCompliancePerAge(), 1.0, p.getAgeOrDefault(-1));
 
 		if (compliance == 0.0)
-			p.setTraceable(false);
+			p.setVaccinable(false);
 		else if (compliance == 1.0 || localRnd.nextDouble() < compliance)
-			p.setTraceable(true);
+			p.setVaccinable(true);
 
 		return p;
 	}
