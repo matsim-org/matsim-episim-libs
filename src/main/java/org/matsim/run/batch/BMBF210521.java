c469738f2a27bf2e7afaf8e22092f073c062fd9e
smuellervsp <mueller@vsp.tu-berlin.de>
2021-05-18 17:21:15 +0200

    new runs

--- src/main/java/org/matsim/run/batch/BMBF210521.java
+++ src/main/java/org/matsim/run/batch/BMBF210521.java
@@ -8,6 +8,8 @@ import org.matsim.episim.EpisimConfigGroup;
 import org.matsim.episim.TestingConfigGroup;
 import org.matsim.episim.VaccinationConfigGroup;
 import org.matsim.episim.VirusStrainConfigGroup;
+import org.matsim.episim.BatchRun.Parameter;
+import org.matsim.episim.BatchRun.StringParameter;
 import org.matsim.episim.model.FaceMask;
 import org.matsim.episim.model.VirusStrain;
 import org.matsim.episim.policy.FixedPolicy;
@@ -56,7 +58,7 @@ public class BMBF210521 implements BatchRun<BMBF210521.Params> {
 				.setChristmasModel(SnzBerlinProductionScenario.ChristmasModel.restrictive)
 				.createSnzBerlinProductionScenario();
 		Config config = module.config();
-		config.global().setRandomSeed(params.seed);
+		config.global().setRandomSeed(3831662765844904176L);
 
 
 		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
@@ -79,7 +81,7 @@ public class BMBF210521 implements BatchRun<BMBF210521.Params> {
 		Map<LocalDate, Double> curfewCompliance = new HashMap<LocalDate, Double>();
 		curfewCompliance.put(LocalDate.parse("2021-04-06"), 0.5);
 		
-		LocalDate date1 = LocalDate.parse("2021-05-15");
+		LocalDate date1 = LocalDate.parse("2021-05-22");
 		LocalDate date2 = date1.plusWeeks(3);
 		LocalDate date3 = date2.plusWeeks(3);
 		
@@ -91,8 +93,8 @@ public class BMBF210521 implements BatchRun<BMBF210521.Params> {
 		builder.restrict(date1, 0.75, "shop_daily", "shop_other", "errands");
 		builder.restrict(date2, 1.0, "shop_daily", "shop_other", "errands");
 		
-		builder.restrict(date2, 0.9, "work", "business", "leisure", "visit");
-		builder.restrict(date3, 1.0, "work", "business");
+		builder.restrict(date1, 0.9, "work", "business", "leisure", "visit");
+		builder.restrict(date2, 1.0, "work", "business", "leisure", "visit");
 		
 		curfewCompliance.put(date1, 0.0);
 		
@@ -101,13 +103,15 @@ public class BMBF210521 implements BatchRun<BMBF210521.Params> {
 		
 		episimConfig.setCurfewCompliance(curfewCompliance);
 		
-		if(params.masks.equals("no")) {
+		if(params.testingAndMasks.equals("no")) {
 			builder.restrict(date3, Restriction.ofMask(Map.of(FaceMask.CLOTH, 0.0, FaceMask.SURGICAL, 0.0, FaceMask.N95, 0.0)), AbstractSnzScenario2020.DEFAULT_ACTIVITIES);
 			builder.restrict(date3, Restriction.ofMask(Map.of(FaceMask.CLOTH, 0.0, FaceMask.SURGICAL, 0.0, FaceMask.N95, 0.0)), "pt");
 		}
 		
 		builder.apply("2021-03-26", "2021-04-09", (d, e) -> e.put("fraction", params.workFactor * (double) e.get("fraction")), "work", "business");
-		builder.apply("2021-06-25", "2021-08-06", (d, e) -> e.put("fraction", params.workFactor * (double) e.get("fraction")), "work", "business");
+		
+		builder.restrict("2021-06-25", params.workFactor, "work", "business");
+		builder.restrict("2021-08-06", 1.0, "work", "business");
 
 		
 		episimConfig.setPolicy(FixedPolicy.class, builder.build());
@@ -119,17 +123,38 @@ public class BMBF210521 implements BatchRun<BMBF210521.Params> {
 		virusStrainConfigGroup.getOrAddParams(VirusStrain.B117).setVaccineEffectiveness(1.0);
 		virusStrainConfigGroup.getOrAddParams(VirusStrain.B117).setFactorSeriouslySick(1.5);
 		
+		if (params.b1351inf > 0.0) {
+			Map<LocalDate, Integer> infPerDayB1351 = new HashMap<>();
+			infPerDayB1351.put(LocalDate.parse("2020-01-01"), 0);
+			infPerDayB1351.put(LocalDate.parse(params.b1351date), 1);
+			episimConfig.setInfections_pers_per_day(VirusStrain.B1351, infPerDayB1351);
+			virusStrainConfigGroup.getOrAddParams(VirusStrain.B1351).setInfectiousness(params.b1351inf);
+			virusStrainConfigGroup.getOrAddParams(VirusStrain.B1351).setVaccineEffectiveness(params.b1351VaccinationEffectiveness);
+			virusStrainConfigGroup.getOrAddParams(VirusStrain.B1351).setReVaccineEffectiveness(1.0);	
+		}
+		
 		
 		VaccinationConfigGroup vaccinationConfig = ConfigUtils.addOrGetModule(config, VaccinationConfigGroup.class);
 		vaccinationConfig.setFactorSeriouslySick(0.5);
 		
 		Map<Integer, Double> vaccinationCompliance = new HashMap<>();
 		
-		for(int i = 0; i<18; i++) vaccinationCompliance.put(i, 0.);
+		for(int i = 0; i<15; i++) vaccinationCompliance.put(i, 0.);
+		double vaccinationCompliance1518 = 0.0;
+		if (params.vaccinate1518.equals("yes")) vaccinationCompliance1518 = params.vaccinationCompliance;
+		for(int i = 15; i<18; i++) vaccinationCompliance.put(i, vaccinationCompliance1518);
 		for(int i = 18; i<120; i++) vaccinationCompliance.put(i, params.vaccinationCompliance);
 
 		vaccinationConfig.setCompliancePerAge(vaccinationCompliance);
+		
+		Map<LocalDate, Integer> reVaccinations = new HashMap<>();
 
+		if (!params.revaccinationDate.equals("no")) {
+			reVaccinations.put(LocalDate.parse("2020-01-01"), 0);
+			reVaccinations.put(LocalDate.parse(params.revaccinationDate), (int) (0.01 * 4_831_120));
+			vaccinationConfig.setReVaccinationCapacity_pers_per_day(reVaccinations);
+		}
+		
 		
 		TestingConfigGroup testingConfigGroup = ConfigUtils.addOrGetModule(config, TestingConfigGroup.class);
 		
@@ -153,13 +178,9 @@ public class BMBF210521 implements BatchRun<BMBF210521.Params> {
 						
 		LocalDate testingDate = LocalDate.parse("2021-03-19");
 		
-//		double leisureRate = Integer.parseInt(params.testingRateEduWorkLeisure.split("-")[2]) / 100.;
-//		double workRate = Integer.parseInt(params.testingRateEduWorkLeisure.split("-")[1]) / 100.;
-//		double eduRate = Integer.parseInt(params.testingRateEduWorkLeisure.split("-")[0]) / 100.;
-		
-		double leisureRate = 5 / 100.;
-		double workRate = 5 / 100.;
-		double eduRate = 20 / 100.;
+		double leisureRate1 = Integer.parseInt(params.testingRateEduWorkLeisure1.split("-")[2]) / 100.;
+		double workRate1 = Integer.parseInt(params.testingRateEduWorkLeisure1.split("-")[1]) / 100.;
+		double eduRate1 = Integer.parseInt(params.testingRateEduWorkLeisure1.split("-")[0]) / 100.;
 		
 		Map<LocalDate, Double> leisureTests = new HashMap<LocalDate, Double>();
 		Map<LocalDate, Double> workTests = new HashMap<LocalDate, Double>();
@@ -169,12 +190,14 @@ public class BMBF210521 implements BatchRun<BMBF210521.Params> {
 		eduTests.put(LocalDate.parse("2020-01-01"), 0.);
 		
 		for (int i = 1; i<=31; i++) {
-			if (params.testingRateEduWorkLeisure.equals("new") || i == 31) {
-				leisureTests.put(testingDate.plusDays(i), leisureRate * i / 31.);
-				workTests.put(testingDate.plusDays(i), workRate * i / 31.);
-				eduTests.put(testingDate.plusDays(i), eduRate * i / 31.);
-			}
-			
+			leisureTests.put(testingDate.plusDays(i), leisureRate1 * i / 31.);
+			workTests.put(testingDate.plusDays(i), workRate1 * i / 31.);
+			eduTests.put(testingDate.plusDays(i), eduRate1 * i / 31.);
+		}
+		if (params.testingAndMasks.equals("no")) {
+			leisureTests.put(date3, 0.0);
+			workTests.put(date3, 0.0);
+			eduTests.put(date3, 0.0);
 		}
 		
 		testingConfigGroup.setTestingRatePerActivityAndDate((Map.of(
@@ -192,33 +215,41 @@ public class BMBF210521 implements BatchRun<BMBF210521.Params> {
 		testingConfigGroup.setTestingCapacity_pers_per_day(Map.of(
 				LocalDate.of(1970, 1, 1), 0, 
 				testingDate, Integer.MAX_VALUE));
-		
-
-		
 	
 		return config;
 	}
 
 	public static final class Params {
 
-		@GenerateSeeds(5)
-		public long seed;
+//		@GenerateSeeds(1)
+//		public long seed;
 		
-//		@StringParameter({"0-0-0", "20-5-5"})		
-		@StringParameter({"old", "new"})
-		String testingRateEduWorkLeisure;
+		@StringParameter({"20-5-5"})		
+		String testingRateEduWorkLeisure1;
 		
-		@StringParameter({"current"})
-		String vaccination;
-		
-		@StringParameter({"yes"})
-		String masks;
+		@StringParameter({"no", "yes"})		
+		String testingAndMasks;
 		
 		@Parameter({1.0, 0.83})
 		double workFactor;
 		
-		@Parameter({1.0, 0.8, 0.6})
+		@StringParameter({"2021-04-01", "2021-06-01"})
+		String b1351date;
+		
+		@Parameter({0.0, 1.0, 1.4, 1.8})
+		double b1351inf;
+		
+		@Parameter({1.0, 0.9, 0.7, 0.5, 0.25, 0.0})
+		double b1351VaccinationEffectiveness;
+		
+		@Parameter({0.8, 0.6})
 		double vaccinationCompliance;
+		
+		@StringParameter({"yes", "no"})
+		String vaccinate1518;
+		
+		@StringParameter({"no", "2021-08-01", "2021-10-01"})
+		String revaccinationDate;
 		
 //		@StringParameter({"0-0-0", "20-5-5", "20-10-5"})
 //		String testingRateEduWorkLeisure2;
