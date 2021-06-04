cab917d9de7ac18189ec77d2b67d7f3b171d9ee6
Jakob Rehmann <jakob.rehmann@gmail.com>
2021-06-04 13:06:47 +0200

    fixed problem with Landkreise vs. Stadtkreise

--- src/main/R/nightlyActivityByLK.R
+++ src/main/R/nightlyActivityByLK.R
@@ -3,8 +3,12 @@ library(tidyverse)
 library(tmap)
 library(sf)
 
-svnLocationShape <- "/Users/Ricardo/git/public-svn/matsim/scenarios/countries/de/episim/original-data/landkreise-in-germany/landkreise-in-germany.shp"
-svnLocationData <- "/Users/Ricardo/git/public-svn/matsim/scenarios/countries/de/episim/mobilityData/landkreise/"
+setwd("C:/Users/jakob/projects")
+#setwd("/Users/Ricardo/git")
+
+svnLocationShape <-"public-svn/matsim/scenarios/countries/de/episim/original-data/landkreise-in-germany/landkreise-in-germany.shp"
+svnLocationData <- "public-svn/matsim/scenarios/countries/de/episim/mobilityData/landkreise/"
+svnLocationShapeBld <- "public-svn/matsim/scenarios/countries/de/episim/mobilityData/bundeslaender/vg2500_bld.shp"
 
 outersect <- function(x, y) {
   sort(c(setdiff(x, y),
@@ -14,15 +18,33 @@ outersect <- function(x, y) {
 #Part 1) LK_Timeline 22-5 compare two dates
 data <- read.csv2(paste0(svnLocationData,"LK_Timeline_WeeklyNumbers_perPerson.csv"))
 
-unwanted_hours <- as.character(seq(6,21))
-
 data %>% glimpse()
 
 data2 <- data %>%
   mutate(date = as.Date(date, format = "%d,%m,%y")) %>% 
   filter(type == "endNonHomeActs") %>%
-#  select(!contains(unwanted_hours))# %>% 
-  mutate("night_total" = `X.0h` +`X0.1h`+`X1.2h`+`X2.3h`+`X3.4h`+`X4.5h`+`X22.23h`+`X23.24h`+`X24.25h`+`X25.26h`+`X26.27h`)
+  mutate("night_total" = X.0h + X0.1h + X1.2h + X2.3h + X3.4h + X4.5h + X22.23h + X23.24h + X24.25h + X25.26h + X26.27h)
+
+
+for(i in 1:nrow(data2)){
+  if (grepl("Kreis", data2$area[i],fixed = TRUE)
+      | grepl("kreis", data2$area[i],fixed = TRUE) ){
+    if (grepl("Landkreis", data2$area[i],fixed = TRUE)
+        |grepl("landkreis", data2$area[i],fixed = TRUE) ){
+      data2$isLandkreis[i] = TRUE
+      data2$isKreis[i] = FALSE
+    } else {
+      data2$isLandkreis[i] = FALSE
+      data2$isKreis[i] = TRUE
+      }
+  } else {
+    data2$isLandkreis[i] = FALSE
+    data2$isKreis[i] = FALSE
+  }
+}  
+
+#data2$isLandkreis[i] = if (grepl("Landkreis", data2$area[i],ignore.case = TRUE, fixed = TRUE)) TRUE else FALSE
+#data2$isKreis[i] = if (grepl("Kreis", data2$area[i], ignore.case = TRUE,fixed = TRUE)) TRUE else FALSE
 
 data2$area <- data2$area %>% 
   str_replace("Landkreis ","") %>% 
@@ -36,29 +58,59 @@ data2$area <- data2$area %>%
 
 data_to_merge <- data2 %>% 
   filter(date == "2021-05-02" | date == "2021-03-28") %>% 
-  select(c(date,area,night_total)) %>% 
-  pivot_wider(names_from = date,values_from = night_total,values_fn = mean) %>%
-  mutate(change = `2021-05-02`/`2021-03-28`) %>% select(c(area,change))
+  select(c(date,area,isKreis,isLandkreis,night_total)) %>% 
+  pivot_wider(names_from = date,values_from = night_total) %>%
+  mutate(change = `2021-05-02`/`2021-03-28`) %>% select(c(area,isKreis,isLandkreis,change))
 
 # Landkreise Shapefile 
 
-lk <- st_read(svnLocationShape) %>% 
-  mutate(area = name_2)
+lk <- st_read(svnLocationShape)
 
-lk2 <- lk %>% left_join(data_to_merge, by="area") %>% mutate(percent_reduction = change -1)
+lk$name_2 <- lk$name_2 %>% 
+  str_replace("(Landkreis)","")
+  
 
-lk_names <- lk$name_2
+lk2 <- lk %>% mutate(area = name_2)
+
+for(i in 1:nrow(lk2)){
+  lk2$isLandkreis[i] = if (lk2$type_2[i] == "Landkreis") TRUE else FALSE
+  lk2$isKreis[i] = if (lk2$type_2[i] == "Kreis") TRUE else FALSE
+}  
+
+lk3 <- lk2 %>% left_join(data_to_merge, by=c("area","isLandkreis", "isKreis")) 
+lk3 <- lk3 %>% left_join(data_to_merge, by= "area") 
+
+for(i in 1:nrow(lk3)){
+  if(!is.na(lk3$change.x[i])){
+    lk3$change_final[i] = lk3$change.x[i]
+  } else {
+    lk3$change_final[i] = lk3$change.y[i] 
+  }
+}  
+
+lk3 <-lk3 %>% mutate(percent_reduction = change_final -1)
+
+lk_names <- lk3$name_2
 data_names <- unique(data2$area)
 
 Reduce(intersect,list(lk_names,data_names))
 Reduce(outersect,list(lk_names,data_names))
+bundeslaender <- st_read(svnLocationShapeBld)
 
 tmap_mode("view")
-tm_shape(lk2) +
+tm_shape(lk3) +
   tm_polygons(col = "percent_reduction",
               id = "area", 
-             title.col = "% Reduction of Nightly Activites", title='28.March/02.May')
+             title.col = "% Reduction of Nightly Activites", title='28.March/02.May') #+ 
+  tm_shape(bundeslaender) +
+  tm_polygons()
 
+  
+  
+  
+  
+  
+  
 #Part 2 duration for one day
 
 data_hours <- read.csv2(paste0(svnLocationData,"mobilityData_OverviewLK.csv"))
@@ -85,12 +137,13 @@ lk <- st_read(svnLocationShape) %>%
   mutate(Landkreis = name_2)
 
 lk2 <- lk %>% left_join(data_hours_date, by="Landkreis") %>% mutate(hoursOutHome = outOfHomeDuration)
-
 lk_names <- lk$name_2
 data_names <- unique(data_hours$Landkreis)
 
 Reduce(intersect,list(lk_names,data_names))
 Reduce(outersect,list(lk_names,data_names))
+
+
 
 tm_shape(lk2) +
   tm_polygons(col = "hoursOutHome",
