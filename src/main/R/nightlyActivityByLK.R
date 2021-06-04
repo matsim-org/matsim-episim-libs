7bededdedd06001446e25625c7afc2bfaa4b6ae8
Jakob Rehmann <jakob.rehmann@gmail.com>
2021-06-04 13:16:35 +0200

    fixed problem with Landkreise vs. Stadtkreise

--- src/main/R/nightlyActivityByLK.R
+++ src/main/R/nightlyActivityByLK.R
@@ -8,7 +8,6 @@ setwd("C:/Users/jakob/projects")
 
 svnLocationShape <-"public-svn/matsim/scenarios/countries/de/episim/original-data/landkreise-in-germany/landkreise-in-germany.shp"
 svnLocationData <- "public-svn/matsim/scenarios/countries/de/episim/mobilityData/landkreise/"
-svnLocationShapeBld <- "public-svn/matsim/scenarios/countries/de/episim/mobilityData/bundeslaender/vg2500_bld.shp"
 
 outersect <- function(x, y) {
   sort(c(setdiff(x, y),
@@ -18,14 +17,12 @@ outersect <- function(x, y) {
 #Part 1) LK_Timeline 22-5 compare two dates
 data <- read.csv2(paste0(svnLocationData,"LK_Timeline_WeeklyNumbers_perPerson.csv"))
 
-data %>% glimpse()
-
 data2 <- data %>%
   mutate(date = as.Date(date, format = "%d,%m,%y")) %>% 
   filter(type == "endNonHomeActs") %>%
   mutate("night_total" = X.0h + X0.1h + X1.2h + X2.3h + X3.4h + X4.5h + X22.23h + X23.24h + X24.25h + X25.26h + X26.27h)
 
-
+# Specify whether location is Landkreis or Kreis, needed for merge later, since some kreise and landkreise share a name
 for(i in 1:nrow(data2)){
   if (grepl("Kreis", data2$area[i],fixed = TRUE)
       | grepl("kreis", data2$area[i],fixed = TRUE) ){
@@ -43,19 +40,19 @@ for(i in 1:nrow(data2)){
   }
 }  
 
-#data2$isLandkreis[i] = if (grepl("Landkreis", data2$area[i],ignore.case = TRUE, fixed = TRUE)) TRUE else FALSE
-#data2$isKreis[i] = if (grepl("Kreis", data2$area[i], ignore.case = TRUE,fixed = TRUE)) TRUE else FALSE
-
+# Manual change of names to work with merge later
 data2$area <- data2$area %>% 
-  str_replace("Landkreis ","") %>% 
+  str_replace("Landkreis ","") %>%
+  str_replace("München()","München") %>% 
   str_replace("Kreis ","") %>% 
   str_replace("Nienburg/Weser","Nienburg (Weser)") %>% 
   str_replace("Cottbus - Chóśebuz","Cottbus") %>% 
   str_replace("Lindau","Lindau (Bodensee)") %>% 
   str_replace("Rhein-Neuss", "Rhein-Kreis Neuss") %>% 
-  str_replace("Altenkirchen","Altenkirchen (Westerwald)")
+  str_replace("Altenkirchen","Altenkirchen (Westerwald)") %>% 
+  str_replace("St, Wendel", "St. Wendel")
 
-
+#select dates that should be compared and find the change between two daes. 
 data_to_merge <- data2 %>% 
   filter(date == "2021-05-02" | date == "2021-03-28") %>% 
   select(c(date,area,isKreis,isLandkreis,night_total)) %>% 
@@ -63,11 +60,10 @@ data_to_merge <- data2 %>%
   mutate(change = `2021-05-02`/`2021-03-28`) %>% select(c(area,isKreis,isLandkreis,change))
 
 # Landkreise Shapefile 
-
 lk <- st_read(svnLocationShape)
 
 lk$name_2 <- lk$name_2 %>% 
-  str_replace("(Landkreis)","")
+  str_replace("München(Landkreis)","München")
   
 
 lk2 <- lk %>% mutate(area = name_2)
@@ -77,9 +73,12 @@ for(i in 1:nrow(lk2)){
   lk2$isKreis[i] = if (lk2$type_2[i] == "Kreis") TRUE else FALSE
 }  
 
+# First merge where all three variables are checked --> change.x
 lk3 <- lk2 %>% left_join(data_to_merge, by=c("area","isLandkreis", "isKreis")) 
+#Then, just using location names --> change.y
 lk3 <- lk3 %>% left_join(data_to_merge, by= "area") 
 
+# if there is no value in change.x, then use value from change.y
 for(i in 1:nrow(lk3)){
   if(!is.na(lk3$change.x[i])){
     lk3$change_final[i] = lk3$change.x[i]
@@ -88,6 +87,7 @@ for(i in 1:nrow(lk3)){
   }
 }  
 
+
 lk3 <-lk3 %>% mutate(percent_reduction = change_final -1)
 
 lk_names <- lk3$name_2
@@ -95,15 +95,12 @@ data_names <- unique(data2$area)
 
 Reduce(intersect,list(lk_names,data_names))
 Reduce(outersect,list(lk_names,data_names))
-bundeslaender <- st_read(svnLocationShapeBld)
 
 tmap_mode("view")
 tm_shape(lk3) +
   tm_polygons(col = "percent_reduction",
               id = "area", 
-             title.col = "% Reduction of Nightly Activites", title='28.March/02.May') #+ 
-  tm_shape(bundeslaender) +
-  tm_polygons()
+             title.col = "% Reduction of Nightly Activites", title='28.March/02.May')
 
   
   
@@ -149,6 +146,5 @@ tm_shape(lk2) +
   tm_polygons(col = "hoursOutHome",
               id = "Landkreis", 
               title.col = "% Average hours out of home", title='02. May 2021')
-dddd = lk$name_2
-#View(dddd)
+
 
