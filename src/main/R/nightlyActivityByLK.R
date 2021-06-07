ee6ae427dae4b3e3a193354f4641bb88a8e45a11
Jakob Rehmann <jakob.rehmann@gmail.com>
2021-06-07 10:01:03 +0200

    cleaned up and refactored

--- src/main/R/nightlyActivityByLK.R
+++ src/main/R/nightlyActivityByLK.R
@@ -4,74 +4,86 @@ library(tmap)
 library(sf)
 library(stats)
 
-#setwd("C:/Users/jakob/projects")
+setwd("C:/Users/jakob/projects")
 #setwd("/Users/Ricardo/git")
-setwd("/Users/kainagel/")
+#setwd("/Users/kainagel/")
 
 rm(list = ls())
 
-svnLocationShape <- "public-svn/matsim/scenarios/countries/de/episim/original-data/landkreise-in-germany/"
-svnLocationData <- "public-svn/matsim/scenarios/countries/de/episim/mobilityData/landkreise/"
+## Define global variables
+gbl_location_data <- "public-svn/matsim/scenarios/countries/de/episim/mobilityData/landkreise/"
 
-outersect <- function(x, y) {
+location_shape <- "public-svn/matsim/scenarios/countries/de/episim/original-data/landkreise-in-germany/"
+gbl_bl <- st_read(paste0(location_shape, "Bundesländer_2016_ew.shp"))
+# import and modify shape file for districts (Landkreise, Stadtkreise...)
+
+lk <- st_read(paste0(location_shape, "landkreise-in-germany.shp"))
+gbl_lk <- lk %>% mutate(area = name_2)
+
+for(i in 1:nrow(gbl_lk)){
+  gbl_lk$isLandkreis[i] = if (gbl_lk$type_2[i] == "Landkreis") TRUE else FALSE
+  gbl_lk$isKreis[i] = if (gbl_lk$type_2[i] == "Kreis") TRUE else FALSE
+}  
+
+## Define global functions
+gbl_outersect <- function(x, y) {
   sort(c(setdiff(x, y),
          setdiff(y, x)))
 }
 
+gbl_prep_mobility_data <- function(df){
+  
+  for(i in 1:nrow(df)){
+    if (grepl("Kreis", df$area[i],fixed = TRUE)
+        | grepl("kreis", df$area[i],fixed = TRUE) ){
+      if (grepl("Landkreis", df$area[i],fixed = TRUE)
+          |grepl("landkreis", df$area[i],fixed = TRUE) ){
+        df$isLandkreis[i] <- TRUE
+        df$isKreis[i] <- FALSE
+      } else {
+        df$isLandkreis[i] <- FALSE
+        df$isKreis[i] <- TRUE
+      }
+    } else {
+      df$isLandkreis[i] <- FALSE
+      df$isKreis[i] <- FALSE
+    }
+  }  
+  
+  # Manual change of names to work with merge later
+  df$area <- df$area %>% 
+    str_replace("Landkreis ","") %>%
+    str_replace("Kreis ","") %>%
+    str_replace("Nienburg/Weser","Nienburg (Weser)") %>% 
+    str_replace("Lindau","Lindau (Bodensee)") %>% 
+    str_replace("Rhein-Neuss", "Rhein-Kreis Neuss") %>% 
+    str_replace("Altenkirchen","Altenkirchen (Westerwald)") %>% 
+    str_replace("St, Wendel", "St. Wendel")
+  
+  
+  df[df$area == "München" & df$isLandkreis,"area"] <- "München(Landkreis)"
+  df[grepl("Cottbus",df$area),"area"] <- "Cottbus"
+  
+  return(df)
+}
 
-# Data prep: import and modify shape file for districts (Landkreise, Stadtkreise...)
-# Landkreise Shapefile 
-lk <- st_read(paste0(svnLocationShape, "landkreise-in-germany.shp"))
-bl <- st_read(paste0(svnLocationShape, "Bundesländer_2016_ew.shp"))
+############################################
+## Part 1) Pct Reduction in nightly trips ##
+############################################
 
-lk2 <- lk %>% mutate(area = name_2)
+rm(list = gbl_outersect(ls(),ls(pattern = "^gbl")))
 
-
-for(i in 1:nrow(lk2)){
-  lk2$isLandkreis[i] = if (lk2$type_2[i] == "Landkreis") TRUE else FALSE
-  lk2$isKreis[i] = if (lk2$type_2[i] == "Kreis") TRUE else FALSE
-}  
-
-#Part 1) LK_Timeline 22-5 compare two dates
-#data <- read.csv2(paste0(svnLocationData,"LK_Timeline_WeeklyNumbers_perPerson.csv"),encoding = "DOS")
-data <- read.csv2(paste0(svnLocationData,"LK_Timeline_WeeklyNumbers_perPerson.csv"))
+#data <- read.csv2(paste0(gbl_location_data,"LK_Timeline_WeeklyNumbers_perPerson.csv"),encoding = "DOS")
+data <- read.csv2(paste0(gbl_location_data,"LK_Timeline_WeeklyNumbers_perPerson.csv"), fileEncoding = "UTF-8")
 
 data2 <- data %>%
   mutate(date = as.Date(date, format = "%d,%m,%y")) %>% 
   filter(type == "endNonHomeActs") %>%
   mutate("night_total" = X.0h + X0.1h + X1.2h + X2.3h + X3.4h + X4.5h + X22.23h + X23.24h + X24.25h + X25.26h + X26.27h)
 
-# Specify whether location is Landkreis or Kreis, needed for merge later, since some kreise and landkreise share a name
-for(i in 1:nrow(data2)){
-  if (grepl("Kreis", data2$area[i],fixed = TRUE)
-      | grepl("kreis", data2$area[i],fixed = TRUE) ){
-    if (grepl("Landkreis", data2$area[i],fixed = TRUE)
-        |grepl("landkreis", data2$area[i],fixed = TRUE) ){
-      data2$isLandkreis[i] <- TRUE
-      data2$isKreis[i] <- FALSE
-    } else {
-      data2$isLandkreis[i] <- FALSE
-      data2$isKreis[i] <- TRUE
-    }
-  } else {
-    data2$isLandkreis[i] <- FALSE
-    data2$isKreis[i] <- FALSE
-  }
-}  
 
-# Manual change of names to work with merge later
-data2$area <- data2$area %>% 
-  str_replace("Landkreis ","") %>%
-  str_replace("Kreis ","") %>%
-  str_replace("Nienburg/Weser","Nienburg (Weser)") %>% 
-  str_replace("Lindau","Lindau (Bodensee)") %>% 
-  str_replace("Rhein-Neuss", "Rhein-Kreis Neuss") %>% 
-  str_replace("Altenkirchen","Altenkirchen (Westerwald)") %>% 
-  str_replace("St, Wendel", "St. Wendel")
+data3 <- gbl_prep_mobility_data(data2)
 
-
-data2[data2$area == "München" & data2$isLandkreis,"area"] <- "München(Landkreis)"
-data2[grepl("Cottbus",data2$area),"area"] <- "Cottbus"
 
 # 2020-03-08
 
@@ -87,7 +99,7 @@ dateAfter <- "2021-05-02"
 
 
 #select dates that should be compared and find the change between two daes. 
-data_to_merge <- data2 %>%
+data_to_merge <- data3 %>%
   filter(date == dateAfter | date == dateBefore ) %>%
   select(c(date,area,isKreis,isLandkreis,night_total)) %>% 
   pivot_wider(names_from = date,values_from = night_total, values_fn = mean ) %>%
@@ -101,11 +113,8 @@ data_to_merge <- data2 %>%
 
 # Für München wurde das jetzt manuell gerichtet. -jakob
 
-
-
-
 # First merge where all three variables are checked --> change.x
-lk3 <- lk2 %>% left_join(data_to_merge, by=c("area","isLandkreis", "isKreis")) 
+lk3 <- gbl_lk %>% left_join(data_to_merge, by=c("area","isLandkreis", "isKreis")) 
 #Then, just using location names --> change.y
 lk3 <- lk3 %>% left_join(data_to_merge, by= "area") 
 
@@ -121,67 +130,30 @@ for(i in 1:nrow(lk3)){
 
 lk3 <-lk3 %>% mutate(percent_reduction = (change_final -1)*100)
 
-lk_names <- lk3$name_2
-data_names <- unique(data2$area)
-
-Reduce(intersect,list(lk_names,data_names))
-Reduce(outersect,list(lk_names,data_names))
-
+Reduce(gbl_outersect,list(lk3$name_2,unique(data3$area)))
 
 tmap_mode("view")
 tm_shape(lk3) +
   tm_polygons(col = "percent_reduction",
               id = "area", 
               title.col = "% Reduction of Nightly Activites", title= paste0(dateAfter," vs ",dateBefore) ) +
-  tm_layout(legend.position = c("right", "top"), title= 'Veränderung der Anzahl beendeter Aktivitäten außer Haus zwischen 22-5 Uhr in %',  title.position = c('right', 'top')) #+
-#tm_shape(bl) +
-#  tm_borders(lwd = 2, col = "blue")
+  tm_layout(legend.position = c("right", "top"), title= 'Veränderung der Anzahl beendeter Aktivitäten außer Haus zwischen 22-5 Uhr in %',  title.position = c('right', 'top')) +
+  tm_shape(gbl_bl) +
+    tm_borders(lwd = 2, col = "blue")
 
-#########################################
-#########################################
-rm(list = ls())
+####################################
+## Part 2) duration of activities ##
+####################################
 
-#Part 2 duration for one day
+rm(list = gbl_outersect(ls(),ls(pattern = "^gbl")))
 
-svnLocationShape <- "public-svn/matsim/scenarios/countries/de/episim/original-data/landkreise-in-germany/"
-svnLocationData <- "public-svn/matsim/scenarios/countries/de/episim/mobilityData/landkreise/"
+data_hours <- read.csv2(paste0(gbl_location_data,"mobilityData_OverviewLK.csv"),fileEncoding = "UTF-8")
 
-data_hours <- read.csv2(paste0(svnLocationData,"mobilityData_OverviewLK.csv"))
-data_hours %>% glimpse()
 
 data_hours2 <- data_hours %>%
-  mutate(date = as.Date(strptime(date, "%Y%m%d")))
+  mutate(date = as.Date(strptime(date, "%Y%m%d"))) %>% rename(area = Landkreis)
 
-# Specify whether location is Landkreis or Kreis, needed for merge later, since some kreise and landkreise share a name
-for(i in 1:nrow(data_hours2)){
-  if (grepl("Kreis", data_hours2$Landkreis[i],fixed = TRUE)
-      | grepl("kreis", data_hours2$Landkreis[i],fixed = TRUE) ){
-    if (grepl("Landkreis", data_hours2$Landkreis[i],fixed = TRUE)
-        |grepl("landkreis", data_hours2$Landkreis[i],fixed = TRUE) ){
-      data_hours2$isLandkreis[i] = TRUE
-      data_hours2$isKreis[i] = FALSE
-    } else {
-      data_hours2$isLandkreis[i] = FALSE
-      data_hours2$isKreis[i] = TRUE
-    }
-  } else {
-    data_hours2$isLandkreis[i] = FALSE
-    data_hours2$isKreis[i] = FALSE
-  }
-}  
-
-data_hours2$Landkreis <- data_hours2$Landkreis %>%
-  str_replace("Landkreis ","") %>%
-  str_replace("Kreis ","") %>% 
-  str_replace("Nienburg/Weser","Nienburg (Weser)") %>% 
-  str_replace("Lindau","Lindau (Bodensee)") %>% 
-  str_replace("Rhein-Neuss", "Rhein-Kreis Neuss") %>% 
-  str_replace("Altenkirchen","Altenkirchen (Westerwald)") %>% 
-  str_replace("St, Wendel", "St. Wendel")
-
-data_hours2[data_hours2$Landkreis == "München" & data_hours2$isLandkreis,"Landkreis"] <- "München(Landkreis)"
-data_hours2[grepl("Cottbus",data_hours2$Landkreis),"Landkreis"] <- "Cottbus"
-
+data_hours3 <- gbl_prep_mobility_data(data_hours2)
 # 2020-03-08
 
 # 2021-03-28
@@ -194,33 +166,25 @@ data_hours2[grepl("Cottbus",data_hours2$Landkreis),"Landkreis"] <- "Cottbus"
 dateBefore <- "2020-03-08"
 dateAfter <- "2021-05-02"
 
-data_hours_date <- data_hours2 %>%
+data_hours_date <- data_hours3 %>%
   filter(date == dateAfter ) %>%
-  select(c(Landkreis,isKreis,isLandkreis, outOfHomeDuration))
+  select(c(area,isKreis,isLandkreis, outOfHomeDuration))
 
-data_hours_change <- data_hours2%>%
+data_hours_change <- data_hours3 %>%
   filter(date == dateBefore | date == dateAfter) %>%
-  select(c(date,Landkreis,isKreis,isLandkreis,outOfHomeDuration)) %>% 
+  select(c(date,area,isKreis,isLandkreis,outOfHomeDuration)) %>% 
   pivot_wider(names_from = date,values_from = outOfHomeDuration) %>%
   mutate(change = !!sym(dateAfter)/!!sym(dateBefore) ) %>%
-  select(c(Landkreis,isKreis,isLandkreis,change)) %>%
+  select(c(area,isKreis,isLandkreis,change)) %>%
   {.}
 
-lk <- st_read(paste0(svnLocationShape, "landkreise-in-germany.shp")) 
-lk2 <- lk %>% mutate(Landkreis = name_2)
-
-bl <- st_read(paste0(svnLocationShape, "Bundesländer_2016_ew.shp"))
-for(i in 1:nrow(lk2)){
-  lk2$isLandkreis[i] = if (lk2$type_2[i] == "Landkreis") TRUE else FALSE
-  lk2$isKreis[i] = if (lk2$type_2[i] == "Kreis") TRUE else FALSE
-}  
 
 # First merge where all three variables are checked --> change.x
-lk3_change <- lk2 %>% left_join(data_hours_change, by=c("Landkreis","isLandkreis", "isKreis")) 
-lk3_date <- lk2 %>% left_join(data_hours_date, by=c("Landkreis","isLandkreis", "isKreis")) 
+lk3_change <- gbl_lk %>% left_join(data_hours_change, by=c("area","isLandkreis", "isKreis")) 
+lk3_date <- gbl_lk %>% left_join(data_hours_date, by=c("area","isLandkreis", "isKreis")) 
 #Then, just using location names --> change.y
-lk3_change <- lk3_change %>% left_join(data_hours_change, by= "Landkreis") 
-lk3_date <- lk3_date %>% left_join(data_hours_date, by= "Landkreis")
+lk3_change <- lk3_change %>% left_join(data_hours_change, by= "area") 
+lk3_date <- lk3_date %>% left_join(data_hours_date, by= "area")
 
 # if there is no value in change.x, then use value from change.y
 for(i in 1:nrow(lk3_change)){
@@ -239,33 +203,28 @@ for(i in 1:nrow(lk3_date)){
 }  
 
 
-lk3_date <- lk3_date %>% left_join(data_hours_date, by="Landkreis") %>% mutate(hoursOutHome = outOfHomeDuration_final)
-lk3_changes <- lk3_change %>% left_join(data_hours_change, by="Landkreis") %>% mutate( changeOutOfHome = pmin( 0., (change_final -1.)*100. )  )
+lk3_date <- lk3_date %>% left_join(data_hours_date, by="area") %>% mutate(hoursOutHome = outOfHomeDuration_final)
+lk3_changes <- lk3_change %>% left_join(data_hours_change, by="area") %>% mutate( changeOutOfHome = pmin( 0., (change_final -1.)*100. )  )
+# yyyyyy seems like a duplicated code? -jakob, june '21
 
-head( lk3_changes)
 
+Reduce(gbl_outersect,list(lk3_date$area,unique(data_hours3$area)))
 
-lk_names <- lk2$name_2
-data_names <- unique(data_hours2$Landkreis)
-
-#Reduce(intersect,list(lk_names,data_names))
-Reduce(outersect,list(lk_names,data_names))
-
-tmap_mode("plot" )
+tmap_mode("view" )
 tm_shape(lk3_date) +
   tm_polygons(col = "hoursOutHome",
-              id = "Landkreis", 
+              id = "area", 
               title.col = "% Average hours out of home", title=dateAfter) +
-  tm_layout(legend.position = c("right", "top"), title= 'Durchschnittliche Dauer aushäusiger Aktivitäten pro Person in Stunden',  title.position = c('right', 'top'))# +
-  #tm_shape(bl) +
-  #tm_borders(lwd = 2, col = "blue") 
+  tm_layout(legend.position = c("right", "top"), title = 'Durchschnittliche Dauer aushäusiger Aktivitäten pro Person in Stunden',  title.position = c('right', 'top')) +
+  tm_shape(gbl_bl) +
+  tm_borders(lwd = 2, col = "blue") 
 
 tm_shape(lk3_changes) +
   tm_polygons(col = "changeOutOfHome",
-              id = "Landkreis", 
+              id = "area", 
               title.col = "% Average hours out of home", title=paste0( dateAfter, " vs ", dateBefore )) +
-  tm_layout(legend.position = c("right", "top"), title= 'Veränderung Dauer aushäusiger Aktivitäten pro Person in %',  title.position = c('right', 'top')) #+
-  #tm_shape(bl) +
-  #tm_borders(lwd = 2, col = "blue") 
+  tm_layout(legend.position = c("right", "top"), title= 'Veränderung Dauer aushäusiger Aktivitäten pro Person in %',  title.position = c('right', 'top')) +
+  tm_shape(gbl_bl) +
+  tm_borders(lwd = 2, col = "blue") 
 
 
