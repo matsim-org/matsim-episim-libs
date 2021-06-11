24eb460c74b52502e00dcf006f54f7d201c4767b
Jakob Rehmann <jakob.rehmann@gmail.com>
2021-06-11 11:01:40 +0200

    addded Wohnraum pro Person analysis

--- src/main/R/analysisJR.R
+++ src/main/R/analysisJR.R
@@ -7,6 +7,8 @@ library(readxl)
 library(tidyverse)
 library(janitor)
 library(lubridate)
+library(sf)
+library(tmap)
 
 
 rm(list = ls())
@@ -24,12 +26,12 @@ rki <- read_excel("D:/Dropbox/Documents/VSP/Fallzahlen_Kum_Tab_9JuniB.xlsx",
 
 rki_berlin <- rki %>%
   filter(grepl("berlin", LK, ignore.case = TRUE)) %>%
-  select(-c("...1","LKNR")) %>%
-  pivot_longer(!contains("LK"),names_to = "date",values_to = "cases")
+  select(-c("...1", "LKNR")) %>%
+  pivot_longer(!contains("LK"), names_to = "date", values_to = "cases")
 
-for(i in 1:nrow(rki_berlin)){
+for (i in 1:nrow(rki_berlin)) {
   dateX <- as.character(rki_berlin$date[i])
-  if(grepl("44",dateX)){
+  if (grepl("44", dateX)) {
     date_improved <- as.character(excel_numeric_to_date(as.numeric(dateX)))
     rki_berlin$date[i] <- date_improved
   } else {
@@ -46,17 +48,18 @@ rki_berlin$date <- ymd
 
 rki_berlin$LK <- rki_berlin$LK %>%
   str_replace("SK Berlin ", "") %>%
-  str_replace("-","_") %>%
-  str_replace("ö","oe")
+  str_replace("-", "_") %>%
+  str_replace("ö", "oe")
 
 
-rki_berlin <- rki_berlin %>% rename(district = LK) %>% mutate(cases = cases/7)
+rki_berlin <- rki_berlin %>%
+  rename(district = LK) %>%
+  mutate(cases = cases / 7)
 
 ggplot(rki_berlin, mapping = aes(x = date, y = cases)) +
   geom_line(aes(color = district)) +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b-%y")+
+  scale_x_date(date_breaks = "1 month", date_labels = "%b-%y") +
   labs(title = paste0("Daily  Cases in Berlin"), x = "Date", y = "Cases")
-
 
 
 ### 1b) RKI Previous Data
@@ -65,14 +68,14 @@ rki_old <- read_csv("D:/Dropbox/Documents/VSP/RKI_COVID19_02112020.csv")
 rki_berlin_old <- rki_old %>%
   filter(grepl("berlin", Landkreis, ignore.case = TRUE)) %>%
   mutate(date = as.Date(Refdatum, format = "%m/%d/%Y"), district = Landkreis) %>% ## TODO: RefDatum or Meldedaturm
-  select(district,AnzahlFall,date) %>%
-  group_by(district,date) %>%
+  select(district, AnzahlFall, date) %>%
+  group_by(district, date) %>%
   summarise(rki_cases_old = sum(AnzahlFall)) %>%
-  mutate(district = str_replace(district,"SK Berlin ","")) %>%
-  mutate(district = str_replace(district,"-","_")) %>%
-  mutate(district = str_replace(district,"ö","oe"))
+  mutate(district = str_replace(district, "SK Berlin ", "")) %>%
+  mutate(district = str_replace(district, "-", "_")) %>%
+  mutate(district = str_replace(district, "ö", "oe"))
 
-ggplot(rki_berlin_old %>% filter(district=="Mitte"), aes(x= date, y= rki_cases_old))+
+ggplot(rki_berlin_old %>% filter(district == "Mitte"), aes(x = date, y = rki_cases_old)) +
   geom_line(aes(color = district))
 
 
@@ -82,30 +85,34 @@ ggplot(rki_berlin_old %>% filter(district=="Mitte"), aes(x= date, y= rki_cases_o
 
 # facilities
 fac_to_district_map <- read_delim("C:/Users/jakob/projects/matsim-episim/FacilityToDistrictMapCOMPLETE.txt",
-                                     ";", escape_double = FALSE, col_names = FALSE,
-                                     trim_ws = TRUE) %>% rename("facility" = "X1") %>% rename("district" = "X2")
+                                  ";", escape_double = FALSE, col_names = FALSE,
+                                  trim_ws = TRUE) %>%
+  rename("facility" = "X1") %>%
+  rename("district" = "X2")
 
 fac_to_district_map[is.na(fac_to_district_map)] <- "outta berlin"
 
 fac_to_district_map$facility <- fac_to_district_map$facility %>%
-  str_replace("[A-Z]$","")
+  str_replace("[A-Z]$", "")
 
 
 # episim
 episim_df <- read_delim("C:/Users/jakob/Desktop/locationBasedRestrictions3.infectionEvents.txt",
-                        "\t", escape_double = FALSE, trim_ws = TRUE) %>% select(date,facility)
+                        "\t", escape_double = FALSE, trim_ws = TRUE) %>% select(date, facility)
 
-episim_df2 <- episim_df %>% filter(!grepl("^tr_",facility))
+episim_df2 <- episim_df %>% filter(!grepl("^tr_", facility))
 
 episim_df2$facility <- episim_df2$facility %>%
-  str_replace("home_","") %>%
-  str_replace("_split\\d","") %>%
-  str_replace("[A-Z]$","")
+  str_replace("home_", "") %>%
+  str_replace("_split\\d", "") %>%
+  str_replace("[A-Z]$", "")
 
 merged <- episim_df2 %>%
-  left_join(fac_to_district_map, by = c("facility"),keep = TRUE)
+  left_join(fac_to_district_map, by = c("facility"), keep = TRUE)
 
-na_facs <- merged %>% filter(is.na(district)) %>% pull(facility.x)
+na_facs <- merged %>%
+  filter(is.na(district)) %>%
+  pull(facility.x)
 length(unique(na_facs))
 
 # All unique facilities in episim output: 39.734
@@ -118,39 +125,93 @@ length(unique(na_facs))
 # remove A/B at end : 6571
 
 
-episim_final  <- merged %>%
+episim_final <- merged %>%
   filter(!is.na(district)) %>%
-  filter(district!="outta berlin") %>%
+  filter(district != "outta berlin") %>%
   select(!starts_with("facility")) %>%
   group_by(date, district) %>%
-  count() %>% rename(infections = n)
+  count() %>%
+  rename(infections = n)
 
-ggplot(episim_final, mapping = aes(x=date,y = infections))+
+ggplot(episim_final, mapping = aes(x = date, y = infections)) +
   geom_line(mapping = aes(color = district))
-
 
 
 # 3) Compare data, see if my ... made improvement
 rki_and_episim <- episim_final %>%
-  full_join(rki_berlin, by = c("date","district")) %>%
+  full_join(rki_berlin, by = c("date", "district")) %>%
   rename(rki = cases, episim = infections) %>%
-  full_join(rki_berlin_old, by = c("date","district")) %>%
+  full_join(rki_berlin_old, by = c("date", "district")) %>%
   mutate(month = months(date)) %>%
   mutate(week = week(date)) %>%
   mutate(year = year(date)) %>%
-  group_by(district,year,week) %>%
-  summarise(episim_week = mean(episim,na.rm = TRUE),
-            rki_week = mean(rki,na.rm = TRUE),
-            rki_old_week = mean(rki_cases_old,na.rm = TRUE)) %>%
-  mutate(week_year = as.Date(paste(year, week, 1, sep="-"), "%Y-%U-%u"))
+  group_by(district, year, week) %>%
+  summarise(episim_week = mean(episim, na.rm = TRUE),
+            rki_week = mean(rki, na.rm = TRUE),
+            rki_old_week = mean(rki_cases_old, na.rm = TRUE)) %>%
+  mutate(week_year = as.Date(paste(year, week, 1, sep = "-"), "%Y-%U-%u")) %>%
+  pivot_longer(c(rki_week, rki_old_week, episim_week), names_to = "data_source", values_to = "cases")
 
 # ggplot(rki_and_episim %>% filter(district=="Mitte"), aes(x = date)) +
 #   geom_line(aes(y= rki), color = 'red') +
 #   geom_line(aes(y= episim), color = 'blue')+
 #   geom_line(aes(y = rki_cases_old), color = 'orange')
 
-ggplot(rki_and_episim %>% filter(district=="Mitte"), aes(x = week_year)) +
-  geom_line(aes(y= rki_week), color = 'red') +
-  geom_line(aes(y = rki_old_week), color = 'dark red') +
-  geom_line(aes(y= episim_week), color = 'blue')
+# ggplot(rki_and_episim %>% filter(district=="Mitte"), aes(x = week_year)) +
+#   geom_line(aes(y= rki_week), color = 'red') +
+#   geom_line(aes(y = rki_old_week), color = 'orange') +
+#   geom_line(aes(y= episim_week), color = 'blue') +
+#   scale_x_date(date_breaks = "1 month", date_labels = "%b-%y")+
+#   labs(title = "Infections per Day for Mitte (Weekly Average)", x = "Date", y="New Infections")
+
+ggplot(rki_and_episim %>% filter(district == "Mitte"), aes(x = week_year, y = cases)) +
+  geom_line(aes(color = data_source)) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b-%y") +
+  labs(title = "Infections per Day for Mitte (Weekly Average)",
+       subtitle = "With Location Based Restrictions",
+       x = "Date", y = "New Infections") +
+  theme(axis.text.x = element_text(angle = 90))
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### Living Area per Person
+rm(list = ls())
+LOR_shape <-  #LOR_SHP_2021/lor_plr.shp"
+bzk <- st_read("D:/Dropbox/Documents/VSP/bezirke/bezirksgrenzen.shp")
+lor <- st_read("D:/Dropbox/Documents/VSP/LOR_SHP_2015/RBS_OD_LOR_2015_12.shp") %>%
+  rename(PLR_ID = PLR)
+
+
+mss2019 <- read_excel("D:/Dropbox/Documents/VSP/4.1.KontextInd_Anteile_PLR_MSS2019.xlsx", skip = 14) %>%
+  rename(PLR_ID = Planungsraum,
+         PLR_NAME = ...2,
+         EW = "EW          31.12.2018") %>%
+  select(- "...4") %>%
+  filter(grepl("^[0-9][0-9]{1,}[0-9]$", PLR_ID))
+
+#K15 : Wohnfläche: Wohnfläche in m² je Einwohnerinnen und Einwohner am 31.12.2018
+#K14 : Wohnräume: Anzahl der Wohnräume (einschl. Küche) je Einwohnerinnen und Einwohner am 31.12.2018
+
+k15 <- mss2019 %>%
+  select(contains("PLR"), EW, "K 15") %>%
+  rename("m2 pro Person" = "K 15")
+
+
+
+joined <- lor %>% left_join(k15,by= "PLR_ID")
+
+joined$`m2 pro Person`[joined$`m2 pro Person` == 0] <- NA
+
+
+antiiiiii <- lor %>% anti_join(k15, by = "PLR_ID")
+
+library(viridis)
+library(spData)
+
+tmap_mode("view")
+tm_basemap(leaflet::providers$OpenStreetMap) +
+  tm_shape(joined)+
+  tm_polygons(col = "m2 pro Person", id = "PLRNAME", palette = viridis(9), alpha = 0.85) +
+  tm_shape(bzk) +
+  tm_borders(col= "blue")+
+  tm_layout(title = "Wohnraum pro Planungsraum, Stand 2018")
 
