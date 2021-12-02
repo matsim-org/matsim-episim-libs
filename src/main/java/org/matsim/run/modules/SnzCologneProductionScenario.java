cfc6c8c8f5c258c6860b41bd55a871347ce22cbe
smuellervsp <mueller@vsp.tu-berlin.de>
2021-12-02 10:59:43 +0100

    cologne omicron runs

--- src/main/java/org/matsim/run/modules/SnzCologneProductionScenario.java
+++ src/main/java/org/matsim/run/modules/SnzCologneProductionScenario.java
@@ -264,7 +264,7 @@ public final class SnzCologneProductionScenario extends SnzProductionScenario {
 		//restrictions and masks
 		CreateRestrictionsFromCSV activityParticipation = new CreateRestrictionsFromCSV(episimConfig);
 
-		activityParticipation.setInput(INPUT.resolve("cologneSnzData_daily_until20211114.csv"));
+		activityParticipation.setInput(INPUT.resolve("cologneSnzData_daily_until20211126.csv"));
 
 		activityParticipation.setScale(this.scale);
 		activityParticipation.setLeisureAsNightly(this.leisureNightly);
