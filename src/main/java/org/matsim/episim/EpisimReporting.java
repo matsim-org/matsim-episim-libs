971ede329a29428327d32fe88631acaa708e2f74
Christian Rakow <rakow@vsp.tu-berlin.de>
2022-01-17 17:18:27 +0100

    handle testing quarantine in reporting

--- src/main/java/org/matsim/episim/EpisimReporting.java
+++ src/main/java/org/matsim/episim/EpisimReporting.java
@@ -411,6 +411,7 @@ public final class EpisimReporting implements BasicEventHandler, Closeable, Exte
 					report.nInQuarantineFull++;
 					district.nInQuarantineFull++;
 					break;
+				case testing:
 				case no:
 					break;
 				default:
