aa8586ee524a929668c1097e2e322b4d6e9a0c49
Jakob Rehmann <jakob.rehmann@gmail.com>
2022-02-08 11:51:44 +0100

    updated SnzBerlinProductionScenario & SnzBerlinScenario25pct2020i

--- src/main/java/org/matsim/run/modules/SnzBerlinProductionScenario.java
+++ src/main/java/org/matsim/run/modules/SnzBerlinProductionScenario.java
@@ -21,6 +21,7 @@
 package org.matsim.run.modules;
 
 import com.google.inject.Provides;
+import com.google.inject.multibindings.Multibinder;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.episim.EpisimConfigGroup;
@@ -33,11 +34,14 @@ import org.matsim.episim.model.activity.LocationBasedParticipationModel;
 import org.matsim.episim.model.input.CreateAdjustedRestrictionsFromCSV;
 import org.matsim.episim.model.input.CreateRestrictionsFromCSV;
 import org.matsim.episim.model.input.RestrictionInput;
+import org.matsim.episim.model.listener.HouseholdSusceptibility;
 import org.matsim.episim.model.progression.AgeDependentDiseaseStatusTransitionModel;
 import org.matsim.episim.model.progression.DiseaseStatusTransitionModel;
+import org.matsim.episim.model.vaccination.VaccinationFromData;
 import org.matsim.episim.model.vaccination.VaccinationModel;
 import org.matsim.episim.policy.AdjustedPolicy;
 import org.matsim.episim.policy.FixedPolicy;
+import org.matsim.episim.policy.Restriction;
 import org.matsim.episim.policy.ShutdownPolicy;
 
 import javax.inject.Singleton;
@@ -143,6 +147,7 @@ public final class SnzBerlinProductionScenario extends SnzProductionScenario {
 		bind(InfectionModel.class).to(infectionModel).in(Singleton.class);
 		bind(VaccinationModel.class).to(vaccinationModel).in(Singleton.class);
 
+		//TODO: this is not in the cologne prod scenario, is it still needed?
 		if (adjustRestrictions == AdjustRestrictions.yes)
 			bind(ShutdownPolicy.class).to(AdjustedPolicy.class).in(Singleton.class);
 		else
@@ -155,6 +160,20 @@ public final class SnzBerlinProductionScenario extends SnzProductionScenario {
 				bind(ActivityParticipationModel.class).to(DefaultParticipationModel.class);
 			}
 		}
+
+		bind(VaccinationFromData.Config.class).toInstance(
+				VaccinationFromData.newConfig("11000")
+						.withAgeGroup("05-11", 237886.9)
+						.withAgeGroup("12-17", 182304.4)
+						.withAgeGroup("18-59", 2138015)
+						.withAgeGroup("60+", 915851)
+		);
+
+		// TODO: from Cologne, do we need?
+		//		Multibinder.newSetBinder(binder(), SimulationListener.class)
+		//				.addBinding().to(HouseholdSusceptibility.class);
+
+
 	}
 
 	@Provides
@@ -164,15 +183,18 @@ public final class SnzBerlinProductionScenario extends SnzProductionScenario {
 		if (this.sample != 25 && this.sample != 100)
 			throw new RuntimeException("Sample size not calibrated! Currently only 25% is calibrated. Comment this line out to continue.");
 
+		//general config
 		Config config = ConfigUtils.createConfig(new EpisimConfigGroup());
 
-		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
 
 		config.global().setRandomSeed(7564655870752979346L);
 
 		config.vehicles().setVehiclesFile(INPUT.resolve("de_2020-vehicles.xml").toString());
 
 		config.plans().setInputFile(inputForSample("be_2020-week_snz_entirePopulation_emptyPlans_withDistricts_%dpt_split.xml.gz", sample));
+
+		//episim config
+		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
 
 		episimConfig.addInputEventsFile(inputForSample("be_2020-week_snz_episim_events_wt_%dpt_split.xml.gz", sample))
 				.addDays(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY);
@@ -185,28 +207,35 @@ public final class SnzBerlinProductionScenario extends SnzProductionScenario {
 
 		episimConfig.setActivityHandling(activityHandling);
 
-		episimConfig.setThreads(6);
+		//		episimConfig.setThreads(6); // TODO: check repeat from below
 
-//		episimConfig.addInputEventsFile(inputForSample("be_2020-week_snz_episim_events_wt_%dpt_split.xml.gz", sample))
-//				.addDays(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY);
-//
-//		episimConfig.addInputEventsFile(inputForSample("be_2020-week_snz_episim_events_sa_%dpt_split.xml.gz", sample))
-//				.addDays(DayOfWeek.SATURDAY);
-//
-//		episimConfig.addInputEventsFile(inputForSample("be_2020-week_snz_episim_events_so_%dpt_split.xml.gz", sample))
-//				.addDays(DayOfWeek.SUNDAY);
+		//		episimConfig.addInputEventsFile(inputForSample("be_2020-week_snz_episim_events_wt_%dpt_split.xml.gz", sample))
+		//				.addDays(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY);
+		//
+		//		episimConfig.addInputEventsFile(inputForSample("be_2020-week_snz_episim_events_sa_%dpt_split.xml.gz", sample))
+		//				.addDays(DayOfWeek.SATURDAY);
+		//
+		//		episimConfig.addInputEventsFile(inputForSample("be_2020-week_snz_episim_events_so_%dpt_split.xml.gz", sample))
+		//				.addDays(DayOfWeek.SUNDAY);
 
 		episimConfig.setCalibrationParameter(1.7E-5 * 0.8);
 		episimConfig.setStartDate("2020-02-25");
 		episimConfig.setFacilitiesHandling(EpisimConfigGroup.FacilitiesHandling.snz);
 		episimConfig.setSampleSize(this.sample / 100.);
 		episimConfig.setHospitalFactor(0.5);
-		episimConfig.setProgressionConfig(AbstractSnzScenario2020.baseProgressionConfig(Transition.config()).build());
 		episimConfig.setThreads(8);
+		//		episimConfig.setDaysInfectious(Integer.MAX_VALUE); // TODO: from cologne, should this be included?
 
-		if (this.snapshot != Snapshot.no) episimConfig.setStartFromSnapshot(INPUT.resolve("snapshots/" + snapshot + ".zip").toString());
+		//progression model
+		//		episimConfig.setProgressionConfig(AbstractSnzScenario2020.baseProgressionConfig(Transition.config()).build());
+		episimConfig.setProgressionConfig(SnzProductionScenario.progressionConfig(Transition.config()).build()); // TODO: why does this immediately override?
 
-		//inital infections and import
+
+		//snapshot
+		if (this.snapshot != Snapshot.no)
+			episimConfig.setStartFromSnapshot(INPUT.resolve("snapshots/" + snapshot + ".zip").toString());
+
+		//inital infections and import // TODO: this is done differently in Cologne
 		episimConfig.setInitialInfections(Integer.MAX_VALUE);
 		if (this.diseaseImport != DiseaseImport.no) {
 
@@ -224,19 +253,21 @@ public final class SnzBerlinProductionScenario extends SnzProductionScenario {
 			episimConfig.setCalibrationParameter(2.54e-5);
 		}
 
+		//age-dependent infection model
 		if (this.infectionModel != AgeDependentInfectionModelWithSeasonality.class) {
 			if (this.diseaseImport == DiseaseImport.yes) {
-				episimConfig.setCalibrationParameter(1.6E-5);
+				episimConfig.setCalibrationParameter(1.6E-5); // TODO: setting calibration param in multiple places may be error-prone
 			} else {
 				episimConfig.setCalibrationParameter(1.6E-5 * 2.54e-5 / 1.7E-5);
 			}
 		}
 
+		//contact intensities
 		SnzProductionScenario.configureContactIntensities(episimConfig);
 
 		//restrictions and masks
 		RestrictionInput activityParticipation;
-		SnzBerlinScenario25pct2020.BasePolicyBuilder basePolicyBuilder = new SnzBerlinScenario25pct2020.BasePolicyBuilder(episimConfig);
+		SnzBerlinScenario25pct2020.BasePolicyBuilder basePolicyBuilder = new SnzBerlinScenario25pct2020.BasePolicyBuilder(episimConfig); // TODO: should SnzBerlinScenario25pct2020 be absorbed into this class to match Cologne
 		if (adjustRestrictions == AdjustRestrictions.yes) {
 			activityParticipation = new CreateAdjustedRestrictionsFromCSV();
 		} else {
@@ -244,7 +275,7 @@ public final class SnzBerlinProductionScenario extends SnzProductionScenario {
 		}
 
 		String untilDate = "20210802";
-		activityParticipation.setInput(INPUT.resolve("BerlinSnzData_daily_until20210822.csv"));
+		activityParticipation.setInput(INPUT.resolve("BerlinSnzData_daily_until20210822.csv")); // TODO: update
 
 		//location based restrictions
 		if (locationBasedRestrictions == LocationBasedRestrictions.yes) {
@@ -283,13 +314,22 @@ public final class SnzBerlinProductionScenario extends SnzProductionScenario {
 		basePolicyBuilder.setCiCorrections(Map.of());
 		FixedPolicy.ConfigBuilder builder = basePolicyBuilder.buildFixed();
 
-		// tracing
+		//curfew TODO: when is the restriction of closing hours lifted? (also for Cologne)
+		builder.restrict("2021-04-24", Restriction.ofClosingHours(22, 5), "leisure", "visit");
+		Map<LocalDate, Double> curfewCompliance = new HashMap<LocalDate, Double>();
+		curfewCompliance.put(LocalDate.parse("2021-04-24"), 1.0);//https://www.berlin.de/aktuelles/berlin/6522691-958092-naechtliche-ausgangsbeschraenkungen-ab-s.html
+		curfewCompliance.put(LocalDate.parse("2021-05-19"), 0.0); //https://www.berlin.de/aktuelles/berlin/6593002-958092-ausgangsbeschraenkungen-gelten-mittwoch-.html
+		episimConfig.setCurfewCompliance(curfewCompliance);
+
+
+		//tracing
 		if (this.tracing == Tracing.yes) {
 
 			SnzProductionScenario.configureTracing(config, 1.0);
 
 		}
 
+		//christmas and easter models
 		Map<LocalDate, DayOfWeek> inputDays = new HashMap<>();
 
 		if (this.christmasModel != ChristmasModel.no) {
@@ -306,7 +346,7 @@ public final class SnzBerlinProductionScenario extends SnzProductionScenario {
 		if (this.weatherModel != WeatherModel.no) {
 
 			SnzProductionScenario.configureWeather(episimConfig, weatherModel,
-					INPUT.resolve("tempelhofWeatherUntil20210816.csv").toFile(),
+					INPUT.resolve("tempelhofWeatherUntil20210816.csv").toFile(), // TODO: need new weather file DownloadWeatherData
 					INPUT.resolve("temeplhofWeatherDataAvg2000-2020.csv").toFile())
 			;
 
@@ -316,6 +356,7 @@ public final class SnzBerlinProductionScenario extends SnzProductionScenario {
 					LocalDate.of(2020, 1, 1), 0.)
 			);
 		}
+
 		//leisure & work factor
 		double leisureFactor = 1.6;
 		if (this.restrictions != Restrictions.no) {
@@ -323,21 +364,47 @@ public final class SnzBerlinProductionScenario extends SnzProductionScenario {
 
 			double workVacFactor = 0.92;
 			BiFunction<LocalDate, Double, Double> f = (d, e) -> workVacFactor * e;
-
+			//WorkVac Factors are applied from last school day -> last vacation day (Saturday not Sunday,
+			// if vacation ends on weekend, b/c new restrictions for following week are always set sundays)
 			builder.applyToRf("2020-04-03", "2020-04-17", f, "work", "business");
+			//Sommerferien
 			builder.applyToRf("2020-06-26", "2020-08-07", f, "work", "business");
+			//Herbstferien
 			builder.applyToRf("2020-10-09", "2020-10-23", f, "work", "business");
+			//Weihnachtsferien
 			builder.applyToRf("2020-12-18", "2021-01-01", f, "work", "business");
+			//Winterferien
 			builder.applyToRf("2021-01-29", "2021-02-05", f, "work", "business");
+			//Osterferien
 			builder.applyToRf("2021-03-26", "2021-04-09", f, "work", "business");
+			//Sommerfeirien
 			builder.applyToRf("2021-06-25", "2021-08-06", f, "work", "business");
+			//Herbstferien
+			builder.applyToRf("2021-10-08", "2021-10-22", f, "work", "business");
+			//Weihnachtsferien
+			builder.applyToRf("2021-12-17", "2022-01-04", f, "work", "business");
+			//Winterferien
+			builder.applyToRf("2022-01-28", "2022-02-04", f, "work", "business");
+			// future
+			//Osterferien
+			builder.restrict(LocalDate.parse("2022-04-08"), 0.92, "work", "business");
+			builder.restrict(LocalDate.parse("2022-04-23"), 1.0, "work", "business");
+			//Sommerferien
+			builder.restrict(LocalDate.parse("2022-07-07"), 0.92, "work", "business");
+			builder.restrict(LocalDate.parse("2022-08-19"), 1.0, "work", "business");
+			//Herbstferien
+			builder.restrict(LocalDate.parse("2022-10-21"), 0.92, "work", "business");
+			builder.restrict(LocalDate.parse("2022-11-05"), 1.0, "work", "business");
+			//Weihnachtsferien
+			builder.restrict(LocalDate.parse("2022-12-22"), 0.92, "work", "business");
+			builder.restrict(LocalDate.parse("2023-01-02"), 1.0, "work", "business");
+
 		}
 
 
-
+		//vaccinations
 		if (this.vaccinations.equals(Vaccinations.yes)) {
 
-			// vaccinations
 			VaccinationConfigGroup vaccinationConfig = ConfigUtils.addOrGetModule(config, VaccinationConfigGroup.class);
 
 			SnzProductionScenario.configureVaccines(vaccinationConfig, 4_800_000);
