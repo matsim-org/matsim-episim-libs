d7460e91aaae06e9b1961b70a80963d3a157be4e
Kai Nagel <nagel@vsp.tu-berlin.de>
2022-02-14 13:35:22 +0100

    cleaning up the new test

--- src/test/java/org/matsim/episim/model/InfectionModelWithAntibodiesTest.java
+++ src/test/java/org/matsim/episim/model/InfectionModelWithAntibodiesTest.java
@@ -4,19 +4,13 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.population.Person;
-import org.matsim.core.config.Config;
 import org.matsim.episim.EpisimPerson;
-import org.matsim.episim.EpisimReporting;
 import org.matsim.episim.EpisimTestUtils;
 import org.matsim.episim.events.EpisimInfectionEvent;
 import org.matsim.testcases.MatsimTestUtils;
-import org.matsim.utils.objectattributes.attributable.Attributes;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.SplittableRandom;
 
 public class InfectionModelWithAntibodiesTest{
 	private static final Logger log = Logger.getLogger( InfectionModelWithAntibodiesTest.class );
@@ -53,9 +47,7 @@ public class InfectionModelWithAntibodiesTest{
 				basePerson.possibleInfection(
 						new EpisimInfectionEvent( 50 * 3600 * 24., basePerson.getPersonId(), infector.getPersonId(), null, "dummy", 2, VirusStrain.OMICRON_BA1, 1. ) );
 				basePerson.checkInfection();
-				nAbBase = InfectionModelWithAntibodies.getAntibodyLevel( basePerson, 100, basePerson.getNumVaccinations(), basePerson.getNumInfections(),
-						VirusStrain.SARS_CoV_2 )
-								 / InfectionModelWithAntibodies.getAk50( basePerson, VirusStrain.SARS_CoV_2, ak50PerStrain, basePerson.getNumInfections() );
+				nAbBase = relativeAbLevel( ak50PerStrain, basePerson, VirusStrain.SARS_CoV_2 );
 			}
 			
 			// Fig.1 A (vaccinated + omicron):
@@ -66,26 +58,22 @@ public class InfectionModelWithAntibodiesTest{
 			person.checkInfection();
 			{
 				VirusStrain strain = VirusStrain.SARS_CoV_2;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 4000./4000., nAb/nAbBase, 0.1);
 			}
 			{
 				VirusStrain strain = VirusStrain.ALPHA;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 4000./4000., nAb/nAbBase, 0.1 );
 			}
 			{
 				VirusStrain strain = VirusStrain.DELTA;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 4000./4000., nAb/nAbBase, 0.5 );
 			}
 			{
 				VirusStrain strain = VirusStrain.OMICRON_BA1;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 1000./4000., nAb/nAbBase, 0.5 );
 			}
 
@@ -95,26 +83,22 @@ public class InfectionModelWithAntibodiesTest{
 			person.checkInfection();
 			{
 				VirusStrain strain = VirusStrain.SARS_CoV_2;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 0./4000., nAb/nAbBase, 0.1);
 			}
 			{
 				VirusStrain strain = VirusStrain.ALPHA;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 0./4000., nAb/nAbBase, 0.1 );
 			}
 			{
 				VirusStrain strain = VirusStrain.DELTA;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 10./4000., nAb/nAbBase, 0.1 );
 			}
 			{
 				VirusStrain strain = VirusStrain.OMICRON_BA1;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 60./4000., nAb/nAbBase, 0.1 );
 			}
 
@@ -161,31 +145,32 @@ public class InfectionModelWithAntibodiesTest{
 			person.checkInfection();
 			{
 				VirusStrain strain = VirusStrain.SARS_CoV_2;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 1000./4000., nAb/nAbBase, 0.5);
 			}
 			{
 				VirusStrain strain = VirusStrain.ALPHA;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 1000./4000., nAb/nAbBase, 0.5 );
 			}
 			{
 				VirusStrain strain = VirusStrain.DELTA;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 2000./4000., nAb/nAbBase, 0.5 );
 			}
 			{
 				VirusStrain strain = VirusStrain.OMICRON_BA1;
-				double nAb = InfectionModelWithAntibodies.getAntibodyLevel( person, 100, person.getNumVaccinations(), person.getNumInfections(), strain )
-							     / InfectionModelWithAntibodies.getAk50( person, strain, ak50PerStrain, person.getNumInfections() );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
 				Assert.assertEquals( 500./4000., nAb/nAbBase, 0.5 );
 			}
 
 
 		}
 
+	}
+	private double relativeAbLevel( Map<VirusStrain, Double> ak50PerStrain, EpisimPerson basePerson, VirusStrain sars_coV_2 ){
+		return InfectionModelWithAntibodies.getAntibodyLevel( basePerson, 100, basePerson.getNumVaccinations(), basePerson.getNumInfections(),
+				sars_coV_2 )
+				       / InfectionModelWithAntibodies.getAk50( basePerson, sars_coV_2, ak50PerStrain, basePerson.getNumInfections() );
 	}
 }
