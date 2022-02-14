0645876457e2cb8e06e3c888d8683ff35b114bc0
Kai Nagel <nagel@vsp.tu-berlin.de>
2022-02-14 13:56:24 +0100

    start adding antibody level tests for 2nd paper

--- src/test/java/org/matsim/episim/model/InfectionModelWithAntibodiesTest.java
+++ src/test/java/org/matsim/episim/model/InfectionModelWithAntibodiesTest.java
@@ -17,6 +17,62 @@ public class InfectionModelWithAntibodiesTest{
 	@Rule public MatsimTestUtils utils = new MatsimTestUtils();
 
 	@Test
+	public void yuEtAl(){
+		// https://doi.org/10.1101/2022.02.06.22270533
+
+		EpisimPerson infector = EpisimTestUtils.createPerson();
+
+		Map<VirusStrain, Double> ak50PerStrain = new HashMap<>();
+
+		ak50PerStrain.put( VirusStrain.SARS_CoV_2, 0.2 );
+		ak50PerStrain.put( VirusStrain.ALPHA, 0.2 );
+		ak50PerStrain.put( VirusStrain.B1351, 0.4 );
+		ak50PerStrain.put( VirusStrain.DELTA, 0.4 );
+		ak50PerStrain.put( VirusStrain.OMICRON_BA1, 2.4 );
+		ak50PerStrain.put( VirusStrain.OMICRON_BA2, 3.0 );
+		ak50PerStrain.put( VirusStrain.STRAIN_A, 3.0 * 2 );
+		ak50PerStrain.put( VirusStrain.STRAIN_B, 3.0 );
+
+		EpisimPerson recoveredPerson = EpisimTestUtils.createPerson();
+		recoveredPerson.possibleInfection( new EpisimInfectionEvent( 0, recoveredPerson.getPersonId(), infector.getPersonId(),
+				null, "dummyInfectionType", 2, VirusStrain.DELTA, 1. ) );
+		recoveredPerson.checkInfection();
+
+		{
+			// I use the 658 against the wild variant as base:
+			double nAbBase;
+			{
+				EpisimPerson basePerson = EpisimTestUtils.createPerson();
+				basePerson.setVaccinationStatus( EpisimPerson.VaccinationStatus.yes, VaccinationType.mRNA, 0 );
+				nAbBase = relativeAbLevel( ak50PerStrain, basePerson, VirusStrain.SARS_CoV_2, 100 );
+			}
+
+			// only vaccinated:
+
+			EpisimPerson person = EpisimTestUtils.createPerson();
+			person.setVaccinationStatus( EpisimPerson.VaccinationStatus.yes, VaccinationType.mRNA, 0 );
+			{
+				VirusStrain strain = VirusStrain.SARS_CoV_2;
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
+				Assert.assertEquals( 658./658., nAb/nAbBase, 0.5);
+			}
+			{
+				VirusStrain strain = VirusStrain.OMICRON_BA1;
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
+				Assert.assertEquals( 29./658., nAb/nAbBase, 0.5 );
+			}
+			{
+				VirusStrain strain = VirusStrain.OMICRON_BA2;
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
+				Assert.assertEquals( 24./658., nAb/nAbBase, 0.5 );
+			}
+
+			// yyyy more to be added here ...
+
+		}
+	}
+
+	@Test
 	public void roesslerEtAl(){
 		// http://dx.doi.org/10.1101/2022.02.01.22270263 Fig.1
 
@@ -47,7 +103,7 @@ public class InfectionModelWithAntibodiesTest{
 				basePerson.possibleInfection(
 						new EpisimInfectionEvent( 50 * 3600 * 24., basePerson.getPersonId(), infector.getPersonId(), null, "dummy", 2, VirusStrain.OMICRON_BA1, 1. ) );
 				basePerson.checkInfection();
-				nAbBase = relativeAbLevel( ak50PerStrain, basePerson, VirusStrain.SARS_CoV_2 );
+				nAbBase = relativeAbLevel( ak50PerStrain, basePerson, VirusStrain.SARS_CoV_2, 100 );
 			}
 			
 			// Fig.1 A (vaccinated + omicron):
@@ -58,22 +114,22 @@ public class InfectionModelWithAntibodiesTest{
 			person.checkInfection();
 			{
 				VirusStrain strain = VirusStrain.SARS_CoV_2;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 4000./4000., nAb/nAbBase, 0.1);
 			}
 			{
 				VirusStrain strain = VirusStrain.ALPHA;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 4000./4000., nAb/nAbBase, 0.1 );
 			}
 			{
 				VirusStrain strain = VirusStrain.DELTA;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 4000./4000., nAb/nAbBase, 0.5 );
 			}
 			{
 				VirusStrain strain = VirusStrain.OMICRON_BA1;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 1000./4000., nAb/nAbBase, 0.5 );
 			}
 
@@ -83,22 +139,22 @@ public class InfectionModelWithAntibodiesTest{
 			person.checkInfection();
 			{
 				VirusStrain strain = VirusStrain.SARS_CoV_2;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 0./4000., nAb/nAbBase, 0.1);
 			}
 			{
 				VirusStrain strain = VirusStrain.ALPHA;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 0./4000., nAb/nAbBase, 0.1 );
 			}
 			{
 				VirusStrain strain = VirusStrain.DELTA;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 10./4000., nAb/nAbBase, 0.1 );
 			}
 			{
 				VirusStrain strain = VirusStrain.OMICRON_BA1;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 60./4000., nAb/nAbBase, 0.1 );
 			}
 
@@ -145,22 +201,22 @@ public class InfectionModelWithAntibodiesTest{
 			person.checkInfection();
 			{
 				VirusStrain strain = VirusStrain.SARS_CoV_2;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 1000./4000., nAb/nAbBase, 0.5);
 			}
 			{
 				VirusStrain strain = VirusStrain.ALPHA;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 1000./4000., nAb/nAbBase, 0.5 );
 			}
 			{
 				VirusStrain strain = VirusStrain.DELTA;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 2000./4000., nAb/nAbBase, 0.5 );
 			}
 			{
 				VirusStrain strain = VirusStrain.OMICRON_BA1;
-				double nAb = relativeAbLevel( ak50PerStrain, person, strain );
+				double nAb = relativeAbLevel( ak50PerStrain, person, strain, 100 );
 				Assert.assertEquals( 500./4000., nAb/nAbBase, 0.5 );
 			}
 
@@ -168,8 +224,8 @@ public class InfectionModelWithAntibodiesTest{
 		}
 
 	}
-	private double relativeAbLevel( Map<VirusStrain, Double> ak50PerStrain, EpisimPerson basePerson, VirusStrain sars_coV_2 ){
-		return InfectionModelWithAntibodies.getAntibodyLevel( basePerson, 100, basePerson.getNumVaccinations(), basePerson.getNumInfections(),
+	private double relativeAbLevel( Map<VirusStrain, Double> ak50PerStrain, EpisimPerson basePerson, VirusStrain sars_coV_2, int iteration ){
+		return InfectionModelWithAntibodies.getAntibodyLevel( basePerson, iteration, basePerson.getNumVaccinations(), basePerson.getNumInfections(),
 				sars_coV_2 )
 				       / InfectionModelWithAntibodies.getAk50( basePerson, sars_coV_2, ak50PerStrain, basePerson.getNumInfections() );
 	}
