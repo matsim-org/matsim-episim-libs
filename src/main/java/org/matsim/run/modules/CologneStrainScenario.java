374172080779dfe4b1ba11bf3928bb9f3809167d
smuellervsp <mueller@vsp.tu-berlin.de>
2022-03-28 16:16:58 +0200

    configure alpha

--- src/main/java/org/matsim/run/modules/CologneStrainScenario.java
+++ src/main/java/org/matsim/run/modules/CologneStrainScenario.java
@@ -1,8 +1,19 @@
 package org.matsim.run.modules;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.episim.EpisimConfigGroup;
+import org.matsim.episim.VirusStrainConfigGroup;
 import org.matsim.episim.model.InfectionModelWithAntibodies;
+import org.matsim.episim.model.VirusStrain;
 import org.matsim.episim.model.vaccination.NoVaccination;
+
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 /**
  * Cologne scenario for calibration of different strains.
@@ -22,5 +33,27 @@ public class CologneStrainScenario extends SnzCologneProductionScenario {
 		);
 
 	}
+	
+	@Provides
+ 	@Singleton
+ 	public Config config() {
+
+ 		Config config = super.config();
+ 		
+		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
+
+		VirusStrainConfigGroup virusStrainConfigGroup = ConfigUtils.addOrGetModule(config, VirusStrainConfigGroup.class);
+
+		Map<LocalDate, Integer> infPerDayALPHA = new HashMap<>();
+		infPerDayALPHA.put(LocalDate.parse("2020-01-01"), 0);
+		infPerDayALPHA.put(LocalDate.parse("2021-01-15"), 4);
+		infPerDayALPHA.put(LocalDate.parse("2021-01-22"), 1);
+		episimConfig.setInfections_pers_per_day(VirusStrain.ALPHA, infPerDayALPHA);
+
+		virusStrainConfigGroup.getOrAddParams(VirusStrain.ALPHA).setInfectiousness(1.65);
+		virusStrainConfigGroup.getOrAddParams(VirusStrain.ALPHA).setFactorSeriouslySick(1.0);
+
+ 		return config;
+ 	}
 
 }
