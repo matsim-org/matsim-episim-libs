5cfe0aa34ae6e59e9e870edd942b25d18abd93d1
Christian Rakow <rakow@vsp.tu-berlin.de>
2022-04-05 14:37:58 +0200

    updated params

--- src/main/java/org/matsim/run/batch/CologneStrainBatch.java
+++ src/main/java/org/matsim/run/batch/CologneStrainBatch.java
@@ -6,13 +6,10 @@ import org.matsim.episim.BatchRun;
 import org.matsim.episim.EpisimConfigGroup;
 import org.matsim.run.RunParallel;
 import org.matsim.run.modules.CologneStrainScenario;
-import org.matsim.run.modules.SnzProductionScenario;
 
 import javax.annotation.Nullable;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Batch to run all seeds for the cologne strain scenario.
@@ -21,7 +18,7 @@ public class CologneStrainBatch implements BatchRun<CologneStrainBatch.Params> {
 
 	@Override
 	public CologneStrainScenario getBindings(int id, @Nullable Params params) {
-		return new CologneStrainScenario( 1.7889280917763581);
+		return new CologneStrainScenario( 1.8993316907481814);
 	}
 
 	@Override
@@ -40,7 +37,7 @@ public class CologneStrainBatch implements BatchRun<CologneStrainBatch.Params> {
 
 		EpisimConfigGroup episimConfig = ConfigUtils.addOrGetModule(config, EpisimConfigGroup.class);
 
-		episimConfig.setCalibrationParameter(1.1293077849372072e-05);
+		episimConfig.setCalibrationParameter(1.1293063756440906e-05);
 		episimConfig.setSnapshotInterval((int) ChronoUnit.DAYS.between(episimConfig.getStartDate(), LocalDate.parse("2021-01-14")));
 		episimConfig.setSnapshotPrefix("strain_base_" + params.seed);
 
