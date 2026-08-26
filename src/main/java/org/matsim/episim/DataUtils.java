53637aa5e3eb6627e3cc67cd5914fa13ec24b8d3
jaroslawsmirnow <jaroslav.smirnov.k@gmail.com>
2026-08-26 16:40:53 +0200

    Checkstyle fixes. Do not change behavior

--- src/main/java/org/matsim/episim/DataUtils.java
+++ src/main/java/org/matsim/episim/DataUtils.java
@@ -33,7 +33,7 @@ public final class DataUtils {
 
 			DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd.MM.yy");
 
-			TreeMap<LocalDate, Double> result = new TreeMap<>();
+			NavigableMap<LocalDate, Double> result = new TreeMap<>();
 
 			for (CSVRecord record : csv) {
 
@@ -58,7 +58,7 @@ public final class DataUtils {
 
 		try (CSVParser csv = new CSVParser(Files.newBufferedReader(path), CSVFormat.DEFAULT.withFirstRecordAsHeader().withCommentMarker('#'))) {
 
-			TreeMap<LocalDate, Map<VirusStrain, Double>> result = new TreeMap<>();
+			NavigableMap<LocalDate, Map<VirusStrain, Double>> result = new TreeMap<>();
 
 
 			for (CSVRecord record : csv) {
